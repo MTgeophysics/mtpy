5d9d99f20cd53b17f406873bc08e9aeaf586b965
nietky <nietky2@gmail.com>
2012-10-05 14:07:03 +0930

    Add scripts to setup script.

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ except ImportError:
     use_setuptools()
     from setuptools import setup
 
-setup(name="MTpy")
+setup(name="MTpy",
+      scripts=["MTpy/utils/CombineEDIs.py",
+               "MTpy/utils/runParalanaMT.py",
+               "MTpy/utils/wsmt-pv.py",
+               "MTpy/utils/occam2d_gui/run1.py",
+               "MTpy/core/RunBIRRPSingleStation.py"]
+      )
