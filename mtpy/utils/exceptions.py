6439d761f45a2eb3a32968af7de922b61f63e6ce
LK <LK@uni>
2013-01-18 16:35:53 +1030

    added exception module (simplification of debugging an d general error handling)
    Signed-off-by: LK <LK@uni>

---
+++ mtpy/utils/exceptions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+class MTpyError_float( Exception ): 
+    pass
+
+
+class MTpyError_inputarguments( Exception ):
+    pass
+
+
+class MTpyError_ts_data( Exception ): 
+    pass
+
