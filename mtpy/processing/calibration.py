e26afd6c9f520911125116a921400406b5bc29e0
LK <LK@uni>
2013-01-21 20:11:35 +1030

    moved calibration and filehandling to processing - started re-sorting calibration module
    Signed-off-by: LK <LK@uni>

--- mtpy/processing/calibration.py
+++ mtpy/processing/calibration.py
@@ -28,7 +28,9 @@ import time
 from mtpy.utils.exceptions import *
 #=================================================================
 
-
+list_of_loggers = ['edl','elogger']
+list_of_instruments = ['dipole','fluxgate', 'coils', 'bartington']
+dict_of_EDL_gain_factors = {'high': 0.1, 'low': 1. , 'verylow': 2.5}
 
 
 #=================================================================
@@ -43,8 +45,114 @@ from mtpy.utils.exceptions import *
 #=================================================================
 
 
-def convertfiles(dirpath,folder,infodict,fmt='%.6g'):
+def calibrate(raw_data, field, dipole_length=1., instrument, amplification, logger, gain):
+
+    """
+    Convert a given time series from raw data (instrument counts) 
+    into field strength amplitude values. 
+    The dipole length factor must be set to 1 for the calibration of B-field data.
+
+    The output is a time series of field strngth values in basic units: 
+    Tesla for the B-field and V/m for the E-field.
+
+    input:
+    - 1D time series (list or numpy array)
+    - type of measured field (E/B)
+    - instrument (fluxgate, coils, dipole, bartington)
+    - amplification factor for the instrument
+    - logger (EDL, elogger, other)
+    - gain factor of the logger
+
+    output:
+    - 1D time series (numpy array)
+
+    """
+
+    raw_data = np.array(raw_data)
+
+    _data_instrument_consitency_check(raw_data,field, dipole_length, instrument, amplification, logger, gain )
     
+
+    data = raw_data / dipole_length * amplification * gain
+
+
+    return data
+
+#=================================================================
+
+
+
+
+def _data_instrument_consitency_check(data,field, dipole, instrument, amplification, logger, gain):
+    """
+    Check, if the input values given  for the calibration make any sense at all.
+
+    """
+
+    if len(data) == 0 :
+        raise MTpyError_ts_data( 'no data provided for calibration' )
+
+    if not field.lower() in ['e','b']:
+        raise MTpyError_inputarguments( 'Field must be E or B' )
+
+    if float(dipole) <= 0:
+        raise MTpyError_inputarguments( 'Dipole length must be positive' )
+
+    if float(amplification) <= 0:
+        raise MTpyError_inputarguments( 'Amplification factor must be positive' )
+
+    if float(gain) <= 0:
+        raise MTpyError_inputarguments( 'Instrument gain must be positive' )
+
+    try:
+        if not logger.lower() in list_of_loggers:
+            raise
+    except:
+        raise MTpyError_inputarguments( 'wrong choice of logger')
+
+    try:
+        if not instrument.lower() in list_of_instruments:
+            raise
+    except:
+        raise MTpyError_inputarguments( 'wrong choice of instrument')
+
+    if field.lower == 'b':
+        if logger.lower() == 'elogger':
+            raise MTpyError_inputarguments( 'wrong choice of logger')
+        if instrument.lower() == 'dipole'
+            raise MTpyError_inputarguments( 'wrong choice of instrument')
+        if not float(dipole) == 1:
+            raise MTpyError_inputarguments( 'Dipole length must be 1 for B-field calibration')
+           
+
+    if field.lower == 'e':
+        if not instrument.lower() == 'dipole'
+            raise MTpyError_inputarguments( 'wrong choice of instrument')
+
+
+
+def e_field(raw_data):
+
+
+
+    return data
+
+
+
+def b_field(raw_data):
+
+
+    return data
+
+#-=---------------------------------------------------------
+# old functions
+
+
+
+
+
+def convertfiles(dirpath,folder,infodict,fmt='%.6g'):
+
     """
     convertfiles will convert data of counts from data logger to units.
     """
@@ -200,8 +308,7 @@ def convertlpB(bfield,dlgain=1,zadj=1):
         bfieldc = scaled bfield 1D array
     """
     bfieldc=np.array(bfield,dtype='float')/10.E7*70000.*float(dlgain)*float(zadj)
-#    for ii in range(len(bfield)):
-#        bfield[ii]=(float(bfield[ii])/10.E7)*70000.*float(dlgain)*float(zadj)
+
     return bfieldc
 
 def convertE(efield,dlgain,egain,dlength):
@@ -222,8 +329,7 @@ def convertE(efield,dlgain,egain,dlength):
     """
     efieldc=np.array(efield,dtype='float')*float(dlgain)/(float(dlength)*\
              float(egain))
-#    for ii in range(len(efield)):
-#        efield[ii]=(float(efield[ii])*float(dlgain))/(float(dlength)*float(egain))
+
     return efieldc
 
 
@@ -232,6 +338,7 @@ def convertCounts2Units(filenames,eyn='n',lpyn='n',egain=1.0,dlgain=1.0,exlen=10
     """convertCounts2Units(filenames,eyn='n',lpyn='n',egain=1.0,dlgain=1.0,exlen=100.,eylen=100.,
     magtype='lp',zadj=2) will convert a set of files given by filenames with
     parameters defined:
+
     filenames,
     eyn => electric channels converted y or n
     lpyn => long period magnetic channels converted y or n
