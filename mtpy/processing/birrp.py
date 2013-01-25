076466b6a36f99532599983a8147090092278d48
LK <LK@uni>
2013-01-25 17:03:14 +1030

    changed setup to single data column - time stamps contained in header row - reason: saving memory space and BIRRP reads single column data
    Signed-off-by: LK <LK@uni>

---
+++ mtpy/processing/birrp.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+"""
+This module contains functions for handling BIRRP software. 
+ 
+- validate_data -- validation of BIRRP input data files
+- run -- call the external function BIRRP as system process
+- sortoutput -- sort output functions
+- validate output -- validate the output functions
+- renamecoherencefiles -- rename the coherence output files
+- setup_arguments -- calculating and validating the input arguments for the call of BIRRP
+- convert -- convert the output of BIRRP into "Z" and "Tipper" values
+
+
+
+@UofA, 2013
+(LK)
+
+"""
+
+#=================================================================
+
+
+
+import numpy as np
+import re
+import sys, os
+import glob
+import os.path as op
+import subprocess
+
+from mtpy.utils.exceptions import *
+import mtpy.utils.format as MTformat
+#=================================================================
+
+
+def run():
+
+    pass
+
+
+def validate_data():
+
+    pass
+
+def sortoutput():
+
+    pass
+
+def validate_outputfiles():
+
+    pass
+
+def renamecoherencefiles():
+
+
+    pass
+
+
+def setup_arguments():
+
+    pass
+
+
+def convert():
+
+    pass
+
+    
+
+
