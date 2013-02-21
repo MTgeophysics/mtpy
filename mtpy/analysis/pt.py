defined PT class and methods. Reading od z-arrays into PT object works. Methods to be written, errors corrected (internal use of standard deviation instead of VAR). Variables VAR only used for in- and output to/from EDI files and objects (in the respective dictionaries)
Signed-off-by: LK <LK@uni>

mtpy/analysis/pt.py
@@ -29,16 +29,14 @@ import math, cmath
 import time, calendar 
 
 import mtpy.core.edi as MTedi 
-reload (MTedi)
-
 import mtpy.core.z as MTz 
-reload (MTz)
-
 import mtpy.utils.format as MTformat
-reload(MTformat)
-
 import mtpy.utils.exceptions as MTexceptions
+
 reload(MTexceptions)
+reload(MTedi)
+reload(MTz)
+reload(MTformat)
 
 
 #=================================================================
@@ -65,7 +63,6 @@ class PhaseTensor(object):
 
     def __init__(self, pt_array = None, pterr_array = None, z_array = None, zerr_array = None, z_object = None, edi_object = None):
     
-
         self.pt = None
         self.pterr = None  
 
@@ -123,7 +120,7 @@ class PhaseTensor(object):
                 pass
 
         #2. otherwise check, if valid Z object is given 
-        elif isinstance(z_object,MTedi.Z):
+        elif isinstance(z_object,MTz.Z):
             
             try:
                 z_array = z_object.z
@@ -145,13 +142,18 @@ class PhaseTensor(object):
                     if z_array.dtype in ['complex', 'float']:
                         try:
                             self.pt = np.zeros((len(z_array),2,2))
-                            for idx_f in range(len(z_array)):
-                                if zerr_array is not None:
-                                    self.pt[idx_f], self.pterr[idx_f] = z2pt(z_array[idx_f], zerr_array[idx_f] )
-                                else:
-                                    self.pt[idx_f] = z2pt( z_array[idx_f])
+                            if zerr_array is not None:
+                                    if zerr_array.shape == z_array.shape:
+                                        self.pterr = np.zeros_like(self.pt)
+                                        for idx_f in range(len(z_array)):
+                                            self.pt[idx_f], self.pterr[idx_f] = z2pt(z_array[idx_f], zerr_array[idx_f] )
+
+                            else:
+                                for idx_f in range(len(z_array)):
+                                    self.pt[idx_f] = z2pt( z_array[idx_f])[0]
+                                    
                         except:
-                            pass
+                            self.pt = None
 
             except:
                 pass
@@ -237,7 +239,6 @@ def z2pt(z_array, zerr_array = None):
     """
     
     """
-
     try:
         if not  len(z_array.shape) in [2,3]:
             raise
@@ -247,6 +248,7 @@ def z2pt(z_array, zerr_array = None):
             raise
     except:
         raise MTexceptions.MTpyError_PT('Error - incorrect z array: %s;%s instead of (N,2,2);complex'%(str(z_array.shape), str(z_array.dtype)))
+    
 
     if zerr_array is not None:
         try:
@@ -254,18 +256,15 @@ def z2pt(z_array, zerr_array = None):
                 raise
             if not zerr_array.shape[-2:] == (2,2):
                 raise
-            if not z_array.dtype in ['float']:
+            if not zerr_array.dtype in ['float']:
                 raise
         except:
             raise MTexceptions.MTpyError_PT('Error - incorrect z-err-array: %s;%s instead of (N,2,2);real'%(str(zerr_array.shape), str(zerr_array.dtype)))
 
         if not z_array.shape == zerr_array.shape:
             raise MTexceptions.MTpyError_PT('Error - z-array and z-err-array have different shape: %s;%s'%(str(z_array.shape), str(zerr_array.shape)))
-
-
     #for a single matrix as input:
     if len(z_array.shape) == 2:
-        
         pt_array = np.zeros((2,2))
 
         realz = np.real(z_array)
@@ -274,15 +273,16 @@ def z2pt(z_array, zerr_array = None):
         if detreal == 0 :
             raise MTexceptions.MTpyError_PT('Error - z-array contains a singular matrix, thus it cannot be converted into a PT!' )
 
-        pt_array[0,0] =  realz[1,1] * immagz[0,0] - realz[0,1] * immagz[1,0] 
-        pt_array[0,1] =  realz[1,1] * immagz[0,1] - realz[0,1] * immagz[1,1] 
-        pt_array[1,0] =  realz[0,0] * immagz[1,0] - realz[1,0] * immagz[0,0] 
-        pt_array[1,1] =  realz[0,0] * immagz[1,1] - realz[1,0] * immagz[0,1] 
+
+        pt_array[0,0] =  realz[1,1] * imagz[0,0] - realz[0,1] * imagz[1,0] 
+        pt_array[0,1] =  realz[1,1] * imagz[0,1] - realz[0,1] * imagz[1,1] 
+        pt_array[1,0] =  realz[0,0] * imagz[1,0] - realz[1,0] * imagz[0,0] 
+        pt_array[1,1] =  realz[0,0] * imagz[1,1] - realz[1,0] * imagz[0,1] 
 
         pt_array /= detreal
 
         if zerr_array is None:
-            return pt_array, pterr_array
+            return pt_array, None
 
         pterr_array = np.zeros_like(pt_array)
         pterr_array[0,0] = 1/detreal * (np.abs( -pt_array[0,0] * realz[1,1] * zerr_array[0,0]) + \

mtpy/core/edi.py
@@ -68,11 +68,8 @@ import numpy as np
 import os
 import sys
 import os.path as op
-import math
+import math, cmath
 import time, calendar 
-
-import mtpy.core.z as MTz 
-reload (MTz)
 
 import mtpy.utils.format as MTformat
 reload(MTformat)
@@ -90,6 +87,9 @@ class Edi(object):
         Edi class - generates an edi-object.
 
         Methods  include reading and writing from and to edi-files, rotations/combinations of edi-files, as well as 'get' and 'set' for all edi file sections
+
+        Errors are given as standard deviations (sqrt(VAR))
+
 
     """
 
@@ -236,7 +236,8 @@ class Edi(object):
                 elif idx_zentry == 1:
                     new_z_dict[section] = list(np.imag(z_array[:,idx_comp/2, idx_comp%2]))
                 elif idx_zentry == 2:
-                    new_z_dict[section] = list(zerr_array[:,idx_comp/2, idx_comp%2])
+                    #squaring the errors (stddev) to get VAR values
+                    new_z_dict[section] = list( (zerr_array[:,idx_comp/2, idx_comp%2])**2 ) 
 
         return new_z_dict
 
@@ -259,7 +260,8 @@ class Edi(object):
                 elif idx_tentry == 1:
                     new_t_dict[section] = list(np.imag(t_array[:,idx_comp/2, idx_comp%2]))
                 elif idx_tentry == 2:
-                    new_t_dict[section] = list(terr_array[:,idx_comp/2, idx_comp%2])
+                    #square errors (stddev) to get VAR values
+                    new_t_dict[section] = list( (terr_array[:,idx_comp/2, idx_comp%2])**2)
 
 
         return new_t_dict
@@ -446,7 +448,7 @@ class Edi(object):
 
     def _read_z(self, edistring):
         """
-        Read in impedances information. 
+        Read in impedances information from a string read from an EDI file. 
         Store it as dictionary and complex array (incl. Zvar values in 'zerr' array)
 
         """
@@ -456,6 +458,7 @@ class Edi(object):
 
         z_array = np.zeros((self.n_freqs(),2,2),dtype=np.complex)
         zerr_array = np.zeros((self.n_freqs(),2,2),dtype=np.float)
+        z_dict = {}
 
         for idx_comp,comp in enumerate(compstrings):
             for idx_zentry,zentry in enumerate(Z_entries):
@@ -463,7 +466,7 @@ class Edi(object):
                 try:
                     temp_string = _cut_sectionstring(edistring,sectionhead)
                 except:
-                    pass
+                    continue
   
                 lo_z_vals = []
 
@@ -488,19 +491,20 @@ class Edi(object):
 
         for idx_freq  in range( self.n_freqs()):
             z_array[idx_freq,0,0] = np.complex(z_dict['ZXXR'][idx_freq], z_dict['ZXXI'][idx_freq])
-            zerr_array[idx_freq,0,0] = z_dict['ZXX.VAR'][idx_freq]
-
             z_array[idx_freq,0,1] = np.complex(z_dict['ZXYR'][idx_freq], z_dict['ZXYI'][idx_freq])
-            zerr_array[idx_freq,0,1] = z_dict['ZXY.VAR'][idx_freq]
-
             z_array[idx_freq,1,0] = np.complex(z_dict['ZYXR'][idx_freq], z_dict['ZYXI'][idx_freq])
-            zerr_array[idx_freq,1,0] = z_dict['ZYX.VAR'][idx_freq]
-
             z_array[idx_freq,1,1] = np.complex(z_dict['ZYYR'][idx_freq], z_dict['ZYYI'][idx_freq])
-            zerr_array[idx_freq,1,1] = z_dict['ZYY.VAR'][idx_freq]
+
+            for idx_comp,comp in enumerate(compstrings):
+                sectionhead = comp + '.VAR'
+                if sectionhead in z_dict:
+                    zerr_array[idx_freq, idx_comp/2, idx_comp%2] = z_dict[sectionhead][idx_freq]
+
 
         self.z = z_array
-        self.zerr = zerr_array
+
+        #errors are stddev, not VAR :
+        self.zerr = np.sqrt(zerr_array)
 
 
     def _read_tipper(self, edistring):
@@ -556,7 +560,8 @@ class Edi(object):
 
 
         self.tipper = tipper_array
-        self.tippererr = tippererr_array
+        #errors are stddev, not VAR :  
+        self.tippererr = np.sqrt(tippererr_array)
 
 
 
@@ -599,6 +604,7 @@ class Edi(object):
         outstring, stationname = _generate_edifile_string(self.edi_dict())
 
         if not _validate_edifile_string(outstring):
+            return outstring
             raise MTexceptions.MTpyError_edi_file('Cannot write EDI file...output string is invalid')
 
 
@@ -636,8 +642,11 @@ class Edi(object):
 
 
     def z2resphase(self):
+        import mtpy.core.z as MTz 
         
         amplitude, phase = MTz.res2phase(self.z)
+
+        del MTz
 
         return amplitude, phase
 
@@ -658,10 +667,13 @@ class Edi(object):
         n_freqs = self.n_freqs()
         lo_original_angles = self.zrot
         z = self.z
-        tipper = self.tipper
         zerr = self.zerr
+
+        tipper = self.tipper
         tippererr = self.tippererr
+        
         angle = angle%360
+
 
         #1. rotation positive in clockwise direction
         #2. orientation of new X axis X' given by rotation angle
@@ -679,9 +691,11 @@ class Edi(object):
         # That is NOT the same as the rotated error matrix Zerr (although the result is similar)
 
         z_rot = z.copy()
-        zerr_rot = zerr.copy()
-        tipper_rot = tipper.copy()
-        tippererr_rot = tippererr.copy()
+
+        zerr_rot = np.copy(zerr)
+
+        tipper_rot = np.copy(tipper)
+        tippererr_rot = np.copy(tippererr)
 
         for idx_freq in range(self.n_freqs()):
 
@@ -697,8 +711,7 @@ class Edi(object):
             z_rot[idx_freq,1,0] = cphi**2 * z_orig[1,0] + cphi*sphi*(z_orig[1,1]-z_orig[0,0]) - sphi**2 * z_orig[0,1]
             z_rot[idx_freq,1,1] = cphi**2 * z_orig[1,1] + cphi*sphi*(-z_orig[0,1]-z_orig[1,0]) + sphi**2 * z_orig[0,0]
 
-            zerr_orig = zerr[idx_freq,:,:]
-            
+           
             # squared propagation of errors
             # zerr_rot[idx_freq,0,0] = np.sqrt( (cphi**2 * zerr_orig[0,0])**2 + cphi**2 * sphi**2 * ( (zerr_orig[0,1])**2 + (zerr_orig[1,0])**2) + (sphi**2 * zerr_orig[1,1])**2)
 
@@ -708,26 +721,30 @@ class Edi(object):
 
             # zerr_rot[idx_freq,1,1] = np.sqrt( (sphi**2 * zerr_orig[0,0])**2 + cphi**2 * sphi**2 * ( (zerr_orig[0,1])**2 + (zerr_orig[1,0])**2) + (cphi**2 * zerr_orig[1,1])**2) 
 
-            #absolute propagation of errors
-            zerr_rot[idx_freq,0,0] = cphi**2 * zerr_orig[0,0] + np.abs(cphi * sphi) * (zerr_orig[0,1] + zerr_orig[1,0]) + sphi**2 * zerr_orig[1,1]
+            if zerr is not None:
+                zerr_orig = zerr[idx_freq,:,:]
+ 
+                #absolute propagation of errors
+                zerr_rot[idx_freq,0,0] = cphi**2 * zerr_orig[0,0] + np.abs(cphi * sphi) * (zerr_orig[0,1] + zerr_orig[1,0]) + sphi**2 * zerr_orig[1,1]
 
-            zerr_rot[idx_freq,0,1] = cphi**2 * zerr_orig[0,1] + np.abs(cphi * sphi) * (zerr_orig[1,1] + zerr_orig[0,0]) + sphi**2 * zerr_orig[1,0] 
+                zerr_rot[idx_freq,0,1] = cphi**2 * zerr_orig[0,1] + np.abs(cphi * sphi) * (zerr_orig[1,1] + zerr_orig[0,0]) + sphi**2 * zerr_orig[1,0] 
 
-            zerr_rot[idx_freq,1,0] = cphi**2 * zerr_orig[1,0] + np.abs(cphi * sphi) * (zerr_orig[1,1] + zerr_orig[0,0]) + sphi**2 * zerr_orig[0,1] 
+                zerr_rot[idx_freq,1,0] = cphi**2 * zerr_orig[1,0] + np.abs(cphi * sphi) * (zerr_orig[1,1] + zerr_orig[0,0]) + sphi**2 * zerr_orig[0,1] 
 
-            zerr_rot[idx_freq,1,1] = cphi**2 * zerr_orig[1,1] + np.abs(cphi * sphi) * (zerr_orig[0,1] + zerr_orig[1,0]) + sphi**2 * zerr_orig[0,0] 
+                zerr_rot[idx_freq,1,1] = cphi**2 * zerr_orig[1,1] + np.abs(cphi * sphi) * (zerr_orig[0,1] + zerr_orig[1,0]) + sphi**2 * zerr_orig[0,0] 
 
+            if tipper is not None:
+                t_orig = tipper[idx_freq,:,:]
 
-            t_orig = tipper[idx_freq,:,:]
+                tipper_rot[idx_freq,0,0] =  cphi * t_orig[0,0] + sphi * t_orig[0,1]
+                tipper_rot[idx_freq,0,1] = -sphi * t_orig[0,0] + cphi * t_orig[0,1]
 
-            tipper_rot[idx_freq,0,0] =  cphi * t_orig[0,0] + sphi * t_orig[0,1]
-            tipper_rot[idx_freq,0,1] = -sphi * t_orig[0,0] + cphi * t_orig[0,1]
+            if tippererr is not None:
+                #absolute error propagation
+                terr_orig = tippererr[idx_freq,:,:]
 
-            #absolute error propagation
-            terr_orig = tippererr[idx_freq,:,:]
-
-            tippererr_rot[idx_freq,0,0] = np.abs(cphi * terr_orig[0,0])  + np.abs(sphi * terr_orig[0,1])
-            tippererr_rot[idx_freq,0,1] = np.abs(-sphi * terr_orig[0,0]) + np.abs(cphi * terr_orig[0,1])
+                tippererr_rot[idx_freq,0,0] = np.abs(cphi * terr_orig[0,0])  + np.abs(sphi * terr_orig[0,1])
+                tippererr_rot[idx_freq,0,1] = np.abs(-sphi * terr_orig[0,0]) + np.abs(cphi * terr_orig[0,1])
  
 
         self.z = z_rot
@@ -744,11 +761,19 @@ class Edi(object):
             return
 
         rho = np.zeros(self.z.shape)
+        rhoerr = np.zeros_like(rho)
 
         for idx_f in range(len(rho)):                         
             rho[idx_f,:,:] = np.abs(self.z[idx_f,:,:])
+        
+        if self.zerr is None:
+            return rho, rhoerr
 
-        return rho
+        #error of rho depends on the phase of z as well: 
+        # z_err is the same for imag and real, therefore like a square around the value of z. Hence if z is purely real or purely imaginary, the error of rho is the same as z_err, otherwise, the amplitude is crossing this square diagonally, therefor increasing the error by up to a maximum factor of sqrt(2):
+
+        phi
+
 
     def phi(self):
         if self.z is None:
@@ -756,13 +781,14 @@ class Edi(object):
             return
 
         phi = np.zeros(self.z.shape)
+        phierr = np.zeros_like(phi)
         
         for idx_f in range(len(phi)):
             for i in range(2):
                 for j in range(2):
                     phi[idx_f,i,j] = math.degrees(cmath.phase(self.z[idx_f,i,j]))
 
-        return phi
+        return phi, phierr
 
 
     def set_rho_phi(self, rho_array, phi_array):

mtpy/core/z.py
@@ -73,14 +73,11 @@ import time, calendar
 import mtpy.utils.calculator as MTc
 
 
-import mtpy.core.edi as MTedi 
-
 import mtpy.utils.format as MTformat
 
 import mtpy.utils.exceptions as MTexceptions
 
 reload(MTexceptions)
-reload (MTedi)
 reload(MTformat)
 reload(MTc)
 
@@ -101,11 +98,14 @@ class Z(object):
         with indices in the following order: 
             Zxx: (0,0) - Zxy: (0,1) - Zyx: (1,0) - Zyy: (1,1)   
 
+        Errors are given as standard deviations (sqrt(VAR))
 
     """
 
     def __init__(self, z_array = None, zerr_array = None, edi_object = None):
     
+
+        import mtpy.core.edi as MTedi 
 
         self.z = None
         self.zerr = None
@@ -160,6 +160,7 @@ class Z(object):
             self.zerr = np.zeros(self.z.shape)
 
         self.rotation_angle = 0.
+
 
     def set_edi_object(self, edi_object):
 
@@ -273,32 +274,34 @@ class Z(object):
         self.z = z_new
 
 
-    def rho(self):
+    def rho_phi(self):
+        
         if self.z is None:
-            print 'z array is None - cannot calculate rho'
+            print 'Z array is None - cannot calculate rho/phi'
             return
+        rhoerr = None
+        phierr = None
+        if self.zerr is not None:
+            rhoerr = np.zeros(self.zerr.shape)
+            phierr = np.zeros(self.zerr.shape)
 
         rho = np.zeros(self.z.shape)
-
-        for idx_f in range(len(rho)):                         
-            rho[idx_f,:,:] = np.abs(self.z[idx_f,:,:])
-
-        return rho
-
-        
-    def phi(self):
-        if self.z is None:
-            print 'z array is None - cannot calculate phi'
-            return
-
         phi = np.zeros(self.z.shape)
-        
-        for idx_f in range(len(phi)):
-            for i in range(2):
-                for j in range(2):
-                    phi[idx_f,i,j] = math.degrees(cmath.phase(self.z[idx_f,i,j]))
 
-        return phi
+
+        for idx_f in range(len(self.z)): 
+            for i in range(2):                        
+                for j in range(2):
+                    rho[idx_f,i,j] = np.abs(self.z[idx_f,i,j])
+                    phi[idx_f,i,j] = math.degrees(cmath.phase(self.z[idx_f,i,j]))
+                
+                    if self.zerr is not None:
+                        r_err, phi_err = MTc.propagate_error_rect2polar( np.real(self.z[idx_f,i,j]), self.zerr[idx_f,i,j], np.imag(self.z[idx_f,i,j]), self.zerr[idx_f,i,j])
+                        rhoerr[idx_f,i,j] = r_err
+                        phierr[idx_f,i,j] = phi_err
+
+        return rho, phi, rhoerr, phierr
+
 
 
     def set_rho_phi(self, rho_array, phi_array):
@@ -430,7 +433,7 @@ class Z(object):
     def no_ss(self, reduce_rho_factor_x = 1., reduce_rho_factor_y = 1.):
         """
         Remove the static shift by providing the correction factors for x and y components.
-        (Factor can be dteremined by using the "Analysis" module for the impedance tensor)
+        (Factor can be determined by using the "Analysis" module for the impedance tensor)
 
         Assume the original observed tensor Z is built by a static shift S and an unperturbated "correct" Z0 :
             Z = S * Z0
@@ -551,27 +554,7 @@ class Z(object):
         #todo :include error on  determinant!!
         D_det = np.linalg.det(distortion_tensor)
 
-
-
-        DI_err[0,0] = np.abs(-1./(distortion_tensor[0,0])**2 * distortion_err_tensor[0,0]) +\
-                    np.abs(1./(distortion_tensor[0,1])**2 * distortion_err_tensor[0,1]) +\
-                    np.abs(-1./(distortion_tensor[1,0])**2 * distortion_err_tensor[1,0]) +\
-                    np.abs( 1./D_det * (1. - distortion_tensor[0,0] * DI[0,0]) * distortion_err_tensor[1,1] )
-
-        DI_err[0,1] = np.abs(1./(distortion_tensor[0,0])**2 * distortion_err_tensor[0,0]) +\
-                    np.abs(-1./D_det * (1. - distortion_tensor[1,0] * DI[0,1]) * distortion_err_tensor[0,1] ) +\
-                    np.abs(1./(distortion_tensor[1,0])**2 * distortion_err_tensor[1,0]) +\
-                    np.abs(-1./(distortion_tensor[1,1])**2 * distortion_err_tensor[1,1])
-
-        DI_err[1,0] = np.abs(1./(distortion_tensor[0,0])**2 * distortion_err_tensor[0,0]) +\
-                    np.abs(1./(distortion_tensor[0,1])**2 * distortion_err_tensor[0,1]) +\
-                    np.abs(-1./D_det * (1. - distortion_tensor[0,1] * DI[1,0]) * distortion_err_tensor[1,0] ) +\
-                    np.abs(-1./(distortion_tensor[1,1])**2 * distortion_err_tensor[1,1])
-
-        DI_err[1,1] = np.abs( 1./D_det * (1. - distortion_tensor[1,1] * DI[1,1]) * distortion_err_tensor[0,0] ) +\
-                    np.abs(1./(distortion_tensor[0,1])**2 * distortion_err_tensor[0,1]) +\
-                    np.abs(-1./(distortion_tensor[1,0])**2 * distortion_err_tensor[1,0]) +\
-                    np.abs(-1./(distortion_tensor[1,1])**2 * distortion_err_tensor[1,1]) 
+        dummy, DI_err = MTc.invertmatrix_incl_errors_real(distortion_tensor, distortion_err_tensor)
 
         #propagation of errors - step 2 - product of D.inverse and Z; D.I * Z, making it 4 summands for each component:
         z_corrected = np.zeros_like(self.z)
@@ -653,6 +636,7 @@ class Tipper(object):
         Tipper class - generates a Tipper-object.
 
 
+        Errors are given as standard deviations (sqrt(VAR))
 
     """
 
@@ -801,32 +785,32 @@ class Tipper(object):
         self.tipper = tipper_new
 
 
-    def rho(self):
+    def rho_phi(self):
         
         if self.tipper is None:
-            print 'tipper array is None - cannot calculate rho'
+            print 'tipper array is None - cannot calculate rho/phi'
             return
+        rhoerr = None
+        phierr = None
+        if self.tippererr is not None:
+            rhoerr = np.zeros(self.tippererr.shape)
+            phierr = np.zeros(self.tippererr.shape)
 
         rho = np.zeros(self.tipper.shape)
-
-        for idx_f in range(len(rho)):                         
-            rho[idx_f,:,:] = np.abs(self.tipper[idx_f,:,:])
-
-        return rho
-
-        
-    def phi(self):
-        if self.tipper is None:
-            print 'tipper array is None - cannot calculate phi'
-            return
-
         phi = np.zeros(self.tipper.shape)
-        
-        for idx_f in range(len(phi)):
-                for j in range(2):
-                    phi[idx_f,0,j] = math.degrees(cmath.phase(self.tipper[idx_f,0,j]))
 
-        return phi
+
+        for idx_f in range(len(tipper)):                         
+            for j in range(2):
+                rho[idx_f,0,j] = np.abs(self.tipper[idx_f,0,j])
+                phi[idx_f,0,j] = math.degrees(cmath.phase(self.tipper[idx_f,0,j]))
+                
+                if self.tippererr is not None:
+                    r_err, phi_err = MTc.propagate_error_rect2polar( np.real(self.tipper[idx_f,0,j]), self.tippererr[idx_f,0,j], np.imag(self.tipper[idx_f,0,j]), self.tippererr[idx_f,0,j])
+                    rhoerr[idx_f,0,j] = r_err
+                    phierr[idx_f,0,j] = phi_err
+
+        return rho, phi, rhoerr, phierr
 
 
     def set_rho_phi(self, rho_array, phi_array):
@@ -978,7 +962,7 @@ def z2rhophi(z_array):
     
     z_object = _read_z_array(z_array)
 
-    return z_object.rho(), z_object.phi()
+    return z_object.rho_phi()
 
 
 def rotate_tipper(tipper_array, alpha, tippererr_array = None):
@@ -989,6 +973,12 @@ def rotate_tipper(tipper_array, alpha, tippererr_array = None):
 
     return tipper_object.tipper, tipper_object.tippererr
 
+
+def tipper2rhophi(tipper_array):
+    
+    tipper_object = _read_tipper_array(tipper_array)
+
+    return tipper_object.rho_phi()
 
 
 def _read_z_array(z_array, zerr_array = None):

mtpy/utils/calculator.py
@@ -21,6 +21,7 @@ import glob
 import calendar
 import time
 import ConfigParser
+import math, cmath
 
 from mtpy.utils.exceptions import *
 import mtpy.utils.format as MTformat
@@ -35,44 +36,73 @@ epsilon = 1e-9
 
 def invertmatrix_incl_errors(inmatrix, inmatrix_err):
 
-        if inmatrix is None or inmatrix_err is None:
-            raise MTexceptions.MTpyError_inputarguments('Matrix AND eror matrix must be defined')
+    if inmatrix is None or inmatrix_err is None:
+        raise MTexceptions.MTpyError_inputarguments('Matrix AND eror matrix must be defined')
 
-        if inmatrix.shape != inmatrix_err.shape:
-            raise MTexceptions.MTpyError_inputarguments('Matrix and err-matrix shapes do not match: %s - %s'%(str(inmatrix.shape), str(inmatrix_err.shape)))
+    if inmatrix.shape != inmatrix_err.shape:
+        raise MTexceptions.MTpyError_inputarguments('Matrix and err-matrix shapes do not match: %s - %s'%(str(inmatrix.shape), str(inmatrix_err.shape)))
 
-        if (inmatrix.shape[-2] != inmatrix.shape[-1]) or (inmatrix_err.shape[-2] != inmatrix_err.shape[-1]) :
-            raise MTexceptions.MTpyError_inputarguments('Matrices must be square!')
+    if (inmatrix.shape[-2] != inmatrix.shape[-1]) or (inmatrix_err.shape[-2] != inmatrix_err.shape[-1]) :
+        raise MTexceptions.MTpyError_inputarguments('Matrices must be square!')
 
-        dim = inmatrix.shape[-1]
+    dim = inmatrix.shape[-1]
 
-        inmatrix_err = np.real(inmatrix_err)
+    inmatrix_err = np.real(inmatrix_err)
 
-        det = np.linalg.det(inmatrix)
+    det = np.linalg.det(inmatrix)
 
-        if det == 0:
-            raise MTexceptions.MTpyError_inputarguments('Matrix is singular - I cannot inv ert that!')
+    if det == 0:
+        raise MTexceptions.MTpyError_inputarguments('Matrix is singular - I cannot invert that!')
 
-        inv_matrix = np.zeros_like(inmatrix)
-        inv_matrix_err = np.zeros_like(inmatrix_err)
+    inv_matrix = np.zeros_like(inmatrix)
+    inv_matrix_err = np.zeros_like(inmatrix_err)
 
-        if dim != 2:
-            raise MTexceptions.MTpyError_inputarguments('Only 2D matrices supported yet')
+    if dim != 2:
+        raise MTexceptions.MTpyError_inputarguments('Only 2D matrices supported yet')
 
-        inv_matrix = np.linalg.inv(inmatrix)
+    inv_matrix = np.linalg.inv(inmatrix)
 
-        for i in range(2):
-            for j in range(2):
-                #looping over the entries of the error matrix
-                err = 0.
-                for k in range(2):
-                    for l in range(2):
-                        #each entry has 4 summands
+    for i in range(2):
+        for j in range(2):
+            #looping over the entries of the error matrix
+            err = 0.
+            for k in range(2):
+                for l in range(2):
+                    #each entry has 4 summands
 
-                        err += np.abs (- inv_matrix[i,k]  * inv_matrix[l,j]  * inmatrix_err[k,l])
+                    err += np.abs (- inv_matrix[i,k]  * inv_matrix[l,j]  * inmatrix_err[k,l])
 
 
-                inv_matrix_err[i,j] = err
+            inv_matrix_err[i,j] = err
 
  
     return inv_matrix, inv_matrix_err
+
+
+def propagate_error_rect2polar(x,x_error,y, y_error):
+    
+    # x_error, y_error define a  rectangular uncertainty box  
+    
+    # rho error is the difference between the closest and furthest point of the box (w.r.t. the origin)
+    # approximation: just take corners and midpoint of edges 
+    lo_points = [ (x + x_error, y), (x - x_error, y), (x, y - y_error ), (x, y - y_error ),\
+                  (x - x_error, y - y_error) ,(x + x_error, y - y_error) ,(x + x_error, y + y_error) ,(x - x_error, y + y_error) ]
+
+
+    #check, if origin is within the box:
+    origin_in_box = False
+    if x_error >= np.abs(x) and y_error >= np.abs(y):
+        origin_in_box = True
+
+    lo_polar_points = [ cmath.polar(np.complex(*i)) for i in lo_points ]
+    lo_rho = [i[0] for i in lo_polar_points ]
+    lo_phi = [i[1] for i in lo_polar_points ]
+
+    rho_err = lo_rho.max() 
+    phi_err = math.degrees(  lo_phi.max() - lo_phi.min() )
+
+    if origin_in_box is False:
+        rho_err -= lo_rho.min()
+        phi_err = 360.
+
+    return rho_error, phi_error