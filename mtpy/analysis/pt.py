rotation of matrices and vectors incl. error propagation in calculator.py done (and corrected)
Signed-off-by: LK <LK@uni>

mtpy/analysis/pt.py
@@ -32,11 +32,13 @@ import mtpy.core.edi as MTedi
 import mtpy.core.z as MTz 
 import mtpy.utils.format as MTformat
 import mtpy.utils.exceptions as MTexceptions
+import mtpy.utils.calculator as MTc
 
 reload(MTexceptions)
 reload(MTedi)
 reload(MTz)
 reload(MTformat)
+reload(MTc)
 
 
 #=================================================================
@@ -270,10 +272,60 @@ class PhaseTensor(object):
         return phimax, phimaxerr
 
 
-    def rotate(self,angle):
+    def rotate(self,alpha):
 
-        pass
+        if self.pt is None :
+            print 'pt-array is "None" - I cannot rotate that'
+            return
 
+        #check for iterable list/set of angles - if so, it must have length 1 or same as len(tipper):
+        if np.iterable(alpha) == 0:
+            try:
+                degreeangle = float(alpha%360)
+            except:
+                print '"Angle" must be a valid number (in degrees)'
+                return
+
+            #make an n long list of identical angles
+            lo_angles = [degreeangle for i in self.pt]
+        else:
+            if len(lo_angles) == 1:
+                try:
+                    degreeangle = float(alpha%360)
+                except:
+                    print '"Angle" must be a valid number (in degrees)'
+                    return
+                #make an n long list of identical angles
+                lo_angles = [degreeangle for i in self.pt]
+            else:                    
+                try:
+                    lo_angles = [ float(i%360) for i in alpha]
+                except:
+                    print '"Angles" must be valid numbers (in degrees)'
+                    return
+            
+        self.rotation_angle = lo_angles
+
+        if len(lo_angles) != len(self.pt):
+            print 'Wrong number Number of "angles" - need %i '%(len(self.pt))
+            self.rotation_angle = 0.
+            return
+        
+        pt_rot = np.copy(self.pt)
+        pterr_rot = np.copy(self.pterr)
+       
+        for idx_freq in range(len(self.pt)):
+                    
+            angle = lo_angles[idx_freq]
+
+            if self.pterr is not None:
+                pt_rot[idx_freq], pterr_rot[idx_freq] = MTc.rotatematrix_incl_errors(self.pt[idx_freq,:,:], angle, self.pterr[idx_freq,:,:])
+            else:
+                pt_rot[idx_freq], pterr_rot = MTc.rotatematrix_incl_errors(self.pt[idx_freq,:,:], angle)
+
+        
+        self.pt = pt_rot
+        self.pterr = pterr_rot
 
 
 def z2pt(z_array, zerr_array = None):

mtpy/core/edi.py
@@ -519,7 +519,7 @@ class Edi(object):
 
         for idx_comp,comp in enumerate(compstrings):
             for idx_tentry,tentry in enumerate(T_entries):
-
+                temp_string = None
                 try:
                     sectionhead = comp + tentry + '.EXP'
                     temp_string = _cut_sectionstring(edistring,sectionhead)
@@ -528,13 +528,21 @@ class Edi(object):
                         sectionhead = comp + tentry
                         temp_string = _cut_sectionstring(edistring,sectionhead)
                     except:
+                        # if tipper is given with sectionhead "TX.VAR"
+                        if (idx_tentry == 2) and (temp_string is None):
+                            try:
+                                sectionhead = comp + '.' + tentry
+                                temp_string = _cut_sectionstring(edistring,sectionhead)
+                            except:
+                                pass
                         pass
-  
+          
                 lo_t_vals = []
                 
                 #check, if correct number of entries are given in the block
                 t0 = temp_string.strip().split('\n')[0]
                 n_dummy = int(float(t0.split('//')[1].strip()))
+
                 if not n_dummy == self.n_freqs():
                     raise
 
@@ -553,10 +561,8 @@ class Edi(object):
         for idx_freq  in range( self.n_freqs()):
             tipper_array[idx_freq,0,0] = np.complex(t_dict['TXR'][idx_freq], t_dict['TXI'][idx_freq])
             tippererr_array[idx_freq,0,0] = t_dict['TXVAR'][idx_freq]
-
             tipper_array[idx_freq,0,1] = np.complex(t_dict['TYR'][idx_freq], t_dict['TYI'][idx_freq])
-            tippererr_array[idx_freq,0,1] = self.t_dict['TYVAR'][idx_freq]
-
+            tippererr_array[idx_freq,0,1] = t_dict['TYVAR'][idx_freq]
 
 
         self.tipper = tipper_array
@@ -667,21 +673,25 @@ class Edi(object):
         
         angle = angle%360
 
+        z_rot = np.copy(self.z)
+        zerr_rot = np.copy(self.zerr)
+        tipper_rot = np.copy(self.tipper)
+        tippererr_rot = np.copy(self.tippererr)
 
         for idx_freq in range(self.n_freqs()):
 
             if self.zerr is not None:
-                z_rot, zerr_rot = MTc.rotatematrix_incl_errors(self.z[idx_freq,:,:], angle, self.zerr[idx_freq,:,:])
+                z_rot[idx_freq], zerr_rot[idx_freq] = MTc.rotatematrix_incl_errors(self.z[idx_freq,:,:], angle, self.zerr[idx_freq,:,:])
             else:
-                z_rot, zerr_rot = MTc.rotatematrix_incl_errors(self.z[idx_freq,:,:], angle)
+                z_rot[idx_freq], zerr_rot = MTc.rotatematrix_incl_errors(self.z[idx_freq,:,:], angle)
   
 
             if tipper is not None:
 
                 if self.tippererr is not None:
-                    tipper_rot, tippererr_rot = MTc.rotatevector_incl_errors(self.tipper[idx_freq,:,:], angle,self.tippererr[idx_freq,:,:] )
+                    tipper_rot[idx_freq], tippererr_rot[idx_freq] = MTc.rotatevector_incl_errors(self.tipper[idx_freq,:,:], angle,self.tippererr[idx_freq,:,:] )
                 else:
-                    tipper_rot, tippererr_rot = MTc.rotatevector_incl_errors(self.tipper[idx_freq,:,:], angle)
+                    tipper_rot[idx_freq], tippererr_rot = MTc.rotatevector_incl_errors(self.tipper[idx_freq,:,:], angle)
 
 
 
@@ -870,17 +880,17 @@ class Edi(object):
         
     
     def set_zrot(self, angle):
-        if isiterable(angle):
+        if np.iterable(angle):
             if len(angle) is not len(self.z):
                 print 'length of angle list not correct (%i instead of %i)'%(len(angle), len(self.z))
                 return
             try:
-                angle = [i%360 for i in angle]
+                angle = [float(i%360) for i in angle]
             except:
                 raise MTexceptions.MTpyError_edi_file('list of angles contains non-numercal values')
         else:
             try:
-                angle = [angle%360 for i in self.z]
+                angle = [float(angle%360) for i in self.z]
             except:
                 raise MTexceptions.MTpyError_edi_file('Angles is a non-numercal value')                
 

mtpy/core/z.py
@@ -360,48 +360,49 @@ class Z(object):
             return
 
         #check for iterable list/set of angles - if so, it must have length 1 or same as len(tipper):
-        if iterable(alpha) == 0:
+        if np.iterable(alpha) == 0:
             try:
-                degreeangle = alpha%360
+                degreeangle = float(alpha%360)
             except:
                 print '"Angle" must be a valid number (in degrees)'
                 return
 
-            self.rotation_angle = degreeangle
             #make an n long list of identical angles
             lo_angles = [degreeangle for i in self.z]
         else:
             if len(lo_angles) == 1:
                 try:
-                    degreeangle = alpha%360
+                    degreeangle = float(alpha%360)
                 except:
                     print '"Angle" must be a valid number (in degrees)'
                     return
-                self.rotation_angle = degreeangle
                 #make an n long list of identical angles
                 lo_angles = [degreeangle for i in self.z]
             else:                    
                 try:
-                    lo_angles = [ i%360 for i in alpha]
+                    lo_angles = [ float(i%360) for i in alpha]
                 except:
                     print '"Angles" must be valid numbers (in degrees)'
                     return
             
-            self.rotation_angle = lo_angles
+        self.rotation_angle = lo_angles
 
         if len(lo_angles) != len(self.z):
             print 'Wrong number Number of "angles" - need %i '%(len(self.z))
-            self.rotation_angle = 0.
+            #self.rotation_angle = 0.
             return
+
+        z_rot = np.copy(self.z)
+        zerr_rot = np.copy(self.zerr)
 
         for idx_freq in range(len(self.zz)):
                     
             angle = lo_angles[idx_freq]
 
             if self.zerr is not None:
-                z_rot, zerr_rot = MTc.rotatematrix_incl_errors(self.z[idx_freq,:,:], angle, self.zerr[idx_freq,:,:])
+                z_rot[idx_freq], zerr_rot[idx_freq] = MTc.rotatematrix_incl_errors(self.z[idx_freq,:,:], angle, self.zerr[idx_freq,:,:])
             else:
-                z_rot, zerr_rot = MTc.rotatematrix_incl_errors(self.z[idx_freq,:,:], angle)
+                z_rot[idx_freq], zerr_rot = MTc.rotatematrix_incl_errors(self.z[idx_freq,:,:], angle)
 
 
         self.z = z_rot
@@ -423,7 +424,7 @@ class Z(object):
         """
         
         #check for iterable list/set of reduce_rho_factor_x - if so, it must have length 1 or same as len(z):
-        if iterable(reduce_rho_factor_x) == 0:
+        if np.iterable(reduce_rho_factor_x) == 0:
             try:
                 x_factor = float(reduce_rho_factor_x)
             except:
@@ -451,7 +452,7 @@ class Z(object):
             return
   
         #check for iterable list/set of reduce_rho_factor_y - if so, it must have length 1 or same as len(z):
-        if iterable(reduce_rho_factor_y) == 0:
+        if np.iterable(reduce_rho_factor_y) == 0:
             try:
                 y_factor = float(reduce_rho_factor_y)
             except:
@@ -835,48 +836,48 @@ class Tipper(object):
             return
 
         #check for iterable list/set of angles - if so, it must have length 1 or same as len(tipper):
-        if iterable(alpha) == 0:
+        if np.iterable(alpha) == 0:
             try:
-                degreeangle = alpha%360
+                degreeangle = float(alpha%360)
             except:
                 print '"Angle" must be a valid number (in degrees)'
                 return
 
-            self.rotation_angle = degreeangle
             #make an n long list of identical angles
             lo_angles = [degreeangle for i in self.tipper]
         else:
             if len(lo_angles) == 1:
                 try:
-                    degreeangle = alpha%360
+                    degreeangle = float(alpha%360)
                 except:
                     print '"Angle" must be a valid number (in degrees)'
                     return
-                self.rotation_angle = degreeangle
                 #make an n long list of identical angles
                 lo_angles = [degreeangle for i in self.z]
             else:                    
                 try:
-                    lo_angles = [ i%360 for i in alpha]
+                    lo_angles = [ float(i%360) for i in alpha]
                 except:
                     print '"Angles" must be valid numbers (in degrees)'
                     return
             
-            self.rotation_angle = lo_angles
+        self.rotation_angle = lo_angles
 
         if len(lo_angles) != len(self.tipper):
             print 'Wrong number Number of "angles" - need %i '%(len(self.tipper))
             self.rotation_angle = 0.
             return
 
-            
+        tipper_rot = np.copy(self.tipper)
+        tippererr_rot = np.copy(self.tippererr)
+
         for idx_freq in range(len(tipper_rot)):
             angle = lo_angles[idx_freq]
 
             if self.tippererr is not None:
-                tipper_rot, tippererr_rot =  MTc.rotatevector_incl_errors(self.tipper[idx_freq,:,:], angle,self.tippererr[idx_freq,:,:] )
+                tipper_rot[idx_freq], tippererr_rot[idx_freq] =  MTc.rotatevector_incl_errors(self.tipper[idx_freq,:,:], angle,self.tippererr[idx_freq,:,:] )
             else:
-                tipper_rot, tippererr_rot = MTc.rotatevector_incl_errors(self.tipper[idx_freq,:,:], angle)
+                tipper_rot[idx_freq], tippererr_rot = MTc.rotatevector_incl_errors(self.tipper[idx_freq,:,:], angle)
 
 
  

mtpy/utils/calculator.py
@@ -205,15 +205,21 @@ def rotatevector_incl_errors(invector, angle, invector_err = None):
 
     rotmat = np.matrix([[ cphi,-sphi],[sphi,cphi] ])
 
-    rotated_vector = np.dot( rotmat, invector )
+    if invector.shape == (1,2):
+        rotated_vector = np.dot( invector, rotmat.I )
+    else:
+        rotated_vector = np.dot( rotmat, invector )
+    
     
     errvec = None
     if (invector_err is not None) :   
         err_orig = np.real(invector_err)
         errvec = np.zeros_like(invector_err)
 
-        errvec[0,0] = cphi**2 * err_orig[0,0] + np.abs(cphi * sphi) * (err_orig[0,1] + err_orig[1,0]) + sphi**2 * err_orig[1,1]
-        errvec[0,1] = cphi**2 * err_orig[0,1] + np.abs(cphi * sphi) * (err_orig[1,1] + err_orig[0,0]) + sphi**2 * err_orig[1,0]
+        if invector_err.shape == (1,2):
+            errvec = np.dot( invector_err, np.abs(rotmat.I) )
+        else:
+            errvec = np.dot( np.abs(rotmat), invector_err )
 
 
     return rotated_matrix, errvec