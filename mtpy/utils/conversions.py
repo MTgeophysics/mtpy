d85f04849de99a13ad60786815ea56fc910ae635
LK <LK@uni>
2013-02-28 10:31:49 +1030

    moved UTM<->LatLon to conversions.py
    Signed-off-by: LK <LK@uni>

--- mtpy/utils/conversions.py
+++ mtpy/utils/conversions.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 
-# Lat Long - UTM, UTM - Lat Long conversions
+"""
+This modules contains functions for the conversion of certain data types.
+
+Examples:
+
+lat/lon <-> UTM
+local time <-> UTC
+meters <-> furlongs
+.
+.
+.
+
+
+@UofA, 2013
+(LK)
+
+"""
+
+#=================================================================
 
 
 from math import pi, sin, cos, tan, sqrt
 
-#LatLong- UTM conversion..h
-#definitions for lat/long to UTM and UTM to lat/lng conversions
-#include <string.h>
+
+
+#=================================================================
+# Lat Long - UTM, UTM - Lat Long conversions
+
 
 _deg2rad = pi / 180.0
 _rad2deg = 180.0 / pi
@@ -16,32 +36,32 @@ _EquatorialRadius = 2
 _eccentricitySquared = 3
 
 _ellipsoid = [
-#  id, Ellipsoid name, Equatorial Radius, square of eccentricity	
+#  id, Ellipsoid name, Equatorial Radius, square of eccentricity    
 # first once is a placeholder only, To allow array indices to match id numbers
-	[ -1, "Placeholder", 0, 0],
-	[ 1, "Airy", 6377563, 0.00667054],
-	[ 2, "Australian National", 6378160, 0.006694542],
-	[ 3, "Bessel 1841", 6377397, 0.006674372],
-	[ 4, "Bessel 1841 (Nambia] ", 6377484, 0.006674372],
-	[ 5, "Clarke 1866", 6378206, 0.006768658],
-	[ 6, "Clarke 1880", 6378249, 0.006803511],
-	[ 7, "Everest", 6377276, 0.006637847],
-	[ 8, "Fischer 1960 (Mercury] ", 6378166, 0.006693422],
-	[ 9, "Fischer 1968", 6378150, 0.006693422],
-	[ 10, "GRS 1967", 6378160, 0.006694605],
-	[ 11, "GRS 1980", 6378137, 0.00669438],
-	[ 12, "Helmert 1906", 6378200, 0.006693422],
-	[ 13, "Hough", 6378270, 0.00672267],
-	[ 14, "International", 6378388, 0.00672267],
-	[ 15, "Krassovsky", 6378245, 0.006693422],
-	[ 16, "Modified Airy", 6377340, 0.00667054],
-	[ 17, "Modified Everest", 6377304, 0.006637847],
-	[ 18, "Modified Fischer 1960", 6378155, 0.006693422],
-	[ 19, "South American 1969", 6378160, 0.006694542],
-	[ 20, "WGS 60", 6378165, 0.006693422],
-	[ 21, "WGS 66", 6378145, 0.006694542],
-	[ 22, "WGS-72", 6378135, 0.006694318],
-	[ 23, "WGS-84", 6378137, 0.00669438]
+    [ -1, "Placeholder", 0, 0],
+    [ 1, "Airy", 6377563, 0.00667054],
+    [ 2, "Australian National", 6378160, 0.006694542],
+    [ 3, "Bessel 1841", 6377397, 0.006674372],
+    [ 4, "Bessel 1841 (Nambia] ", 6377484, 0.006674372],
+    [ 5, "Clarke 1866", 6378206, 0.006768658],
+    [ 6, "Clarke 1880", 6378249, 0.006803511],
+    [ 7, "Everest", 6377276, 0.006637847],
+    [ 8, "Fischer 1960 (Mercury] ", 6378166, 0.006693422],
+    [ 9, "Fischer 1968", 6378150, 0.006693422],
+    [ 10, "GRS 1967", 6378160, 0.006694605],
+    [ 11, "GRS 1980", 6378137, 0.00669438],
+    [ 12, "Helmert 1906", 6378200, 0.006693422],
+    [ 13, "Hough", 6378270, 0.00672267],
+    [ 14, "International", 6378388, 0.00672267],
+    [ 15, "Krassovsky", 6378245, 0.006693422],
+    [ 16, "Modified Airy", 6377340, 0.00667054],
+    [ 17, "Modified Everest", 6377304, 0.006637847],
+    [ 18, "Modified Fischer 1960", 6378155, 0.006693422],
+    [ 19, "South American 1969", 6378160, 0.006694542],
+    [ 20, "WGS 60", 6378165, 0.006693422],
+    [ 21, "WGS 66", 6378145, 0.006694542],
+    [ 22, "WGS-72", 6378135, 0.006694318],
+    [ 23, "WGS-84", 6378137, 0.00669438]
 ]
 
 #Reference ellipsoids derived from Peter H. Dana's website- 
@@ -55,7 +75,7 @@ _ellipsoid = [
 #1984 Technical Report. Part I and II. Washington, DC: Defense Mapping Agency
 
 #def LLtoUTM(int ReferenceEllipsoid, const double Lat, const double Long, 
-#			 double &UTMNorthing, double &UTMEasting, char* UTMZone)
+#            double &UTMNorthing, double &UTMEasting, char* UTMZone)
 
 def LLtoUTM(ReferenceEllipsoid, Lat, Long):
     """
@@ -63,7 +83,7 @@ def LLtoUTM(ReferenceEllipsoid, Lat, Long):
     East Longitudes are positive, West longitudes are negative. 
     North latitudes are positive, South latitudes are negative
     Lat and Long are in decimal degrees
-    Written by Chuck Gantz- chuck.gantz@globalstar.com	
+    Written by Chuck Gantz- chuck.gantz@globalstar.com  
     
     Outputs:
         UTMzone, easting, northing"""
@@ -153,10 +173,10 @@ def _UTMLetterDesignator(Lat):
     elif -56 > Lat >= -64: return 'E'
     elif -64 > Lat >= -72: return 'D'
     elif -72 > Lat >= -80: return 'C'
-    else: return 'Z'	# if the Latitude is outside the UTM limits
+    else: return 'Z'    # if the Latitude is outside the UTM limits
 
 #void UTMtoLL(int ReferenceEllipsoid, const double UTMNorthing, const double UTMEasting, const char* UTMZone,
-#			  double& Lat,  double& Long )
+#             double& Lat,  double& Long )
 
 def UTMtoLL(ReferenceEllipsoid, northing, easting, zone):
     """
@@ -216,11 +236,7 @@ def UTMtoLL(ReferenceEllipsoid, northing, easting, zone):
     return (Lat, Long)
 
 
-
-if __name__ == '__main__':
-    #?????????????????????????????????
-    (z, e, n) = LLtoUTM(23, 45.00, -75.00)
-    print z, e, n
-    (lat, lon) = UTMtoLL(23, n, e, z)
-    print lat, lon
-
+#=================================================================
+# .... conversions:
+# .
+# .
