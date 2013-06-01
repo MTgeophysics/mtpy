3f299f7f9274a1a937832c704ed58a81abf23cfb
JP <peacock.jared@gmail.com>
2013-06-01 13:00:10 -0700

    adding plot res phase pseudo section

--- mtpy/imaging/plotpseudosection.py
+++ mtpy/imaging/plotpseudosection.py
@@ -9,9 +9,10 @@ Created on Thu May 30 18:39:58 2013
 
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 from matplotlib.ticker import MultipleLocator
 import mtpy.imaging.mtplottools as mtpl
+import mtpy.imaging.mtcolors as mtcl
+import matplotlib.gridspec as gridspec
 
 #==============================================================================
 
@@ -25,23 +26,32 @@ class PlotResPhasePseudoSection(object):
     def __init__(self, fn_lst=None, res_object_lst=None, z_object_lst=None, 
                  mt_object_lst=None, fignum=1, dpi=300, rot_z=0, plot_xy='y', 
                  plot_yx='y', plot_xx='n', plot_yy='n', plot_yn='y', 
-                 plotnum=1, title=None, ftol=0.1):
+                 plotnum=1, title=None, ftol=0.1, stationid=(0,4), 
+                 linedir='ew', plot_period=None):
         """
         Initialize parameters
         """
         
         #--> get the inputs into a list of mt objects
-        self.mt_lst = get_mtlst(fn_lst=fn_lst, 
-                                 res_object_lst=res_object_lst,
-                                 z_object_lst=z_object_lst, 
-                                 mt_object_lst=mt_object_lst)
+        self.mt_lst = mtpl.get_mtlst(fn_lst=fn_lst, 
+                                     res_object_lst=res_object_lst,
+                                     z_object_lst=z_object_lst, 
+                                     mt_object_lst=mt_object_lst)
         
         #set some of the properties as attributes much to Lars' discontent
         self.fignum = fignum
+        self.fig_size = [8, 4]
         self.title = title
         self.dpi = dpi
         self.font_size = 7
         self.ftol = ftol
+        self.stationid = stationid
+        self.linedir = linedir
+        self.aspect = 'auto'
+        
+        self.cb_pad = .02
+        self.cb_orientation = 'horizontal'
+        self.cb_shrink = .7
         
         #if rotation angle is an int or float make an array the length of 
         #mt_lst for plotting purposes
@@ -60,106 +70,213 @@ class PlotResPhasePseudoSection(object):
         self.res_cmap = 'mt_rd2gr2bl'
         self.phase_cmap = 'mt_bl2gr2rd'
         
+        #create empty lists to put things into
+        self.stationlst = []
+        self.offsetlst = []
         
+        period_lst = np.array([len(mt.period) for mt in self.mt_lst])
+        
+        #find index where the longest period is if multiple pick the first one
+        max_find = np.where(period_lst==period_lst.max())[0]
+        if len(max_find)>0:
+            max_find = max_find[0]
+    
+        if plot_period is None:
+            self.plot_period = \
+                  self.mt_lst[max_find].period
+        
+        else:
+            self.plot_period = plot_period
+            
+        #create empty arrays to put data into
+        ns = len(self.mt_lst)
+        nt = len(self.plot_period)
+        
+        self.resxx = np.zeros((nt, ns))
+        self.resxy = np.zeros((nt, ns))
+        self.resyx = np.zeros((nt, ns))
+        self.resyy = np.zeros((nt, ns))
+        
+        self.phasexx = np.zeros((nt, ns))
+        self.phasexy = np.zeros((nt, ns))
+        self.phaseyx = np.zeros((nt, ns))
+        self.phaseyy = np.zeros((nt, ns))
+        
+    #---need to rotate data on setting rotz
+    def _set_rot_z(self, rot_z):
+        """
+        need to rotate data when setting z
+        """
+        
+        #if rotation angle is an int or float make an array the length of 
+        #mt_lst for plotting purposes
+        if type(rot_z) is float or type(rot_z) is int:
+            rot_z = np.array([rot_z]*len(self.mt_lst))
+        
+        #if the rotation angle is an array for rotation of different 
+        #freq than repeat that rotation array to the len(mt_lst)
+        elif type(rot_z) is np.ndarray:
+            if rot_z.shape[0]!=len(self.mt_lst):
+                rot_z = np.repeat(rot_z, len(self.mt_lst))
+                
+        else:
+            pass
+        
+        #rotate the data
+        for ii,mt in enumerate(self.mt_lst):
+            mt.rot_z = rot_z[ii]
+            
+    rot_z = property(fset=_set_rot_z, doc="rotation angle(s)")
+
+    
+    def sort_by_offsets(self):
+        """
+        get list of offsets to sort the mt list
+        
+        """
+        
+        dtype = [('station', 'S10'), ('offset', float), ('spot', int)]
+        slst = []
+        #get offsets
+        for ii, mt in enumerate(self.mt_lst):
+            #get offsets between stations
+            if ii == 0:
+                east0 = mt.lon
+                north0 = mt.lat
+                offset = 0.0
+            else:
+                east = mt.lon
+                north = mt.lat
+                #if line is predominantly e-w
+                if self.linedir=='ew': 
+                    if east0 < east:
+                        offset = np.sqrt((east0-east)**2+(north0-north)**2)
+                    elif east0 > east:
+                        offset = -1*np.sqrt((east0-east)**2+(north0-north)**2)
+                    else:
+                        offset = 0
+                #if line is predominantly n-s
+                elif self.linedir == 'ns':
+                    if north0 < north:
+                        offset = np.sqrt((east0-east)**2+(north0-north)**2)
+                    elif north0 > north:
+                        offset = -1*np.sqrt((east0-east)**2+(north0-north)**2)
+                    else:
+                        offset=0
+            #append values to list for sorting            
+            slst.append((mt.station, offset, ii))
+        
+        #create a structured array according to the data type and values
+        v_array = np.array(slst, dtype=dtype)
+        
+        #sort the structured array by offsets
+        sorted_array = np.sort(v_array, order=['offset'])
+        
+        #create an offset list as an attribute
+        self.offset_lst = np.array([ss[1] for ss in sorted_array])
+        
+        #create a station lst as an attribute
+        self.station_lst = np.array([ss[0][self.stationid[0]:self.stationid[1]]
+                                     for ss in sorted_array])
+        
+        #create an index list of the sorted index values 
+        index_lst = [ss[2] for ss in sorted_array]
+        
+        #create a new mt_lst according to the offsets from the new index_lst
+        new_mt_lst = [self.mt_lst[ii] for ii in index_lst]
+        
+        #set the mt_lst attribute as the new sorted mt_lst
+        self.mt_lst = new_mt_lst
+        
+    def get_rp_arrays(self):
+        """
+        get resistivity and phase values in the correct order according to 
+        offsets and periods.
+
+        """        
+        
+        #make sure that mt_lst has been sorted according to offset
+        self.sort_by_offsets()
+        
+        #make a dictionary of the periods to plot for a reference
+        period_dict = dict([(key, vv) 
+                             for vv, key in enumerate(self.plot_period)])
+                             
+        for ii, mt in enumerate(self.mt_lst):
+            #get resisitivity and phase in a dictionary and append to a list
+            rp = mt.get_ResPhase()
+            
+            for kk, iper in enumerate(mt.period):
+                jj = None
+                for rr, rper in enumerate(self.plot_period):
+                    if iper == rper:
+                        jj = period_dict[rper]
+                        self.resxx[jj, ii] = np.log10(rp.resxx[kk])
+                        self.resxy[jj, ii] = np.log10(rp.resxy[kk])
+                        self.resyx[jj, ii] = np.log10(rp.resyx[kk])
+                        self.resyy[jj, ii] = np.log10(rp.resyy[kk])
+                        
+                        self.phasexx[jj, ii] = rp.phasexx[kk]
+                        self.phasexy[jj, ii] = rp.phasexy[kk]
+                        self.phaseyx[jj, ii] = rp.phaseyx[kk]
+                        self.phaseyy[jj, ii] = rp.phaseyy[kk]
+                        
+                        break
+                        
+                    elif rper*(1-self.ftol) <= iper and \
+                         iper <= rper*(1+self.ftol):
+                             jj = period_dict[rper]
+                             self.resxx[jj, ii] = np.log10(rp.resxx[kk])
+                             self.resxy[jj, ii] = np.log10(rp.resxy[kk])
+                             self.resyx[jj, ii] = np.log10(rp.resyx[kk])
+                             self.resyy[jj, ii] = np.log10(rp.resyy[kk])
+                            
+                             self.phasexx[jj, ii] = rp.phasexx[kk]
+                             self.phasexy[jj, ii] = rp.phasexy[kk]
+                             self.phaseyx[jj, ii] = rp.phaseyx[kk]
+                             self.phaseyy[jj, ii] = rp.phaseyy[kk]
+                             
+                             break
+                    else:
+                        pass
+                        
+                if jj is None:
+                    print 'did not find period {0:.6g} (s) for {1}'.format(
+                               iper, self.station_lst[ii])
         
     def plot(self):
         
-    
-        plt.rcParams['font.size']=self.font_size
-        plt.rcParams['figure.subplot.left']=.07
-        plt.rcParams['figure.subplot.right']=.98
-        plt.rcParams['figure.subplot.bottom']=.06
-        plt.rcParams['figure.subplot.top']=.94
-        plt.rcParams['figure.subplot.wspace']=.01
-        plt.rcParams['figure.subplot.hspace']=.20
+        plt.rcParams['font.size'] = self.font_size
+        plt.rcParams['figure.subplot.left'] = .07
+        plt.rcParams['figure.subplot.right'] = .98
+        plt.rcParams['figure.subplot.bottom'] = .06
+        plt.rcParams['figure.subplot.top'] = .94
+        plt.rcParams['figure.subplot.wspace'] = .01
+        plt.rcParams['figure.subplot.hspace'] = .20
 
-        
-        #create empty lists to put things into
-        stationlst = []
-        offsetlst = []
-        minperiodlst = []
-        maxperiodlst = []
-        periodlst = []
-        nlst = []
-        
-        period_lst = [len(mt.period) for mt in self.mt_lst]
-        
-        self.plot_period = np.where
-        
-        #create empty arrays to put data into
-        n=len(self.mt_lst)
-        resxx=np.zeros((maxperiod,n))
-        resxy=np.zeros((maxperiod,n))
-        resyx=np.zeros((maxperiod,n))
-        resyy=np.zeros((maxperiod,n))
-        
-        phasexx=np.zeros((maxperiod,n))
-        phasexy=np.zeros((maxperiod,n))
-        phaseyx=np.zeros((maxperiod,n))
-        phaseyy=np.zeros((maxperiod,n))
-        
-        #get data into arrays
-        for ii,fn in enumerate(edifilelst):
-            #get offsets between stations
-            imp=Z.Z(fn)
-            stationlst.append(imp.station[stationid[0]:stationid[1]])
-            zone,east,north=utm2ll.LLtoUTM(23,imp.lat,imp.lon)
+        self.get_rp_arrays()
             
-            if ii==0:
-                east0=east
-                north0=north
-                offset=0.0
-            else:
-                if linedir=='ew': 
-                    if east0<east:
-                        offset=np.sqrt((east0-east)**2+(north0-north)**2)
-                    elif east0>east:
-                        offset=-1*np.sqrt((east0-east)**2+(north0-north)**2)
-                    else:
-                        offset=0
-                elif linedir=='ns':
-                    if north0<north:
-                        offset=np.sqrt((east0-east)**2+(north0-north)**2)
-                    elif north0>north:
-                        offset=-1*np.sqrt((east0-east)**2+(north0-north)**2)
-                    else:
-                        offset=0
-            offsetlst.append(offset)
-            #get resisitivity and phase in a dictionary and append to a list
-            rp=imp.getResPhase(ffactor=ffactor,thetar=rotz)
-            m=len(imp.period)
-            resxx[0:m,ii]=np.log10(rp.resxx)#[::-1]
-            resxy[0:m,ii]=np.log10(rp.resxy)#[::-1]
-            resyx[0:m,ii]=np.log10(rp.resyx)#[::-1]
-            resyy[0:m,ii]=np.log10(rp.resyy)#[::-1]
-            
-            phasexx[0:m,ii]=rp.phasexx#[::-1]
-            phasexy[0:m,ii]=rp.phasexy#[::-1]
-            phaseyx[0:m,ii]=rp.phaseyx+180#[::-1]
-            phaseyy[0:m,ii]=rp.phaseyy#[::-1]
-            
-            #get min and max of the period, just in case some edi files have 
-            #different period information
-            minperiodlst.append(min(imp.period))
-            maxperiodlst.append(max(imp.period))
-            nlst.append(len(imp.period))
-            periodlst.append(imp.period)
-            
-        nperiod=max(nlst)
-        for pp in range(len(periodlst)):
-            if nlst[pp]==nperiod:
-                period=np.log10(periodlst[pp])#[::-1]
-    
+        #make a general subplot array
+        gs = gridspec.GridSpec(2, 4, height_ratios=[2, 1.5], hspace=.05, 
+                               wspace=.1)
+                               
+        
         #plot data
-        fig=plt.figure(1,[12,4],dpi=150)
-        extent=(0,n,period[-1],period[0])
-        aspect=aspect
-        cmap=cmap
-        pad=.2
-        kwargs={'aspect':aspect,'cmap':cmap,'extent':extent}
-        cbarkwargs={'shrink':.7,'orientation':'horizontal','pad':pad}
+        self.fig = plt.figure( 1, self.fig_size, dpi=self.dpi)
+        extent=(0, nt, self.plot_period[-1], self.plot_period[0])
+
+        rkwargs = {'aspect':self.aspect,
+                   'cmap':self.res_cmap,
+                   'extent':extent}
+                   
+        pkwargs = {'aspect':self.aspect,
+                   'cmap':self.phase_cmap,
+                   'extent':extent}
+                   
+        cbarkwargs = {'shrink':self.cb_shrink,
+                      'orientation':self.cb_orientation,
+                      'pad':self.cb_pad}
     
-        
         ax2=plt.subplot(2,4,2)
         plt.imshow(resxy[0:nperiod,:],**kwargs)
         plt.colorbar(**cbarkwargs)
