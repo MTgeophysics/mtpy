c9ece8fa0042c0187f1ad05558cb5642a802fe7f
JP <peacock.jared@gmail.com>
2014-01-09 11:33:42 -0800

    fixed but in core.z.Z when computing res_phase if freq is None

--- mtpy/core/z.py
+++ mtpy/core/z.py
@@ -187,8 +187,11 @@ class Z(object):
             except IndexError:
                 print 'Need to input frequency array'
 
-    def _get_freq(self): 
-		return np.array(self._freq)
+    def _get_freq(self):
+            if self._freq is None:
+                return None
+            else:
+                return np.array(self._freq)
 		
     freq = property(_get_freq, _set_freq, doc='array of frequencies in Hz')
     
@@ -389,7 +392,9 @@ class Z(object):
             
         if self.z is None:
             print 'Z array is None - cannot calculate Res/Phase'
-            return            
+            return 
+        
+        
 
         self._resistivity_err = None
         self._phase_err = None
