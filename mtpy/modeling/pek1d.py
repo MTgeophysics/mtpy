1c4a3df7da526e272235a1dd935f0174fad94558
alkirkby <alison.kirkby@adelaide.edu.au>
2014-06-10 11:46:49 +0930

    pek1d - updates to savepath conventions

--- mtpy/modeling/pek1d.py
+++ mtpy/modeling/pek1d.py
@@ -274,11 +274,7 @@ def build_run():
     
     # establish the rank of the computer
     rank = MPI.COMM_WORLD.Get_rank()
-    
-    # make a master directory under the working directory to save all runs into
-    master_directory = fh.make_unique_folder(input_parameters['working_directory'],
-                                             basename = input_parameters['master_savepath'])
-    build_parameters['master_savepath'] = master_directory
+
    
     # create a list of edi files to model
     edi_list = create_filelist(input_parameters['working_directory'],
@@ -292,6 +288,11 @@ def build_run():
             build_inputs[key] = input_parameters[key]
         except:
             pass
+
+    # make a master directory under the working directory to save all runs into
+    master_directory = fh.make_unique_folder(input_parameters['working_directory'],
+                                             basename = input_parameters['master_savepath'])
+    build_inputs['master_savepath'] = master_directory
 
     # build a model
     Data = generate_inputfiles(edi_list[rank],build_inputs)
