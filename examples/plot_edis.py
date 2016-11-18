928d8328aea15e8b1a682e6e11560e535a390d08
Fei Zhang <fei.zhang@ga.gov.au>
2016-11-19 09:02:49 +1100

    modified plot_edi.py to use edi_path as a commandline argument

--- examples/plot_edis.py
+++ examples/plot_edis.py
@@ -13,22 +13,46 @@ import mtpy.core.edi as mtedi
 import mtpy.analysis.geometry as mtg
 import os.path as op
 import os
+import sys
+import glob
 import matplotlib.pyplot as plt
 import numpy as np
 
 
+def main(edi_path):
+	"""
+	plot edi files from the input directory edi_dir
 
-epath = r'C:\Git\mtpy\examples\data\edi_files\georgina'
+	edi_path = r'C:\Git\mtpy\examples\data\edi_files\georgina'
 
-svdir = r'V:\2008\09GA_GA1_Georgina\Reprocessing_2016\plots\station_plots'
-
-elst=[op.join(epath,edi) for edi in os.listdir(epath) if (edi.endswith('.edi'))]# and edi.startswith('GB')
-
-dimensionality = np.zeros((len(elst),46))
+	svdir = r'V:\2008\09GA_GA1_Georgina\Reprocessing_2016\plots\station_plots'
+	"""
 
 
-for efile in elst:
-#    eo = mtedi.Edi(filename=efile)
-	pr = mtpr.PlotResponse(fn=efile,plot_num=2,res_limits=(1,10000),phase_limits=(0,90))
-	plt.close()
+	#elst=[op.join(edi_path,edi) for edi in os.listdir(edi_path) if (edi.endswith('.edi'))]# and edi.startswith('GB')
+
+	elst= glob.glob(os.path.join(edi_path, "*.edi"))
+
+	#dimensionality = np.zeros((len(elst),46))
+
+	for efile in elst:
+	#    eo = mtedi.Edi(filename=efile)
+		pr = mtpr.PlotResponse(fn=efile,plot_num=2,res_limits=(1,10000),phase_limits=(0,90))
+		plt.close()
+
+
+#########################################################
+if __name__ == '__main__':
+	""" plot one-by-one edi files in a given dirpath
+	How to Run:
+	export PYTHONPATH=/Softlab/Githubz/mtpy:$PYTHONPATH
+	cd /Softlab/Githubz/mtpy/examples
+	python plot_edis.py data/edi_files/
+	"""
+
+
+	edi_path=sys.argv[1]
+
+	main(edi_path)
+
 
