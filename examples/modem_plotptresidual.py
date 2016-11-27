1d80d09580a1994f86a29dece96b1872448ce004
Fei Zhang <fei.zhang@ga.gov.au>
2016-11-28 08:49:52 +1100

    update examples/modem_plotptresidual.py at vdi works OK

--- examples/modem_plotptresidual.py
+++ examples/modem_plotptresidual.py
@@ -9,6 +9,7 @@ import mtpy.modeling.modem_new as mtmn
 import os.path as op
 
 workdir = r'E:\Github\mtpy2\examples\data\ModEM_files'
+workdir = r'/g/data/ha3/fxz547/Githubz/mtpy2/examples/data/ModEM_files'
 modeldir = op.join(workdir,'VicSynthetic05')
 
 ptmap= mtmn.PlotPTMaps(data_fn=op.join(modeldir,'ModEM_Data_noise10inv.dat'),
