b2666687334acbfe9b95384c979e382740d5aa32
Fei Zhang <fei.zhang@ga.gov.au>
2016-12-11 11:04:31 +1100

    Added unit-testing into dir tests/

---
+++ tests/core/test_edi.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+
+class TestEdi(TestCase):
+    def test_read_edi_file(self):
+        self.failIf(False)
+
+    def test__read_data(self):
+        self.failIf(False)
+
+    def test__read_mt(self):
+        self.failIf(False)
+
+    def test__read_spectra(self):
+        self.failIf(False)
+
+    def test_write_edi_file(self):
+        self.failIf(False)
+
+    def test__write_data_block(self):
+        self.failIf(False)
+
+    def test__get_lat(self):
+        self.failIf(False)
+
+    def test__set_lat(self):
+        self.failIf(False)
+
+    def test__get_lon(self):
+        self.failIf(False)
+
+    def test__set_lon(self):
+        self.failIf(False)
+
+    def test__get_elev(self):
+        self.failIf(False)
+
+    def test__set_elev(self):
+        self.failIf(False)
+
+    def test__get_station(self):
+        self.failIf(False)
+
+    def test__set_station(self):
+        self.failIf(False)
