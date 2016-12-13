8577509ec50764e2495d20637c84df2ce02adb0b
Fei Zhang <fei.zhang@ga.gov.au>
2016-12-13 16:18:56 +1100

    added ediFile util updates and refactoring

--- check_code.sh
+++ check_code.sh
@@ -8,10 +8,11 @@
 pep8 tests  exp  --max-line-length 120
 
 echo "pylint .... "
-pylint -j 2 --reports=no tests 
-
+#pylint -j 2 --reports=no tests  #j2 cpu run no report
+pylint -E tests
 echo "pylint ....end "
 
+py.test tests
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
 #? py.test -r sx --cov datacube --durations=5 datacube tests datacube_apps $@
