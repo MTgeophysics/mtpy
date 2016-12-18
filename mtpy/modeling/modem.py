modem_new minor changes to fix setting of topography and covariance mask array

mtpy/modeling/modem.py
@@ -1799,7 +1799,7 @@ class Model(object):
         log_z = np.logspace(np.log10(self.z1_layer),
                             np.log10(self.z_target_depth),
                             num=self.n_layers - self.pad_z - self.n_airlayers + 1)
-        log_z = log_z[1:] - log_z[:-1]
+#        log_z = log_z[1:] - log_z[:-1]
         z_nodes = np.array([zz - zz % 10 ** np.floor(np.log10(zz)) for zz in
                             log_z])
         # index of top of padding
@@ -3080,6 +3080,8 @@ class Covariance(object):
                 self.mask_arr[np.where(mod_obj.res_model > air * .9)] = 0
                 self.mask_arr[np.where((mod_obj.res_model < sea_water * 1.1) &
                                        (mod_obj.res_model > sea_water * .9))] = 9
+                # flip mask arr as it needs to be in opposite order
+                self.mask_arr = self.mask_arr[::-1]
         else:
             if self.mask_arr is None:
                 self.mask_arr = np.ones((self.grid_dimensions[0],