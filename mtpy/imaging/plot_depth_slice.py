f245dcdaead23274f30a91772c58550c37894420
ak <u64125@PC-65328.prod.lan>
2017-01-19 11:35:44 +1100

    change default x and y limits in depth slice plots so that they plot symmetrically

--- mtpy/imaging/plot_depth_slice.py
+++ mtpy/imaging/plot_depth_slice.py
@@ -254,7 +254,7 @@ class PlotDepthSlice(object):
                 xlimits = (np.floor(self.station_east.min()),
                            np.ceil(self.station_east.max()))
             else:
-                xlimits = (self.grid_east[5], self.grid_east[-5])
+                xlimits = (self.grid_east[5], self.grid_east[-6])
         else:
             xlimits = self.ew_limits
 
@@ -263,7 +263,7 @@ class PlotDepthSlice(object):
                 ylimits = (np.floor(self.station_north.min()),
                            np.ceil(self.station_north.max()))
             else:
-                ylimits = (self.grid_north[5], self.grid_north[-5])
+                ylimits = (self.grid_north[5], self.grid_north[-6])
         else:
             ylimits = self.ns_limits
 
