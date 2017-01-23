071528005edfb5b5949052a1aa7c7a3bf5bda435
zhang01GA <fei.zhang@ga.gov.au>
2017-01-24 08:31:20 +1100

    updated plot_edi.py

--- examples/plot_edis.py
+++ examples/plot_edis.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 18 15:35:39 2013
-
-@author: Alison Kirkby
-
 plots phase tensor ellipses as a pseudo section (distance along profile vs period)
+
+CreatedOn:      Wed Sep 18 15:35:39 2013
+CreatedBy:      Alison Kirkby
+
+LastUpdated:    2017-01-24
+UpdatedBy:      fei.zhang@ga.gov.au
 """
+
 
 import os
 import sys
@@ -18,60 +21,62 @@ from mtpy.utils.mtpylog import MtPyLog
 logger = MtPyLog().get_mtpy_logger(__name__)
 #logger = MtPyLog(path2configfile='logging.yml').get_mtpy_logger(__name__) # specific
 
-def main(edi_path):
-    """ plot edi files from the input directory edi_dir
+def plot_edi_dir(edi_path):
+    """ plot edi files from the input directory edi_path
     """
 
-    # plt.style.use('dark_background')
-    plt.style.use('seaborn-deep')
-    plt.style.use('classic')
+    edi_files = glob.glob(os.path.join(edi_path, "*.edi"))
 
-    elst = glob.glob(os.path.join(edi_path, "*.edi"))
+    logger.debug(edi_files)
 
-    logger.debug(elst)
-
-    for efile in elst[:2]:
+    for efile in edi_files:
+        # for efile in edi_files[:2]:
         logger.debug("plotting %s", efile)
         # eo = mtedi.Edi(filename=efile)
-        pr = mtpr.PlotResponse(fn=efile, plot_num=2, res_limits=(
-            1, 10000), phase_limits=(0, 90))
-
-        plt.close()
+        plot_edi_file(efile)
 
     return
 
 
-def plot1(edi_file):
+def plot_edi_file(edi_file):
     """
-    Plot a given edi file
+    Plot the input edi_file
     Args:
         edi_file: path2edifile
 
     Returns:
 
     """
+
+    # plt.style.use('dark_background')
     plt.style.use('seaborn-deep')
     plt.style.use('classic')
 
-    logger.info("Plotting edi file %s", edi_file)
+    logger.info("Plotting the edi file %s", edi_file)
 
     pr = mtpr.PlotResponse(fn=edi_file, plot_num=2, res_limits=(1, 10000), phase_limits=(0, 90))
 
     return pr
 
 
-#########################################################
+###############################################################################
 # plot one-by-one edi files in a given dirpath
 # How to Run:
-# export PYTHONPATH=/Softlab/Githubz/mtpy2:$PYTHONPATH
-# python plot_edis.py data/edi_files/
-
+#    export PYTHONPATH=/Softlab/Githubz/mtpy2:$PYTHONPATH
+#    python  examples/plot_edis.py data/edi_files/
+#    python  examples/plot_edis.py tests/data/edifiles/15125A.edi
+# =============================================================================
 if __name__ == '__main__':
-    edi_path = sys.argv[1]
 
-    if os.path.isdir(edi_path):
-        main(edi_path)
-    elif os.path.isfile(edi_path):
-        plot1(edi_path)
+    if len(sys.argv) < 2:
+        print ("\n please provide path to edi files\n USAGE:  %s path2edifile" % sys.argv[0])
+        sys.exit(1)
     else:
-        logger.error("Usage %s %s", sys.argv[0], "path2edi")
+        edi_path = sys.argv[1]
+
+        if os.path.isfile(edi_path):
+            plot_edi_file(edi_path)
+        elif os.path.isdir(edi_path):
+            plot_edi_dir(edi_path)
+        else:
+            logger.error("Usage %s %s", sys.argv[0], "path2edi")
