f8470fa19c2973de4b48f122b12cfc4f1fbc4cd2
Fei Zhang <fei.zhang@ga.gov.au>
2017-02-03 10:54:39 +1100

    more plots for penetration depth working OK.

--- examples/plot_penetration_depth.py
+++ examples/plot_penetration_depth.py
@@ -142,7 +142,7 @@ def plot_multi_station_pen_depth(per_index, edifiles):
     # per_index=0,1,2,....
     periods = []
 
-    app_resis = []
+    depths = []
 
     stations = []
 
@@ -159,14 +159,14 @@ def plot_multi_station_pen_depth(per_index, edifiles):
         periods.append(per)
         penetration_depth = -scale_param * np.sqrt(zeta.resistivity[per_index, 0, 1] * per)
 
-        app_resis.append(penetration_depth)
+        depths.append(penetration_depth)
         stations.append(mt_obj.station)
 
     #plt.plot(app_resis, color='b', marker='o')
 
-    index = np.arange(len(app_resis))
+    index = np.arange(len(depths))
 
-    plt.bar(index, app_resis, color='#000000')
+    plt.bar(index, depths, color='#000000')
 
     # plt.xaxis.tick_top()
     # plt.set_xlabel('X LABEL')
@@ -184,7 +184,133 @@ def plot_multi_station_pen_depth(per_index, edifiles):
     plt.show()
 
     #Check that the periods are the same value for all stations
-    return (stations, app_resis, periods)
+    return (stations, depths, periods)
+
+def plot_3Dbar_depth(per_index, edifiles):
+    """
+    plot 3D bar of penetration depths
+    For a given freq/period index of a set of edifiles/dir, the station,periods, pendepth,(lat, lon) are extracted
+    the geo-bounding box calculated, and the mapping from stations to grids is constructed and plotted.
+
+    :param per_index: period index number 0,1,2
+    :param edifiles: an edi_dir or list of edi_files
+    :return:
+    """
+    if os.path.isdir(edifiles):
+        edi_dir = edifiles # "E:/Githubz/mtpy2/tests/data/edifiles/"
+        edifiles = glob.glob(os.path.join(edi_dir, '*.edi'))
+        logger.debug(edifiles)
+    else:
+        # Assume edifiles is [a list of files]
+        pass
+
+    scale_param = np.sqrt(1.0 / (2.0 * np.pi * 4 * np.pi * 10 ** (-7)))
+
+    # per_index=0,1,2,....
+    periods = []
+    pen_depth = []
+    stations = []
+    latlons = []
+
+    for afile in edifiles:
+        mt_obj = mt.MT(afile)
+
+        latlons.append((mt_obj.lat, mt_obj.lon))
+
+        # the attribute Z
+        zeta = mt_obj.Z
+
+        if per_index >= len(zeta.freq):
+            raise Exception("Index out_of_range Error: period index must be less than number of periods in zeta.freq")
+
+        per = 1.0 / zeta.freq[per_index]
+        periods.append(per)
+
+        penetration_depth = - scale_param * np.sqrt(zeta.resistivity[per_index, 0, 1] * per)
+        pen_depth.append(penetration_depth)
+
+        stations.append(mt_obj.station)
+
+    #return (stations, periods, pen_depth, latlons)
+
+    lats = [tup[0] for tup in latlons]
+    lons = [tup[1] for tup in latlons]
+    minlat = min(lats)
+    maxlat = max(lats)
+    minlon = min(lons)
+    maxlon = max(lons)
+
+    pixelsize = 0.001  # degree 0.001 = 100meters
+    offset = 3
+    LL_lat = minlat - offset * pixelsize
+    LL_lon = minlon - offset * pixelsize
+
+    xgrids = maxlon - minlon
+    ygrids = maxlat - minlat
+
+    nx = xgrids / pixelsize
+    ny = ygrids / pixelsize
+
+    from mpl_toolkits.mplot3d import Axes3D
+    # import matplotlib.pyplot as plt
+    # import numpy as np
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111, projection='3d')
+
+    xpos = []  # a seq (1,2,3,4,5,6,7,8,9,10)
+    ypos = []  # a seq [2,3,4,5,1,6,2,1,7,2]
+    dz = []
+    for iter, pair in enumerate(latlons):
+        xpos.append(get_index(pair[0], pair[1],LL_lat, LL_lon, pixelsize)[0])
+        ypos.append(get_index(pair[0], pair[1], LL_lat, LL_lon, pixelsize)[1])
+        dz.append(np.abs(pen_depth[iter]))
+
+    num_elements = len(xpos)
+    zpos = np.zeros(num_elements)  # zpos = [0,0,0,0,0,0,0,0,0,0]
+    dx = np.ones(num_elements)
+    dy = np.ones(num_elements)
+    # dz = [1,2,3,4,5,6,7,8,9,10]
+
+    print(xpos)
+    print(ypos)
+    print(zpos)
+
+    print(dx)
+    print(dy)
+    print(dz)
+    ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color='r')
+
+    #ax1
+
+    plt.title('Penetration Depth Across Stations, for MT period= %6.3f Seconds' % periods[0], fontsize=16)
+    plt.xlabel('Latitude (deg-grid)', fontsize=16)
+    plt.ylabel('Longitude (deg-grid)', fontsize=16)
+    # bar_width = 0.4
+    # plt.xticks(index + bar_width / 2, stations, rotation='horizontal', fontsize=16)
+    # plt.legend()
+    #
+    # # plt.tight_layout()
+    # plt.gca().xaxis.tick_top()
+    # plt.show()
+
+    plt.show()
+
+
+def get_index(lat, lon, LL_lat, LL_lon, pixelsize):
+    """ Mapping of lat lon to a grid
+    :param lat:
+    :param lon:
+    :param LL_lon:
+    :param LL_lat:
+    :param pixelsize:
+    :return:
+    """
+
+    index_x = (lon - LL_lon) / pixelsize
+    index_y = (lat - LL_lat) / pixelsize
+
+    return (int(index_x), int(index_y))
 
 ###############################################################################
 # plot one-by-one edi files in a given dirpath
@@ -207,6 +333,7 @@ if __name__ == '__main__':
         elif os.path.isdir(edi_path):
             #plot_edi_dir(edi_path )
             #plot_edi_dir(edi_path, rholist=['det'] )
-            plot_multi_station_pen_depth(42, edi_path)
+            #plot_multi_station_pen_depth(10, edi_path)
+            plot_3Dbar_depth(40,edi_path)
         else:
             logger.error("Usage %s %s", sys.argv[0], "path2edi")
