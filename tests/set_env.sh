4822ba29f42b3fa235db205c8f9f73182e062fa6
Fei Zhang <fei.zhang@ga.gov.au>
2017-02-24 14:17:05 +1100

    add tests/set_env.sh for anaconda env

---
+++ tests/set_env.sh
@@ -0,0 +1,10 @@
+# to use anaconda2 in Windows PC bash shell
+# 	source set_env.sh
+
+export export PATH=/c/Anaconda2:/c/Anaconda2/Scripts:/c/Anaconda2/Library/bin:$PATH
+export  PYTHONPATH=/e/Githubz/mtpy2
+
+# Then run python plot_phase_tensor_section.py
+
+# python notebook
+#	jupyter notebook &> localdir/jupyter.log &
