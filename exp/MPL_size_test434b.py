4354904eba69ed34f8003884316b8c2c4c44d007
Fei Zhang <fei.zhang@ga.gov.au>
2017-03-18 16:45:55 +1100

    minor updates to exp/MPL_size_test434b.py

--- exp/MPL_size_test434b.py
+++ exp/MPL_size_test434b.py
@@ -62,7 +62,8 @@ generating PNGs for web pages and the like.
 
 import matplotlib
 print "using MPL version:", matplotlib.__version__
-#matplotlib.use("WXAgg") # do this before pylab so you don'tget the default back end.
+matplotlib.use("WXAgg") # do this before pylab so you don't get the default back end.
+#sudo apt-get install python-wxgtk3.0
 
 import pylab
 import numpy as np
@@ -81,9 +82,9 @@ DefaultSize = F.get_size_inches()
 print "Default size in Inches", DefaultSize
 print "Which should result in a %i x %i Image"%(DPI*DefaultSize[0], DPI*DefaultSize[1])
 
-pylab.show(1)
+#pylab.show(1)
 # the default is 100dpi for savefig:
-F.savefig("test1.png")
+F.savefig("test1.jpg")
 # this gives me a 797 x 566 pixel image, which is about 100 DPI
 
 # Now make the image twice as big, while keeping the fonts and all the
@@ -92,8 +93,8 @@ F.set_size_inches( (DefaultSize[0]*2, DefaultSize[1]*2) )
 size2 = F.get_size_inches()
 print "Now Size in Inches X2", size2
 
-F.show() # this would not show?
-F.savefig("test2.png")
+#F.show() # this would not show?
+F.savefig("test2.jpg")
 # this results in a 1595x1132 image
 
 # Now make the image twice as big, making all the fonts and lines
@@ -101,8 +102,8 @@ F.savefig("test2.png")
 
 F.set_size_inches( DefaultSize )# resetthe size
 size3 = F.get_size_inches()
-pylab.show(1)
+#pylab.show(1)
 
 print "Size in Inches back to default", size3
-F.savefig("test3.png", dpi = (200)) # change the dpi
+F.savefig("test3.jpg", dpi = (200)) # change the dpi
 # this also results in a 1595x1132 image, but the fonts are larger.
