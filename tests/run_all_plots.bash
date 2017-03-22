ec57bddc1f4e2e9e7e16da0ab3c5b1374e5145b4
Fei Zhang <fei.zhang@ga.gov.au>
2017-03-23 08:16:24 +1100

    modified  tests/run_all_plots.bash

--- tests/run_all_plots.bash
+++ tests/run_all_plots.bash
@@ -9,9 +9,23 @@ python examples/Modem_buildinputfiles.py
 python examples/plot_edis.py tests/data/edifiles/15125A.edi
 
 
-python examples/plot_phase_tensor_map.py  /e/Data/MT_Datasets/3D_MT_data_edited_fromDuanJM/ 10 /e/MTPY2_Outputs
-python examples/plot_phase_tensor_map.py  /e/Data/MT_Datasets/GA_UA_edited_10s-10000s/ 0.0625 /e/MTPY2_Outputs/
+python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/3D_MT_data_edited_fromDuanJM/ 10
+python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 0.0625s
+python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 10s
+python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 16s
+python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 40s
+python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 341.0s
+python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 4096s
+python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 2048s
+python mtpy/imaging/penetration_depth_3d_profile.py tests/data/edifiles/ 10
+python mtpy/imaging/penetration_depth_3d_profile.py tests/data/edifiles/ 2.8571s
 
+python examples/plot_phase_tensor_map.py  /e/Data/MT_Datasets/3D_MT_data_edited_fromDuanJM/ 10 /e/MTPY2_Outputs
+
+python examples/plot_phase_tensor_map.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 0.025  /e/MTPY2_Outputs
+python examples/plot_phase_tensor_map.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 0.01  /e/MTPY2_Outputs
+python examples/plot_phase_tensor_map.py  /e/Data/MT_Datasets/GA_UA_edited_10s-10000s/ 0.0625 /e/MTPY2_Outputs/
+python examples/plot_phase_tensor_map.py  /e/Data/MT_Datasets/GA_UA_edited_10s-10000s/ 0.0004883 /e/MTPY2_Outputs/
 
 #python examples/plot_phase_tensor_map.py  examples/data/edi_files 10 /e/MTPY2_Outputs/
 #python examples/plot_phase_tensor_map.py  examples/data/edi2 10
@@ -34,16 +48,9 @@ python examples/plot_phase_tensor_map.py  /e/Data/MT_Datasets/GA_UA_edited_10s-1
 #python examples/plot_phase_tensor_map.py tests/data/edifiles/ 10 /e/MTPY2_Outputs/ptmap3deg.png
 #
 #
-#
-python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/3D_MT_data_edited_fromDuanJM/ 10
-python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 0.0625s
-python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 10s
-python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 16s
-python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 40s
-python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 341.0s
-python mtpy/imaging/penetration_depth_3d_profile.py /e/Data/MT_Datasets/GA_UA_edited_10s-10000s 4096.0s
 
-python mtpy/imaging/penetration_depth_3d_profile.py tests/data/edifiles/ 10
-python mtpy/imaging/penetration_depth_3d_profile.py tests/data/edifiles/ 2.8571s
+# phase tensor pseudo sections
+
 
 # shape files generation
+
