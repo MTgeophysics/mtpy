8accb7eeafdf800ab867c167a476edc5f2e45bda
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-06-20 16:15:25 +1000

    implemented generic show and export function

--- mtpy/imaging/imaging_base.py
+++ mtpy/imaging/imaging_base.py
@@ -9,45 +9,77 @@
     Date: 20/06/2017
 """
 
-from abc import ABCMeta
+import abc
 import mtpy.core.mt as mt
 
 
 class ImagingBase:
-    __metaclass__ = ABCMeta
+    __metaclass__ = abc.ABCMeta
 
-    @abstractmethod
+    @abc.abstractmethod
     def plot(self, **kwargs):
         pass
 
-    def show(self):
+    def show(self, block=True):
         """
         display the image
         :return:
         """
         if self._fig == None:
             self.plot()
-        self._fig.show()
+        if block:
+            import matplotlib.pyplot as plt
+            plt.show()
+        else:
+            self._fig.show()
 
 
-    @abstractmethod
     def export_image(self, fn, dpi=800):
-        if self.fig == None:
+        if self._fig == None:
             self.plot()
         self._fig.savefile(fn, dpi)
 
-    @abstractmethod
-    def set_edi(self, edis):
+    @abc.abstractmethod
+    def set_data(self, data):
+        pass
+
+    def _set_edis(self, edis):
+        """
+        set edi (mt.MT) objects for plotting
+        :param edis:
+        :return:
+        """
         if isinstance(edis, list):
             if all(isinstance(edi, mt.MT) for edi in edis):
-                self._edis = edis
+                self._data = edis
             else:
                 # todo: raise an exception
                 pass
         elif isinstance(edis, mt.MT):
             # if its a single mt object, put it in an list
-            self._edis = [edis]
+            self._data = [edis]
         else:
             # todo raise an exception
             pass
 
+    def _set_edi(self, edi):
+        """
+        set a single data source as the source for plotting
+        :param edi:
+        :return:
+        """
+        if isinstance(edi, mt.MT):
+            # if its a single mt object, put it in an list
+            self._data = edi
+        else:
+            # todo raise an exception
+            pass
+
+
+    def clear(self):
+        """
+        clear the figure
+        :return:
+        """
+        # todo decide if this is necessory
+        raise NotImplemented
