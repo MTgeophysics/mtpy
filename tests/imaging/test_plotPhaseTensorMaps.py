25bb2cb268e2942dafe1caea11c70e5e43962b1f
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-07-11 16:06:54 +1000

    testing case using mtpy.core.z.Z as the input of PhaseTensorMaps
    issue:
    constructor of the plotting class does not initialize lon and lat when the input is not a file path

--- tests/imaging/test_plotPhaseTensorMaps.py
+++ tests/imaging/test_plotPhaseTensorMaps.py
@@ -8,15 +8,19 @@ import os.path
 # configure matplotlib for testing
 import matplotlib
 # matplotlib.use('Agg')  # comment out this line if you want to see the plots 1-by-1 on screen.
+from mtpy.imaging.penetration import load_edi_files
 from mtpy.imaging.phase_tensor_maps import PlotPhaseTensorMaps
+
 # use non-interactive backend 'Agg', so that you do not have to see figure pop-out.
 
 edi_paths = [
     "",
     "tests\\data\\edifiles",
     "../MT_Datasets/3D_MT_data_edited_fromDuanJM/",
-    "../MT_Datasets/GA_UA_edited_10s-10000s/"
+    "../MT_Datasets/GA_UA_edited_10s-10000s/",
+    "tests\\data\\edifiles2"
 ]
+
 
 class TestPlotPhaseTensorMaps(TestCase):
     def setUp(self):
@@ -98,6 +102,44 @@ class TestPlotPhaseTensorMaps(TestCase):
                        "%s.png" % inspect.currentframe().f_code.co_name,
                        "params_%s" % inspect.currentframe().f_code.co_name)
 
+    @unittest.skipUnless(os.path.isdir(edi_paths[3]), "data file not found")
+    def test_plot_04_01(self):
+        edi_path = edi_paths[4]
+        freq = 1
+        self._plot_gen(edi_path, freq,
+                       "%s.png" % inspect.currentframe().f_code.co_name,
+                       "params_%s" % inspect.currentframe().f_code.co_name)
+
+    @unittest.skipUnless(os.path.isdir(edi_paths[3]), "data file not found")
+    def test_plot_04_02(self):
+        """
+        testing to use Z and tipper objects as input
+        :return:
+        """
+        edi_path = edi_paths[4]
+        freq = 1
+        mt_objs = load_edi_files(edi_path)
+        z_objs = [mt.Z for mt in mt_objs]
+        tipper = [mt.Tipper for mt in mt_objs]
+        save_figure_path = os.path.join(self._temp_dir, "%s.png" % inspect.currentframe().f_code.co_name)
+        save_param_path = os.path.join(self._temp_dir, "params_%s" % inspect.currentframe().f_code.co_name)
+        pt_obj = PlotPhaseTensorMaps(z_object_list=z_objs,
+                                     tipper_object_list=tipper,
+                                     plot_freq=freq,
+                                     ftol=0.10,  # freq tolerance,which will decide how many data points included
+                                     mapscale='deg',  # deg or m, or km
+                                     xpad=0.4,  # plot margin; change according to lat-lon in edifiles
+                                     ypad=0.4,  # ~ 2* ellipse size
+                                     ellipse_dict=self.ellipse_dict,
+                                     plot_tipper='yr',
+                                     arrow_dict=self.arrow_dict,
+                                     # arrow_legend_dict=arrow_legend_dict,
+                                     # fig_spython examples/plot_phase_tensor_map.py tests/data/edifiles/ 10 /e/MTPY2_Outputs/ptmap3deg.pngize=(6, 5),
+                                     # fig_dpi=300, the default is OK. Higher dpi
+                                     # may distort figure
+                                     save_fn=save_figure_path)
+        path2figure = pt_obj.plot(save_path=save_figure_path)
+        pt_obj.export_params_to_file(save_path=save_param_path)
 
     def _plot_gen(self, edi_path, freq, save_figure_path=None, save_param_path=None):
         edi_file_list = glob.glob(os.path.join(edi_path, "*.edi"))
