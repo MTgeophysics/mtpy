f50fdffa463ee01105bf5a8c82892fbf9ed5bb4c
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-07-24 17:07:07 +1000

    updated

--- mtpy/gui/SmartMT/gui/export_dialog.py
+++ mtpy/gui/SmartMT/gui/export_dialog.py
@@ -23,7 +23,7 @@ class ExportDialog(QtGui.QDialog):
 
         # setup file types
         self._formats = []
-        filetypes = plt.gcf().canvas.get_supported_filetypes() # this may need to be set everytime
+        filetypes = plt.gcf().canvas.get_supported_filetypes()  # this may need to be set everytime
         for type, description in filetypes.iteritems():
             self.ui.comboBox_fileType.addItem("%s (.%s)" % (description, type))
             self._formats.append((type, description))
@@ -31,37 +31,26 @@ class ExportDialog(QtGui.QDialog):
         # setup directory and dir dialog
         self._dir_dialog = QtGui.QFileDialog(self)
         # self._dir_dialog.setDirectory(os.path.expanduser("~"))
-        self._dir_dialog.setFileMode(QtGui.QFileDialog.AnyFile)
+        self._dir_dialog.setFileMode(QtGui.QFileDialog.DirectoryOnly)
         self._dir_dialog.setWindowTitle("Save to ...")
         self.ui.comboBox_directory.addItem(os.path.expanduser("~"))
         self.ui.pushButton_browse.clicked.connect(self._browse)
 
         # file name
-        self.ui.comboBox_fileName
+        self.ui.comboBox_fileName.currentIndexChanged.connect(self._file_name_changed)
+
+    def _file_name_changed(self, *args, **kwargs):
+        filename = str(self.ui.comboBox_fileName.currentText())
+        filename, extension = os.path.splitext(filename)
+        extension = extension[1:]  # get ride of .
+        # check if the extension is supported
+        index = [i for i, (ext, dsc) in enumerate(self._formats) if ext == extension]
+        if index:
+            self.ui.comboBox_fileType.setCurrentIndex(index[0])
 
     def _browse(self, *args, **kwargs):
         if self._dir_dialog.exec_() == QtGui.QDialog.Accepted:
             directory = str(self._dir_dialog.selectedFiles()[0])
-            if os.path.isfile(directory):
-                filename = os.path.basename(directory)
-                filename, extension = os.path.splitext(filename)
-                extension = extension[1:]  # get ride of .
-                directory = os.path.dirname(directory)
-                # check if the extension is supported
-                index = [i for i, (ext, dsc) in enumerate(self._formats) if ext == extension]
-                if index:
-                    self.ui.comboBox_fileType.setCurrentIndex(index[0])
-                else:
-                    # use the currently selected extension instead
-                    extension = self._formats[self.ui.comboBox_fileType.currentIndex()][0]
-                filename = "%s.%s" % (filename, extension)
-                # update file name
-                index = self.ui.comboBox_fileName.findText(filename)
-                if index == -1:
-                    self.ui.comboBox_fileName.addItem(filename)
-                self.ui.comboBox_fileName.setCurrentIndex(index
-                                                          if index >= 0
-                                                          else self.ui.comboBox_fileName.findText(filename))
             # update directory
             index = self.ui.comboBox_directory.findText(directory)
             if index == -1:
@@ -70,7 +59,16 @@ class ExportDialog(QtGui.QDialog):
                                                        if index >= 0
                                                        else self.ui.comboBox_directory.findText(directory))
 
-
-
     def show_dialog(self, fig):
         self.show()
+
+    def keyPressEvent(self, event):
+        """Capture and ignore all key press events.
+
+        This is used so that return key event does not trigger any button
+        from the dialog. We need to allow the return key to be used in filters
+        in the widget."""
+        if event.key() == QtCore.Qt.Key_Escape:
+            # call reject if Escape is pressed.
+            self.reject()
+        pass
