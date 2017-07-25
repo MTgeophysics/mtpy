076a484d766989ad9872ca82c56ab00ce0240086
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-07-25 12:39:39 +1000

    updated implementation, added unit test for export dialog

--- mtpy/gui/SmartMT/gui/export_dialog.py
+++ mtpy/gui/SmartMT/gui/export_dialog.py
@@ -48,6 +48,14 @@ class ExportDialog(QtGui.QDialog):
         # export button
         self.ui.pushButton_export.clicked.connect(lambda b: self.accept())
 
+        # message box for when the file already exist
+        self._msg_box = QtGui.QMessageBox()
+        self._msg_box.setWindowTitle("Export...")
+        self._msg_box.button_overwrite = self._msg_box.addButton(self.tr("Overwrite"), QtGui.QMessageBox.AcceptRole)
+        self._msg_box.button_save_as = self._msg_box.addButton(self.tr("Save As"), QtGui.QMessageBox.ActionRole)
+        self._msg_box.button_cancel = self._msg_box.addButton(QtGui.QMessageBox.Cancel)
+        self._msg_box.setDefaultButton(self._msg_box.button_save_as)
+
     _orientation = ['portrait', 'landscape']
 
     def _file_type_changed(self, *args, **kwargs):
@@ -72,6 +80,12 @@ class ExportDialog(QtGui.QDialog):
         index = [i for i, (ext, dsc) in enumerate(self._formats) if ext == extension]
         if index:
             self.ui.comboBox_fileType.setCurrentIndex(index[0])
+        elif extension:
+            # no extension
+            pass
+        else:
+            # extension not supported:
+            pass
 
     def _browse(self, *args, **kwargs):
         if self._dir_dialog.exec_() == QtGui.QDialog.Accepted:
@@ -85,28 +99,59 @@ class ExportDialog(QtGui.QDialog):
                                                        else self.ui.comboBox_directory.findText(directory))
 
     def export_to_file(self, fig):
-        if self.exec_() == QtGui.QDialog.Accepted:
+        response = self.exec_()
+        if response == QtGui.QDialog.Accepted:
             # saving files
-            fname = self.get_save_file_namee()
+
+            fname = self.get_save_file_name()
+
+            if os.path.exists(fname):
+                new_name = generate_unique_file_name(fname)
+                self._show_file_exist_message(fname, new_name)
+                if self._msg_box.clickedButton() == self._msg_box.button_cancel:
+                    return  # todo go back to the export window
+                elif self._msg_box.clickedButton() == self._msg_box.button_save_as:
+                    fname = new_name  # save_as
+                else:
+                    pass  # use the original name to overwrite
+
             params = self.get_savefig_params()
             fig.savefig(fname, **params)
+            return fname
+            # elif response == QtGu
 
-    def get_save_file_namee(self):
-        return os.path.join(
+    def _show_file_exist_message(self, fname, new_name):
+        self._msg_box.setText(
+            "<p>File \"{0}\" already exists. Do you want to overwrite the existing, or save to \"{1}\" instead?<\p>".format(
+                fname, new_name))
+        self._msg_box.exec_()
+
+    def get_save_file_name(self):
+        name = os.path.join(
             str(self.ui.comboBox_directory.currentText()),
             str(self.ui.comboBox_fileName.currentText())
-        )  # todo check if directory and/or file exists
+        )
+        return name
 
     def get_savefig_params(self):
         # todo error checking
         params = {
             'dpi': self.ui.spinBox_dpi.value(),
-            'orientation': self._orientation[self.ui.comboBox_orientation.currentIndex()],
-            # 'format': self._formats[self.ui.comboBox_fileType.currentIndex()],
+            'orientation': self.get_orientation(),
+            'format': self.get_file_format()[0],
             'transparent': self.ui.checkBox_transparent.isChecked(),
-            'bbox_inches': 'tight' if self.ui.checkBox_tightBbox.isChecked() else None
+            'bbox_inches': self.get_bbox_inches()
         }
         return params
+
+    def get_file_format(self):
+        return self._formats[self.ui.comboBox_fileType.currentIndex()]
+
+    def get_bbox_inches(self):
+        return 'tight' if self.ui.checkBox_tightBbox.isChecked() else None
+
+    def get_orientation(self):
+        return self._orientation[self.ui.comboBox_orientation.currentIndex()]
 
     def keyPressEvent(self, event):
         """Capture and ignore all key press events.
@@ -118,3 +163,13 @@ class ExportDialog(QtGui.QDialog):
             # call reject if Escape is pressed.
             self.reject()
         pass
+
+
+def generate_unique_file_name(basename):
+    name, ext = os.path.splitext(basename)
+    counter = 1  # start from 2 as the first extension
+    new_name = basename
+    while os.path.exists(new_name):
+        counter += 1
+        new_name = "{0}_{1}{2}".format(name, counter, ext)
+    return new_name
