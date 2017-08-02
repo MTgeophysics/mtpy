2e0d33aa86afb70cd0765d2b8a514088b929cf11
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-08-02 15:15:00 +1000

    bug fix in test

--- tests/modeling/test_data.py
+++ tests/modeling/test_data.py
@@ -49,75 +49,70 @@ edi_paths = [
 epsg_code = 28354
 epsg_code = 3112
 
-error_types = ['floor', 'value', 'egbert', 'floor_egbert']
+error_types = [
+    'floor',
+    'value',
+    'egbert',
+    'floor_egbert'
+]
 
 
-def _test_gen(index, edi_path):
+def _test_gen(index, edi_path, error_type):
     """
     generate list of tests for the given edi path
     :param index:
     :param edi_path:
     :return:
     """
-    tests = []
+    def test_func(self):
+        if not os.path.isdir(edi_path):
+            # input file does not exist, skip test after remove the output dir
+            os.rmdir(self._output_dir)
+            self.skipTest("edi path does not exist: {}".format(edi_path))
 
-    for error_type in error_types:
-        def test_func(self):
-            if not os.path.isdir(edi_path):
-                # input file does not exist, skip test after remove the output dir
-                os.rmdir(self._output_dir)
-                self.skipTest("edi path does not exist: {}".format(edi_path))
+        edi_list = glob.glob(edi_path + '/*.edi')
+        period_list = select_periods(edi_list)
+        datob = Data(edi_list=edi_list,
+                     inv_mode='1',
+                     period_list=period_list,
+                     epsg=epsg_code,
+                     error_type=error_type,
+                     error_floor=10)
+        datob.write_data_file(save_path=self._output_dir)
 
-            edi_list = glob.glob(edi_path + '/*.edi')
-            period_list = select_periods(edi_list)
-            datob = Data(edi_list=edi_list,
-                         inv_mode='1',
-                         period_list=period_list,
-                         epsg=epsg_code,
-                         error_type=error_type,
-                         error_floor=10)
-            datob.write_data_file(save_path=self._output_dir)
-
-            # check the output
-            if self._expected_output_dir:
-                output_data_file = os.path.normpath(os.path.join(self._output_dir, "ModEM_Data.dat"))
-                self.assertTrue(os.path.isfile(output_data_file), "output data file does not exist")
-                expected_data_file = os.path.normpath(os.path.join(self._expected_output_dir,
-                                                                   "ModEM_Data.dat"))
-                self.assertTrue(
-                    os.path.isfile(expected_data_file),
-                    "expected output data file does not exist, nothing to compare"
-                )
-
-                print "\ncomparing", output_data_file, "and", expected_data_file
-                with open(output_data_file, 'r') as output:
-                    with open(expected_data_file, 'r') as expected:
-                        diff = difflib.unified_diff(
-                            expected.readlines(),
-                            output.readlines(),
-                            fromfile='expected',
-                            tofile='output'
-                        )
-                        count = 0
-                        for line in diff:
-                            sys.stdout.write(line)
-                            count += 1
-                        self.assertTrue(count == 0, "output different!")
-            else:
-                print "no expected output exist, nothing to compare"
-
-        tests.append(
-            (
-                error_type,  # test name
-                test_func  # test func
+        # check the output
+        if self._expected_output_dir:
+            output_data_file = os.path.normpath(os.path.join(self._output_dir, "ModEM_Data.dat"))
+            self.assertTrue(os.path.isfile(output_data_file), "output data file does not exist")
+            expected_data_file = os.path.normpath(os.path.join(self._expected_output_dir,
+                                                               "ModEM_Data.dat"))
+            self.assertTrue(
+                os.path.isfile(expected_data_file),
+                "expected output data file does not exist, nothing to compare"
             )
-        )
-    return tests
+
+            print "\ncomparing", output_data_file, "and", expected_data_file
+            with open(output_data_file, 'r') as output:
+                with open(expected_data_file, 'r') as expected:
+                    diff = difflib.unified_diff(
+                        expected.readlines(),
+                        output.readlines(),
+                        fromfile='expected',
+                        tofile='output'
+                    )
+                    count = 0
+                    for line in diff:
+                        sys.stdout.write(line)
+                        count += 1
+                    self.assertTrue(count == 0, "output different!")
+        else:
+            print "no expected output exist, nothing to compare"
+    return test_func
 
 
 # generate tests
 for index, edi_path in enumerate(edi_paths):
-    tests = _test_gen(index, edi_path)
-    for name, test_func in tests:
-        test_func.__name__ = "test_{}_{}_{}".format(index + 1, os.path.basename(edi_path), name)
+    for error_type in error_types:
+        test_func = _test_gen(index, edi_path, error_type)
+        test_func.__name__ = "test_{}_{}_{}".format(index + 1, os.path.basename(edi_path), error_type)
         setattr(TestData, test_func.__name__, test_func)
