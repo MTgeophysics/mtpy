fix to interpolate function, also fix to the calculation of number of stations and number of periods when creating data file

mtpy/core/mt.py
@@ -674,15 +674,14 @@ class MT(object):
                 non_zero = np.nonzero(self.Z.z[:,ii,jj])
                 ind = np.argsort(self.Z.freq[non_zero])
                 if len(ind) > 0:
-                    print "indices z",ind
-                    z_func_real = spi.interp1d(np.log10(self.Z.freq[ind]), self.Z.z[ind][:, ii, jj].real,
+                    z_func_real = spi.interp1d(np.log10(self.Z.freq[non_zero][ind]), self.Z.z[non_zero][ind][:, ii, jj].real,
                                                kind=interpkind, bounds_error=False, fill_value=0.)
-                    z_func_imag = spi.interp1d(np.log10(self.Z.freq[ind]), self.Z.z[ind][:, ii, jj].imag,
+                    z_func_imag = spi.interp1d(np.log10(self.Z.freq[non_zero][ind]), self.Z.z[non_zero][ind][:, ii, jj].imag,
                                                kind=interpkind, bounds_error=False, fill_value=0.)
                     new_Z.z[:, ii, jj] = z_func_real(np.log10(new_freq_array)) + \
                         1j * z_func_imag(np.log10(new_freq_array))
     
-                    z_func_err = spi.interp1d(np.log10(self.Z.freq[ind]), self.Z.z_err[ind][:, ii, jj],
+                    z_func_err = spi.interp1d(np.log10(self.Z.freq[non_zero][ind]), self.Z.z_err[ind][:, ii, jj],
                                               kind=interpkind, bounds_error=False)
                     new_Z.z_err[:, ii, jj] = z_func_err(np.log10(new_freq_array))
 
@@ -696,17 +695,16 @@ class MT(object):
             ind = np.argsort(self.Z.freq[non_zero])
 
             if len(ind) > 0:
-                print "indices tip",ind
-                t_func_real = spi.interp1d(np.log10(self.Z.freq[ind]),
-                                           self.Tipper.tipper[ind][:, 0, jj].real,
+                t_func_real = spi.interp1d(np.log10(self.Z.freq[non_zero][ind]),
+                                           self.Tipper.tipper[non_zero][ind][:, 0, jj].real,
                                            kind=interpkind, bounds_error=False, fill_value=0.)
-                t_func_imag = spi.interp1d(np.log10(self.Z.freq[ind]),
-                                           self.Tipper.tipper[ind][:, 0, jj].imag,
+                t_func_imag = spi.interp1d(np.log10(self.Z.freq[non_zero][ind]),
+                                           self.Tipper.tipper[non_zero][ind][:, 0, jj].imag,
                                            kind=interpkind, bounds_error=False, fill_value=0.)
                 new_Tipper.tipper[:, 0, jj] = t_func_real(np.log10(new_freq_array)) + \
                     1j * t_func_imag(np.log10(new_freq_array))
     
-                t_func_err = spi.interp1d(np.log10(self.Z.freq[ind]),
+                t_func_err = spi.interp1d(np.log10(self.Z.freq[non_zero][ind]),
                                           self.Tipper.tipper_err[ind][:, 0, jj],
                                           kind=interpkind, bounds_error=False)
                 new_Tipper.tipper_err[

mtpy/modeling/modem_data.py
@@ -956,9 +956,6 @@ class Data(object):
         # reset the header string to be informational
         self._set_header_string()
 
-        # number of periods - subtract periods with all zero components
-        nper = len(np.where(np.mean(
-            np.mean(np.mean(np.abs(self.data_array['z']), axis=0), axis=1), axis=1) > 0)[0])
 
         dlines = []
         for inv_mode in self.inv_mode_dict[self.inv_mode]:
@@ -971,15 +968,19 @@ class Data(object):
                 dlines.append('> exp({0}i\omega t)\n'.format(
                     self.wave_sign_impedance))
                 dlines.append('> {0}\n'.format(self.units))
+                nper = len(np.nonzero(np.abs(self.data_array['z']).sum(axis=(1,2,3)))[0])
+                nsta = len(np.nonzero(np.abs(self.data_array['z']).sum(axis=(0,2,3)))[0])
             elif inv_mode.find('Vertical') >= 0:
                 dlines.append('> exp({0}i\omega t)\n'.format(
                     self.wave_sign_tipper))
                 dlines.append('> []\n')
+                nper = len(np.nonzero(np.abs(self.data_array['tip']).sum(axis=(1,2,3)))[0])
+                nsta = len(np.nonzero(np.abs(self.data_array['tip']).sum(axis=(0,2,3)))[0])
             dlines.append('> 0.00\n')  # oriention, need to add at some point
             dlines.append('> {0: >10.6f} {1:>10.6f}\n'.format(
                 self.center_position[1], self.center_position[0]))  # (lat,long) correct order
             dlines.append('> {0} {1}\n'.format(nper,
-                                               self.data_array['z'].shape[0]))
+                                               nsta))
 
             # YG: create new list for sorting data
             data_lines = []