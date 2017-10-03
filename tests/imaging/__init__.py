308e70e5289a496a47c3e92612813131498126d6
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-10-04 09:29:04 +1100

    ALAMP-74 changed matplotlib backend for travis

--- tests/imaging/__init__.py
+++ tests/imaging/__init__.py
@@ -2,8 +2,8 @@ import os
 import matplotlib
 
 if os.name == "posix" and 'DISPLAY' not in os.environ:
-    print("MATPLOTLIB: No Display found, using non-interactive svg backend")
-    matplotlib.use('svg')
+    print("MATPLOTLIB: No Display found, using non-interactive agg backend")
+    matplotlib.use('agg')
     import matplotlib.pyplot as plt
 else:
     # matplotlib.use('svg')
