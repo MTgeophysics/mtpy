133538ffed205c845918dc67a96f2712bfacee58
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-10-04 13:53:39 +1100

    ALAMP-74 updated baseline image

--- tests/imaging/__init__.py
+++ tests/imaging/__init__.py
@@ -1,8 +1,12 @@
+from __future__ import print_function
+
 import os
 import matplotlib
 
+import sys
+
 if os.name == "posix" and 'DISPLAY' not in os.environ:
-    print("MATPLOTLIB: No Display found, using non-interactive agg backend")
+    print("MATPLOTLIB: No Display found, using non-interactive agg backend", sys.stderr)
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
 else:
