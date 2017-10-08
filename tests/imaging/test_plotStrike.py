1abc2bbc49413fade49a23bb2e27efec6d9b4ad9
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-10-09 10:28:37 +1100

    ALAMP-74 enabled image comparison tests

--- tests/imaging/test_plotStrike.py
+++ tests/imaging/test_plotStrike.py
@@ -6,87 +6,84 @@ from unittest import TestCase
 
 # configure matplotlib for testing
 import matplotlib.pyplot as plt
+import pytest
 
 from mtpy.imaging.plotstrike import PlotStrike
+from mtpy.utils.decorator import ImageCompare
+from tests.imaging import ImageTestCase
 
 plt.ion()
 
 edi_paths = [
-    "",
     "tests/data/edifiles",
     "examples/data/edi2",
     "examples/data/edi_files",
-    "../MT_Datasets/3D_MT_data_edited_fromDuanJM/",
-    "../MT_Datasets/GA_UA_edited_10s-10000s/",
+    "../MT_Datasets/3D_MT_data_edited_fromDuanJM",
+    "../MT_Datasets/GA_UA_edited_10s-10000s",
     "tests/data/edifiles2"
 ]
 
 
-class TestPlotStrike(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls._temp_dir = "tests/temp"
-        if not os.path.isdir(cls._temp_dir):
-            os.mkdir(cls._temp_dir)
-
-    @classmethod
-    def tearDownClass(cls):
-        plt.close('all')
-
+class TestPlotStrike(ImageTestCase):
     def setUp(self):
         plt.clf()
 
-    def test_plot_01(self):
-        edi_path = edi_paths[1]
-        self._plot(edi_path, "%s.png" % inspect.currentframe().f_code.co_name)
 
-    @unittest.skipUnless(os.path.isdir(edi_paths[2]), "data file not found")
-    def test_plot_02(self):
-        edi_path = edi_paths[2]
-        self._plot(edi_path,
-                   "%s.png" % inspect.currentframe().f_code.co_name)
-
-    @unittest.skipUnless(os.path.isdir(edi_paths[3]), "data file not found")
-    def test_plot_03(self):
-        edi_path = edi_paths[3]
-        self._plot(edi_path,
-                   "%s.png" % inspect.currentframe().f_code.co_name)
-
-    @unittest.skipUnless(os.path.isdir(edi_paths[4]), "data file not found")
-    def test_plot_04(self):
-        edi_path = edi_paths[4]
-        self._plot(edi_path,
-                   "%s.png" % inspect.currentframe().f_code.co_name)
-
-    @unittest.skipUnless(os.path.isdir(edi_paths[5]), "data file not found")
-    def test_plot_05(self):
-        edi_path = edi_paths[5]
-        self._plot(edi_path,
-                   "%s.png" % inspect.currentframe().f_code.co_name)
-
-    def _plot(self, edi_path, save_figure_path):
+def test_gen(edi_path):
+    def default(self):
         edi_file_list = glob.glob(os.path.join(edi_path, "*.edi"))
-        save_figure_path = os.path.join(self._temp_dir, save_figure_path)
-        pt_obj = PlotStrike(fn_list=edi_file_list, plot_yn='n')
+        pt_obj = PlotStrike(fn_list=edi_file_list, plot_yn='n', save_figure_path=self._temp_dir)
         pt_obj.plot()
         plt.pause(1)
+        save_figure_name = "{}.png".format(default.__name__)
+        save_figure_path = os.path.join(self._temp_dir, save_figure_name)
+        pt_obj.save_plot(save_figure_path, file_format='png', close_plot='n')
+        assert (os.path.isfile(save_figure_path))
 
+    def rotation(self):
+        edi_file_list = glob.glob(os.path.join(edi_path, "*.edi"))
         # change rotation
         pt_obj = PlotStrike(fn_list=edi_file_list, plot_yn='n', rot_z=90)
         pt_obj.plot()
         plt.pause(1)
 
+    def type(self):
+        edi_file_list = glob.glob(os.path.join(edi_path, "*.edi"))
         # plot type
         pt_obj = PlotStrike(fn_list=edi_file_list, plot_yn='n', plot_type=1)
         pt_obj.plot()
         plt.pause(1)
 
+    def tipper(self):
+        edi_file_list = glob.glob(os.path.join(edi_path, "*.edi"))
         # plot_tipper
         pt_obj = PlotStrike(fn_list=edi_file_list, plot_yn='n', plot_tipper='y')
         pt_obj.plot()
         plt.pause(1)
 
+    def fold(self):
+        edi_file_list = glob.glob(os.path.join(edi_path, "*.edi"))
         # fold
         pt_obj = PlotStrike(fn_list=edi_file_list, plot_yn='n', fold=False)
         pt_obj.plot()
         plt.pause(1)
+
+    return default, rotation, type, tipper, fold
+
+
+# generate tests
+for edi_path in edi_paths:
+    if os.path.isdir(edi_path):
+        test_name = os.path.basename(edi_path)
+        for test_func in test_gen(edi_path):
+            test_func.__name__ = "test_{test_name}_{plot_name}".format(
+                test_name=test_name, plot_name=test_func.__name__)
+            setattr(
+                TestPlotStrike,
+                test_func.__name__,
+                ImageCompare(fig_size=(8, 6), savefig_kwargs={"dpi": 100}).__call__(test_func))
+
+if 'test_gen' in globals():
+    del globals()['test_gen']
+if 'test_func' in globals():
+    del globals()['test_func']
