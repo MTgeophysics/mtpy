fc6d1329dfe839614beae6e8de5d607f47efd36a
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-10-11 09:52:31 +1100

    ALAMP-81 updated travis config

--- bin/install_libgdal.sh
+++ bin/install_libgdal.sh
@@ -9,12 +9,14 @@ set -ex
 if [ ! -d "$BUILDING_DIR" ]; then
     mkdir "$BUILDING_DIR"
 fi
-if [ ! -f "$BUILDING_DIR/gdal-$GDAL_VERSION.tar.gz" ]; then
-    wget "http://download.osgeo.org/gdal/CURRENT/gdal-$GDAL_VERSION.tar.gz" -O "$BUILDING_DIR/gdal-$GDAL_VERSION.tar.gz"
-    tar xvfz "gdal-$GDAL_VERSION.tar.gz"
+pushd "$BUILDING_DIR"
+if [ ! -f "gdal-$GDAL_VERSION.tar.gz" ]; then
+    wget "http://download.osgeo.org/gdal/CURRENT/gdal-$GDAL_VERSION.tar.gz" -O "gdal-$GDAL_VERSION.tar.gz"
 fi
-pushd "$BUILDING_DIR/gdal-$GDAL_VERSION"
+tar xvfz "gdal-$GDAL_VERSION.tar.gz"
+pushd "gdal-$GDAL_VERSION"
 ./configure --with-python
 make
 sudo make install
+popd
 popd
