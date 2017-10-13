89d0ea9f37ca9a123f17d9d180bfa6cfb93d6f72
Fei Zhang <fei.zhang@ga.gov.au>
2017-10-13 16:06:19 +1100

    ALAMP-31 skeleton tests ready for further refinement

--- tests/modeling/test_modemSlices.py
+++ tests/modeling/test_modemSlices.py
@@ -11,20 +11,18 @@ Developer:      fei.zhang@ga.gov.au
 
 Revision History:
     LastUpdate:     13/10/2017   FZ
-    LastUpdate:     dd/mm/yyyy  Who     Optional description
+    LastUpdate:
 """
 
 # import section
 import os
-
-# Section to define functions or class
 from unittest import TestCase
+from mtpy.modeling.modem_output_to_views import ModemSlices
 
 class TestModemSlices(TestCase):
 
-
     def test_find_stations_in_meshgrid(self):
-        self.assertTrue(2 > 1, msg='Dummy test')
+        self.assertTrue(2 > 1, msg='Dummy True test')
 
     def test_get_slice_data(self):
         self.assertTrue(2 > 1, msg='Dummy test')
