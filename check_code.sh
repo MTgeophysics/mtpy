463f8449950d23f3cdaac64d4217fd7f702a18f1
Fei Zhang <fei.zhang@ga.gov.au>
2017-10-19 14:49:06 +1100

    update check_code.sh to include coveralls

--- check_code.sh
+++ check_code.sh
@@ -4,16 +4,20 @@
 #set -eu
 #set -x
 
-#pep8 tests mtpy tests examples --max-line-length 120
-pep8 tests  exp  --max-line-length 120
+#pep8 tests mtpy  --max-line-length 120
+pep8 mtpy --max-line-length 120
 
 echo "pylint .... "
 #pylint -j 2 --reports=no tests  #j2 cpu run no report
-pylint -E tests
+pylint -E mtpy
 echo "pylint ....end "
 
+# do pytest and generate coverage report
 py.test tests
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
 #? py.test -r sx --cov datacube --durations=5 datacube tests datacube_apps $@
 
+# send/publish coverage stats to coveralls.io
+# and get an URL like https://coveralls.io/jobs/30383099
+# coveralls
