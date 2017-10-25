871ebdbd8e132074101c40d6b669bde11f1340ee
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-10-25 13:22:21 +1100

    ALAMP-95 Updated resources for pyqt5 and pyqt4

--- mtpy/gui/SmartMT/ui_asset/__init__.py
+++ mtpy/gui/SmartMT/ui_asset/__init__.py
@@ -1,8 +1,12 @@
 import os
 import sys
 from qtpy.uic import compileUiDir
-
+from qtpy import QT_VERSION
 _dir = os.path.dirname(__file__)
-_resource_suffix = "_py{python_version}_rc".format(python_version=sys.version_info[0])
+
+if QT_VERSION.startswith("4"):
+    _resource_suffix = "_py{python_version}_qt4_rc".format(python_version=sys.version_info[0])
+else:
+    _resource_suffix = "_qt5_rc"
 compileUiDir(_dir, recurse=True, resource_suffix=_resource_suffix)
 del _dir, _resource_suffix
