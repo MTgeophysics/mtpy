db9ab8954f2efb5c34f86cd2d345817faac373c3
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-11-07 15:15:48 +1100

    ALAMP-98 combined tests to one test suite

--- tests/modeling/test_occam1d.py
+++ tests/modeling/test_occam1d.py
@@ -31,6 +31,7 @@ Current issue:
 import os
 
 import tests
+import numpy as np
 
 from unittest import TestCase
 
@@ -135,4 +136,54 @@ class TestOccam1D(TestCase):
             print msg
             self.assertTrue(is_identical)
 
+    def test_view_outputs(self):
+        # FZ's workdir
+        savepath = tests.TEST_TEMP_DIR
 
+        # model and data file names
+        modelfn = os.path.join(self._expected_output_dir, 'Model1D')
+        datafn = os.path.join(self._expected_output_dir, 'Occam1d_DataFile_DET.dat')
+
+        # list of all the 'smooth' files, exclude the log file
+        fn_list = np.array([ff for ff in os.listdir(self._expected_output_dir)])
+
+        # go to model results directory and find the latest iteration file
+        # iterfile = 'ITER_135.iter'
+        # respfile = 'ITER_135.resp'
+        iterfile = 'ITER_97.iter'
+        respfile = 'ITER_97.resp'
+
+        # get maximum iteration
+        iterfn = os.path.join(self._expected_output_dir, iterfile)
+        respfn = os.path.join(self._expected_output_dir, respfile)
+
+        # read in the model, don't need these lines to view the output but useful if you want to analyse the data
+        oc1m = mtoc1d.Model(model_fn=modelfn)
+        oc1m.read_iter_file(iterfn)
+
+        # read in the data file
+        oc1d = mtoc1d.Data(data_fn=datafn)
+        oc1d.read_data_file(data_fn=datafn)
+        oc1d.read_resp_file(resp_fn=respfn, data_fn=datafn)
+
+        # plot the model output
+        pr = mtoc1d.Plot1DResponse(data_te_fn=datafn,
+                                   data_tm_fn=datafn,
+                                   model_fn=modelfn,
+                                   resp_te_fn=respfn,
+                                   iter_te_fn=iterfn,
+                                   resp_tm_fn=respfn,
+                                   iter_tm_fn=iterfn,
+                                   depth_limits=(0, 1),
+
+                                   )
+        pr.axm.set_xlim(1e-1, 1e3)
+        pr.axr.set_ylim(1, 100)
+
+        p2file = os.path.join(savepath, 'occam1dplot.png')
+        pr.save_figure(p2file, close_plot='n')
+
+        tests._plt_wait(1)
+        tests._plt_close()
+
+        assert (os.path.exists(p2file))
