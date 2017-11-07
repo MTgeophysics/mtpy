4321294cd32c2cac68410151d17e26936fdca584
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-11-07 16:56:08 +1100

    merged tests

--- tests/imaging/test_plot_phase_tensor_map.py
+++ tests/imaging/test_plot_phase_tensor_map.py
@@ -11,15 +11,11 @@ The ellipse properties are not being set via the arguments - need to create a
 phase_tensor_map object then set the properties then run redraw_plot.
 
 """
-import os
 import os.path as op
 
 import mtpy.imaging.plotptmaps as pptmaps
 from mtpy.core.mt import MT
 from tests.beta import *
-# import matplotlib.pyplot as plt
-# plt.ion()  # make figure disappear automatically:
-# plt.ioff()  # make figure show normally and need to click to close the figure to continue the proc
 from tests.imaging import ImageTestCase
 
 
@@ -72,6 +68,57 @@ class test_plotPhaseTensorMaps(ImageTestCase):
         phase_tensor_map.ellipse_size = 0.01
         phase_tensor_map.redraw_plot()
 
+        if save:
+            phase_tensor_map.save_figure(savepath, close_plot='n')
+            assert (os.path.exists(savepath))
+
+    def test_func(self):
+        """
+        test fun
+        :return:
+        """
+
+        # directory containing edis
+        edipath = EDI_DATA_DIR2
+        # whether or not to save the figure to file
+        save = True
+
+        # full path to file to save to
+        savepath = os.path.join(self._temp_dir, 'phase_tensor_map_2.png')
+
+        # frequency to plot
+        plot_freq = 1.318400e-01
+
+        # gets edi file names as a list
+        elst = [op.join(edipath, f) for f in os.listdir(edipath) if f.endswith('.edi')]
+        mtlist = [MT(ff) for ff in elst]
+
+        # parameters describing ellipses
+        ellipse_dict = {'ellipse_size': 0.1, 'ellipse_colorby': 'phimin', 'ellipse_range': (0, 90, 1), 'cmap': 'mt_bl2gr2rd'}
+
+        # parameters describing the induction vector arrows
+        arrow_dict = {'arrow_size': 0.02,
+                      'arrow_lw': 0.01,
+                      'arrow_head_width': 0.002,
+                      'arrow_head_length': 0.002,
+                      'arrow_color_real': 'b',
+                      'direction': 0,
+                      'threshold': 0.8}
+
+        phase_tensor_map = pptmaps.PlotPhaseTensorMaps(
+            # fn_list = elst,
+            mt_object_list=mtlist,
+            plot_freq=plot_freq,
+            #  ftol = .5,
+            #  xpad = 0.02,
+            plot_tipper='yr',
+            arrow_dict=arrow_dict,
+            ellipse_dict=ellipse_dict,
+        )
+
+        phase_tensor_map.ellipse_size = 0.5
+        phase_tensor_map.arrow_size = 10
+        phase_tensor_map.redraw_plot()
         if save:
             phase_tensor_map.save_figure(savepath, close_plot='n')
             assert (os.path.exists(savepath))
