90374484ca83e9a261fae12e7e90e9f8952925b1
Fei Zhang <fei.zhang@ga.gov.au>
2017-11-07 12:25:30 +1100

    fixed the unit test tests/beta/test_get_dimensionality_from_edi_file.py edi file path

--- tests/beta/test_get_dimensionality_from_edi_file.py
+++ tests/beta/test_get_dimensionality_from_edi_file.py
@@ -4,11 +4,12 @@ Created on Tue Oct 31 13:19:35 2017
 
 @author: u64125
 """
-
+import os
 import numpy as np
 
 import mtpy.analysis.geometry as mtg
 from mtpy.core.mt import MT
+from tests.beta import *
 
 
 def test_fun():
@@ -17,7 +18,9 @@ def test_fun():
     :return: T/F
     """
     # mtObj = MT(r'C:\Git\mtpy\examples\data\edi_files\pb42c.edi')
-    mtObj = MT(r'E:/Githubz/mtpy/examples/data/edi_files/pb42c.edi')
+
+    mtObj = MT(os.path.join(EDI_DATA_DIR, 'pb42c.edi'))
+
     dimensionality_result = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2,
                                       2, 2, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
 
@@ -26,7 +29,7 @@ def test_fun():
 
     print differ
 
-    assert np.all(np.abs(dimensionality - dimensionality_result) < 1e-8)
+    assert np.all(differ < 1e-8)
 
 if __name__ == "__main__":
     test_fun()
