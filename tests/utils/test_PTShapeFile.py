492304c59537ab69537bee629ea1b5110795937d
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-11-13 16:09:44 +1100

    updated test case for the shape file

--- tests/utils/test_PTShapeFile.py
+++ tests/utils/test_PTShapeFile.py
@@ -2,8 +2,10 @@ import inspect
 import os
 import unittest
 from unittest import TestCase
+import numpy as np
+import pytest
 
-from mtpy.utils.shapefiles import create_phase_tensor_shpfiles
+from mtpy.utils.shapefiles import create_phase_tensor_shpfiles, transform_ll_to_utm, transform_utm_to_ll
 from tests import TEST_TEMP_DIR
 
 edi_paths = [
@@ -15,6 +17,7 @@ edi_paths = [
     "../MT_Datasets/GA_UA_edited_10s-10000s/",
     "data/edifiles2"
 ]
+
 
 class TestPTShapeFile(TestCase):
     def setUp(self):
@@ -49,3 +52,28 @@ class TestPTShapeFile(TestCase):
         if not os.path.isdir(save_path):
             os.mkdir(save_path)
         create_phase_tensor_shpfiles(edi_path, save_path, ellipse_size=6000, every_site=1)
+
+
+class TestShapefileTools(TestCase):
+    def setUp(self):
+        self.nref = 23  # 23, "WGS-84"
+        self.lat = -34.3
+        self.lon = 149.2
+
+        self.zone = '55H'
+        self.easting = 702466.54318647704
+        self.northing = 9997808.8686107509
+
+    def test_transform_ll_to_utm(self):
+        zone, (easting, northing, elev) = transform_ll_to_utm(self.lat, self.lon, "WGS84")
+
+        self.assertTrue(zone, self.zone)
+        self.assertTrue(easting, self.easting)
+        self.assertTrue(northing, self.northing)
+
+    @pytest.mark.xfail
+    def test_transform_utm_to_ll(self):
+        new_lat, new_lon = transform_utm_to_ll(self.easting, self.northing, 55)
+        # this test fails due to unknown return type of transform_utm_to_ll
+        self.assertTrue(np.isclose(self.lat, new_lat))
+        self.assertTrue(np.isclose(self.lon, new_lon))
