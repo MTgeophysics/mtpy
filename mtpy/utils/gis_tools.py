87aff486b0a73545d01f5c3c783e33e23b1c7a26
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-11-14 10:37:01 +1100

    undated and fixed ll_to_utm() and gis_tool tests

--- mtpy/utils/gis_tools.py
+++ mtpy/utils/gis_tools.py
@@ -502,7 +502,7 @@ _ellipsoid = [
 
 @deprecated("This function may be removed in later release. mtpy.utils.gis_tools.project_point_ll2utm() should be "
             "used instead.")
-def ll_to_utm(reerence_ellipsoid, lat, long):
+def ll_to_utm(reference_ellipsoid, lat, lon):
     """
     converts lat/long to UTM coords.  Equations from USGS Bulletin 1532
     East Longitudes are positive, West longitudes are negative.
@@ -513,13 +513,12 @@ def ll_to_utm(reerence_ellipsoid, lat, long):
     Outputs:
         UTMzone, easting, northing"""
 
-    a = _ellipsoid[reerence_ellipsoid][_equatorial_radius]
-    ecc_squared = _ellipsoid[reerence_ellipsoid][_eccentricity_squared]
+    a = _ellipsoid[reference_ellipsoid][_equatorial_radius]
+    ecc_squared = _ellipsoid[reference_ellipsoid][_eccentricity_squared]
     k0 = 0.9996
 
     # Make sure the longitude is between -180.00 .. 179.9
-    long_temp = (long + 180) - int((long + 180) / 360) * \
-                               360 - 180  # -180.00 .. 179.9
+    long_temp = (lon + 180) - int((lon + 180) / 360) * 360 - 180  # -180.00 .. 179.9
 
     lat_rad = lat * _deg2rad
     long_rad = long_temp * _deg2rad
@@ -540,42 +539,52 @@ def ll_to_utm(reerence_ellipsoid, lat, long):
         elif 33.0 <= long_temp < 42.0:
             zone_number = 37
 
-    # +3 puts origin in middle of zone
-    long_origin = (zone_number - 1) * 6 - 180 + 3
+    long_origin = (zone_number - 1) * 6 - 180 + 3  # +3 puts origin in middle of zone
     long_origin_rad = long_origin * _deg2rad
 
     # compute the UTM Zone from the latitude and longitude
     utm_zone = "%d%c" % (zone_number, _utm_letter_designator(lat))
 
     ecc_prime_squared = ecc_squared / (1 - ecc_squared)
-    n = a / np.sqrt(1 - ecc_squared * np.sin(lat_rad) ** 2)
-    t = np.tan(lat_rad) * np.tan(lat_rad)
-    c = ecc_prime_squared * np.cos(lat_rad) ** 2
-    a = np.cos(lat_rad) * (long_rad - long_origin_rad)
+    N = a / np.sqrt(1 - ecc_squared * np.sin(lat_rad) ** 2)
+    T = np.tan(lat_rad) ** 2
+    C = ecc_prime_squared * np.cos(lat_rad) ** 2
+    A = np.cos(lat_rad) * (long_rad - long_origin_rad)
 
-    m = a * ((1 - ecc_squared / 4
-              - 3 * ecc_squared ** 2 / 64
-              - 5 * ecc_squared ** 3 / 256) * lat_rad
-             - (3 * ecc_squared / 8
-                + 3 * ecc_squared ** 2 / 32
-                + 45 * ecc_squared ** 3 / 1024) * np.sin(2 * lat_rad)
-             + (15 * ecc_squared ** 2 / 256 + 45 * ecc_squared ** 3 / 1024) * np.sin(4 * lat_rad)
-             - (35 * ecc_squared ** 3 / 3072) * np.sin(6 * lat_rad))
+    M = a * (
+        (1
+         - ecc_squared / 4
+         - 3 * ecc_squared ** 2 / 64
+         - 5 * ecc_squared ** 3 / 256) * lat_rad
+        - (3 * ecc_squared / 8
+           + 3 * ecc_squared ** 2 / 32
+           + 45 * ecc_squared ** 3 / 1024) * np.sin(2 * lat_rad)
+        + (15 * ecc_squared ** 2 / 256
+           + 45 * ecc_squared ** 3 / 1024) * np.sin(4 * lat_rad)
+        - (35 * ecc_squared ** 3 / 3072) * np.sin(6 * lat_rad))
 
-    utm_easting = (k0 * n * (a + (1 - t + c) * a ** 3 / 6
-                             + (5 - 18 * t ** 3 + 72 * c - 58 * ecc_prime_squared) * a ** 5 / 120)
+    utm_easting = (k0 * N * (A
+                             + (1 - T + C) * A ** 3 / 6
+                             + (5 - 18 * T
+                                + T ** 2
+                                + 72 * C
+                                - 58 * ecc_prime_squared) * A ** 5 / 120)
                    + 500000.0)
 
-    utm_northing = (k0 * (m + n * np.tan(lat_rad) * (a * a / 2 + (5 - t + 9 * c + 4 * c * c) * a * a * a * a / 24
-                                                     + (61
-                                                        - 58 * t
-                                                        + t ** 2
-                                                        + 600 * c
-                                                        - 330 * ecc_prime_squared) * a ** 6 / 720)))
+    utm_northing = (k0 * (M
+                          + N * np.tan(lat_rad) * (A ** 2 / 2
+                                                   + (5
+                                                      - T
+                                                      + 9 * C
+                                                      + 4 * C ** 2) * A ** 4 / 24
+                                                   + (61
+                                                      - 58 * T
+                                                      + T ** 2
+                                                      + 600 * C
+                                                      - 330 * ecc_prime_squared) * A ** 6 / 720)))
 
     if lat < 0:
-        # 10000000 meter offset for southern hemisphere
-        utm_northing = utm_northing + 10000000.0
+        utm_northing = utm_northing + 10000000.0  # 10000000 meter offset for southern hemisphere
     return utm_zone, utm_easting, utm_northing
 
 
--- tests/utils/test_gis_tools.py
+++ tests/utils/test_gis_tools.py
@@ -9,52 +9,65 @@ from mtpy.utils.gis_tools import ll_to_utm, utm_to_ll, project_point_ll2utm, pro
 class TestGisTools(TestCase):
     def setUp(self):
         self.nref = 23  # 23, "WGS-84"
-        self.lat = -34.3
-        self.lon = 149.2
+        self.lat = -34.299442
+        self.lon = 149.201031
 
         self.zone = '55H'
-        self.easting = 702466.54318647704
-        self.northing = 9997808.8686107509
+        self.easting = 702562.773
+        self.northing = 6202448.526
+        self.atol = 0.1  # tolerance of error
 
     def test_ll_to_utm(self):
         zone, easting, northing = ll_to_utm(self.nref, self.lat, self.lon)
 
-        self.assertTrue(zone, self.zone)
-        self.assertTrue(easting, self.easting)
-        self.assertTrue(northing, self.northing)
+        print(zone, easting, northing)
 
-    @pytest.mark.xfail
+        self.assertTrue(zone == self.zone)
+        self.assertTrue(np.isclose(easting, self.easting, atol=self.atol))
+        self.assertTrue(np.isclose(northing, self.northing, atol=self.atol))
+
     def test_utm_to_ll(self):
         # test UTM to LL
         new_lat, new_lon = utm_to_ll(self.nref, self.northing, self.easting, self.zone)
 
-        self.assertTrue(np.isclose(self.lat, new_lat))
-        self.assertTrue(np.isclose(self.lon, new_lon))
+        print(new_lat, new_lon)
+
+        self.assertTrue(np.isclose(self.lat, new_lat, atol=self.atol))
+        self.assertTrue(np.isclose(self.lon, new_lon, atol=self.atol))
 
     def test_project_point_ll2utm(self):
-        zone, easting, northing = project_point_ll2utm(self.lat, self.lon)
+        easting, northing, zone = project_point_ll2utm(self.lat, self.lon)
 
-        self.assertTrue(zone, self.zone)
-        self.assertTrue(easting, self.easting)
-        self.assertTrue(northing, self.northing)
+        print(zone, easting, northing)
 
-    @pytest.mark.xfail
+        self.assertTrue(zone == self.zone)
+        self.assertTrue(np.isclose(easting, self.easting))
+        self.assertTrue(np.isclose(northing, self.northing))
+
     def test_project_point_utm2ll(self):
         new_lat, new_lon = project_point_utm2ll(self.easting, self.northing, self.zone)
+
+        print(new_lat, new_lon)
 
         self.assertTrue(np.isclose(self.lat, new_lat))
         self.assertTrue(np.isclose(self.lon, new_lon))
 
     def test_transform_ll_to_utm(self):
-        zone, easting, northing = transform_ll_to_utm(self.lat, self.lon)
+        utm_cs, utm_point = transform_ll_to_utm(self.lon, self.lat)
 
-        self.assertTrue(zone, self.zone)
-        self.assertTrue(easting, self.easting)
-        self.assertTrue(northing, self.northing)
+        easting = utm_point[0]
+        northing = utm_point[1]
 
-    @pytest.mark.xfail
+        print(easting, northing)
+
+        # self.assertTrue(zone, self.zone)
+        self.assertTrue(np.isclose(easting, self.easting))
+        self.assertTrue(np.isclose(northing, self.northing))
+
     def test_transform_utm_to_ll(self):
-        new_lat, new_lon = transform_utm_to_ll(self.easting, self.northing, self.zone)
+        new_lon, new_lat, evel = transform_utm_to_ll(self.easting, self.northing, 55)
+
+        print(new_lat, new_lon)
 
         self.assertTrue(np.isclose(self.lat, new_lat))
         self.assertTrue(np.isclose(self.lon, new_lon))
