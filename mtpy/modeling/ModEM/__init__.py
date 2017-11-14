0502cf6949bdb962da4e56afbe0af03d58079adf
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-11-14 14:17:31 +1100

    moved Residual

--- mtpy/modeling/ModEM/__init__.py
+++ mtpy/modeling/ModEM/__init__.py
@@ -2,5 +2,6 @@ from .exception import ModEMError, DataError
 from .station import Stations
 from .data import Data
 from .model import Model
+from .residual import Residual
 
-__all__ = ['ModEMError', 'DataError', 'Stations', 'Data', 'Model']
+__all__ = ['ModEMError', 'DataError', 'Stations', 'Data', 'Model', 'Residual']
