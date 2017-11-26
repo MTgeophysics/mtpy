f95ff9b48641827aa94f790ff18970342119a83e
Fei Zhang <fei.zhang@ga.gov.au>
2017-11-26 14:57:15 +1100

    generalise the script to search for any file types (edi, py, pdf, jpg) specified.

--- mtpy/utils/find_edi_floders.py
+++ mtpy/utils/find_edi_floders.py
@@ -2,7 +2,8 @@
 # ! /usr/bin/env python
 """
 Description:
-    Find EDI files-containing directories - their full path
+    Find path to directories which containing a given type of files: .edi, .py .jpg, .pdf
+
 
 CreationDate:   26/11/2017
 Developer:      fei.zhang@ga.gov.au
@@ -16,9 +17,10 @@ import sys
 
 class Search4EdiFolders(object):
 
-    def __init__(self, startDir, edifiles_threshold=0):
+    def __init__(self, startDir, edifiles_threshold=0, filetype='edi'):
         self.startDir = startDir
         self.edifiles_threshold =edifiles_threshold
+        self.filetype =filetype
         self.edi_folders=[] # initial empty list
 
     def find_edi_folders(self, aStartDir):
@@ -32,36 +34,43 @@ class Search4EdiFolders(object):
 
             edi_files_count=0
             for fname in fileList:
-                if fname.endswith('edi'):
+                if fname.lower().endswith(self.filetype):
+                #if fname.endswith('.py'):
                     edi_files_count = edi_files_count+1
                 else:
                     pass
 
             if edi_files_count > self.edifiles_threshold:
-                print('Found EDI directory: %s ==> %s EDI files' %(dirName, edi_files_count))
+                print('Found directory: %s ==> %s *.%s files' %(dirName, edi_files_count,self.filetype))
                 self.edi_folders.append(dirName)
 
             return self.edi_folders
 
 
-########################################################
+########################################################, edi_files_count
 if __name__ =="__main__":
 
     if len(sys.argv)<=1:
-        print("USAGE: %s  %s [%s]" % (sys.argv[0], "start_path_dir", "edi_threshold"))
+        print("USAGE: %s  %s [%s] [%s]" % (sys.argv[0], "start_path_dir", "ftype", "edi_threshold"))
         sys.exit(1)
     else:
         root_dir=sys.argv[1]
 
         if len(sys.argv)>2:
-            edifiles_th = int(sys.argv[2])
+            ftype = sys.argv[2].lower()
+        else:
+            ftype='edi'
+
+        if len(sys.argv)>3:
+            edifiles_th = int(sys.argv[3])
         else:
             edifiles_th=0
 
 
-        sObj= Search4EdiFolders(root_dir, edifiles_threshold=edifiles_th)
+        sObj= Search4EdiFolders(root_dir, edifiles_threshold=edifiles_th, filetype=ftype)
+        #sObj= Search4EdiFolders(root_dir, edifiles_threshold=edifiles_th, filetype='py')
 
         edi_dirs_list =sObj.find_edi_folders(root_dir)
 
         print (edi_dirs_list)
-        print ("Number of EDI_folders found =", len(edi_dirs_list))
+        print ("Number of interesting folders found =", len(edi_dirs_list))
