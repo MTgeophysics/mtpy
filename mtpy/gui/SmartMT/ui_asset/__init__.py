ec4012b1f90c5fb5cb5504dee2daa2acd7337d7a
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-12-04 13:05:50 +1100

    updated building pyqt resource

--- mtpy/gui/SmartMT/ui_asset/__init__.py
+++ mtpy/gui/SmartMT/ui_asset/__init__.py
@@ -6,17 +6,22 @@ import os
 import sys
 import threading
 
-from qtpy.uic import compileUiDir
-from qtpy import QT_VERSION
-_dir = os.path.dirname(__file__)
-
+_GUI_RESOURCE_BUILT = False
 _thread_lock = threading.Lock()
 _thread_lock.acquire()
-if QT_VERSION.startswith("4"):
-    _resource_suffix = "_py{python_version}_qt4_rc".format(python_version=sys.version_info[0])
-else:
-    _resource_suffix = "_qt5_rc"
-compileUiDir(_dir, recurse=True, resource_suffix=_resource_suffix)
-del _dir, _resource_suffix
+
+if not _GUI_RESOURCE_BUILT:
+    from qtpy.uic import compileUiDir
+    from qtpy import QT_VERSION
+    _dir = os.path.dirname(__file__)
+
+    if QT_VERSION.startswith("4"):
+        _resource_suffix = "_py{python_version}_qt4_rc".format(python_version=sys.version_info[0])
+    else:
+        _resource_suffix = "_qt5_rc"
+    compileUiDir(_dir, recurse=True, resource_suffix=_resource_suffix)
+    del _dir, _resource_suffix
+    _GUI_RESOURCE_BUILT = True
+
 _thread_lock.release()
 del _thread_lock
