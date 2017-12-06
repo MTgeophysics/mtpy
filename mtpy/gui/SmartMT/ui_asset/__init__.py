a34d16173b6518b90c6192dc3268f32967366294
Yingzhi Gou <Yingzhi.Gou@ga.gov.au>
2017-12-06 12:17:53 +1100

    gui update

--- mtpy/gui/SmartMT/ui_asset/__init__.py
+++ mtpy/gui/SmartMT/ui_asset/__init__.py
@@ -6,19 +6,27 @@ import os
 import sys
 import threading
 
+from mtpy import MtPyLog
+
 _GUI_RESOURCE_BUILT = False
 _thread_lock = threading.Lock()
 _thread_lock.acquire()
 
 if not _GUI_RESOURCE_BUILT:
+    logger = MtPyLog.get_mtpy_logger(__name__)
+
     from qtpy.uic import compileUiDir
     from qtpy import QT_VERSION
+
     _dir = os.path.dirname(__file__)
 
     if QT_VERSION.startswith("4"):
         _resource_suffix = "_py{python_version}_qt4_rc".format(python_version=sys.version_info[0])
     else:
         _resource_suffix = "_qt5_rc"
+
+    logger.debug("Setting PyQt resource suffix to {}".format(_resource_suffix))
+    logger.debug("compiling Qt .ui designer files ...")
     compileUiDir(_dir, recurse=True, resource_suffix=_resource_suffix)
     del _dir, _resource_suffix
     _GUI_RESOURCE_BUILT = True
