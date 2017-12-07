bdc276891ccd0f5e8a08bab730207c4f3e1fc2d7
Yingzhi Gou <yingzhi.gou@ga.gov.au>
2017-12-07 16:32:34 +1100

    testing GUI in the last stage to avoid running into timeout again

---
+++ bin/travis-test-gui.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+"export DISPLAY=:99.0"  # enable display on travis
+"sh -e /etc/init.d/xvfb start"
+sleep 3 # give xvfb some time to start
+py.test -v --cov=mtpy --cov-append --cov-report= tests/SmartMT
