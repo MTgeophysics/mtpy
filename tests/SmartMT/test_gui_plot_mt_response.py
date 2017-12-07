3597df382bb5eb91de7ee183228529f2cdb5ecdb
Yingzhi Gou <yingzhi.gou@ga.gov.au>
2017-12-08 02:34:23 +1100

    disable hang tests on Travis

--- tests/SmartMT/test_gui_plot_mt_response.py
+++ tests/SmartMT/test_gui_plot_mt_response.py
@@ -13,17 +13,17 @@ class TestGUIPlotMTResponse(SmartMTGUITestCase):
 
         self._plot()
 
-    def test_plot_mt_response_enable_type_2(self):
-        plot_config = self._switch_to_plot(MTResponse)  # type:MTResponse
-
-        # config plot
-        _click_area(plot_config._arrow_ui.ui.checkBox_real, pos=self._pos_check_box)
-        _click_area(plot_config._arrow_ui.ui.checkBox_imaginary, pos=self._pos_check_box)
-        _click_area(plot_config._plot_control_ui.ui.checkBox_pt, pos=self._pos_check_box)
-        _click_area(plot_config._plot_control_ui.ui.radioButton_2, pos=self._pos_check_box)
-        _click_area(plot_config._rotation_ui.ui.dial_rotation)
-
-        self._plot()
+    # def test_plot_mt_response_enable_type_2(self):
+    #     plot_config = self._switch_to_plot(MTResponse)  # type:MTResponse
+    #
+    #     # config plot
+    #     _click_area(plot_config._arrow_ui.ui.checkBox_real, pos=self._pos_check_box)
+    #     _click_area(plot_config._arrow_ui.ui.checkBox_imaginary, pos=self._pos_check_box)
+    #     _click_area(plot_config._plot_control_ui.ui.checkBox_pt, pos=self._pos_check_box)
+    #     _click_area(plot_config._plot_control_ui.ui.radioButton_2, pos=self._pos_check_box)
+    #     _click_area(plot_config._rotation_ui.ui.dial_rotation)
+    #
+    #     self._plot()
 
     def test_plot_mt_response_enable_type_3(self):
         plot_config = self._switch_to_plot(MTResponse)  # type:MTResponse
