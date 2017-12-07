3d0113c5448ea367e0cccfde8e6b9c7184655751
Yingzhi Gou <yingzhi.gou@ga.gov.au>
2017-12-08 09:59:26 +1100

    gui fix and new tests

---
+++ tests/SmartMT/test_gui_plot_strike.py
@@ -0,0 +1,25 @@
+from mtpy.gui.SmartMT.visualization import Strike
+from tests.SmartMT import SmartMTGUITestCase, _click_area
+
+
+class TestGUIPlotPhaseTensorPseudoSection(SmartMTGUITestCase):
+    def test_plot(self):
+        plot_gui = self._switch_to_plot(Strike)  # type: Strike
+
+        _click_area(plot_gui._plot_control_ui.ui.radioButton_type_1, pos=self._pos_check_box)
+        _click_area(plot_gui._plot_control_ui.ui.checkBox_plot_tipper, pos=self._pos_check_box)
+        _click_area(plot_gui._rotation_ui.ui.dial_rotation)
+
+        self._plot()
+
+        # fold
+        _click_area(plot_gui._plot_control_ui.ui.checkBox_fold, pos=self._pos_check_box)
+        self._plot()
+
+        # type 2
+        _click_area(plot_gui._plot_control_ui.ui.radioButton_type_2, pos=self._pos_check_box)
+        self._plot()
+
+        # un-fold
+        _click_area(plot_gui._plot_control_ui.ui.checkBox_fold, pos=self._pos_check_box)
+        self._plot()
