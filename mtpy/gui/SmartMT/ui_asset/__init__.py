c445e2de0fee628b19311295ea367a16111ba7ec
Yingzhi Gou <yingzhi.gou@ga.gov.au>
2017-12-13 00:54:16 +1100

    add code for pyinstaller bundle

--- mtpy/gui/SmartMT/ui_asset/__init__.py
+++ mtpy/gui/SmartMT/ui_asset/__init__.py
@@ -8,9 +8,10 @@ import threading
 
 from mtpy import MtPyLog
 
-_GUI_RESOURCE_BUILT = False
 _thread_lock = threading.Lock()
 _thread_lock.acquire()
+
+_GUI_RESOURCE_BUILT = False
 
 if not _GUI_RESOURCE_BUILT:
     logger = MtPyLog.get_mtpy_logger(__name__)
@@ -18,7 +19,12 @@ if not _GUI_RESOURCE_BUILT:
     from qtpy.uic import compileUiDir
     from qtpy import QT_VERSION
 
-    _dir = os.path.dirname(__file__)
+    if getattr(sys, 'frozen', False):
+        # running in a pyinstaller bundle
+        _dir = os.path.join(sys._MEIPASS, "mtpy/gui/SmartMT/ui_asset")
+        sys.path.append(sys._MEIPASS)
+    else:
+        _dir = os.path.dirname(__file__)
 
     if QT_VERSION.startswith("4"):
         _resource_suffix = "_py{python_version}_qt4_rc".format(python_version=sys.version_info[0])
