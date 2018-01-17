52a58718e3b9946621dbe6ef2fed0024788f70cf
Yingzhi Gou <yingzhi.gou@ga.gov.au>
2018-01-17 18:59:00 +1100

    reproduced the reported bug in a test case

--- tests/SmartMT/test_gui_plot_mt_response.py
+++ tests/SmartMT/test_gui_plot_mt_response.py
@@ -1,4 +1,7 @@
+import os
+
 from mtpy.gui.SmartMT.visualization import MTResponse
+from tests import TEST_MTPY_ROOT
 from tests.SmartMT import SmartMTGUITestCase, _click_area
 
 
@@ -35,4 +38,27 @@ class TestGUIPlotMTResponse(SmartMTGUITestCase):
         _click_area(plot_config._plot_control_ui.ui.radioButton_3, pos=self._pos_check_box)
         _click_area(plot_config._rotation_ui.ui.dial_rotation)
 
+        self._plot()
+
+    def test_github_issue_24(self):
+        # this test recreate the bug reported in issue #24 on github
+        # https://github.com/MTgeophysics/mtpy/issues/24
+        data_path = os.path.join(TEST_MTPY_ROOT, "examples/data/edi2")
+        # load data under mtpy/examples/data/edi2
+        self._load_data([os.path.join(data_path, edi) for edi in os.listdir(data_path) if edi.endswith("edi")])
+        # select top 5 stations
+        self._select_data(5)
+        # switch to visualise
+        _click_area(self.smartMT._station_viewer.ui.pushButton_plot)  # trigger plot widget
+        self.assertTrue(self.smartMT.ui.stackedWidget.currentIndex() == 1)
+        # select MT response visualisation
+        index = [i for i in range(self.smartMT._plot_option.ui.comboBoxSelect_Plot.count())
+                 if self.smartMT._plot_option.ui.comboBoxSelect_Plot.itemText(i) == MTResponse.plot_name()]
+        self.assertFalse(len(index) == 0, "plot type not found")
+        self.assertFalse(len(index) > 1, "plot type name is not unique")
+        self.smartMT._plot_option.ui.comboBoxSelect_Plot.setCurrentIndex(index[0])
+        plot_config = self.smartMT._plot_option._current_plot
+        self.assertTrue(isinstance(plot_config, MTResponse))
+
+        # click visualise to start
         self._plot()
