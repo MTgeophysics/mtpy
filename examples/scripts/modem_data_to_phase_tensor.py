d3d9e2a938c039188f2e10715ff9deab4edac877
Sudhir Jain <sudhir.jain@ga.gov.au>
2018-02-21 11:43:45 +1100

    modem_data_to_phase_tensor.py updated for command wrapper

--- examples/scripts/modem_data_to_phase_tensor.py
+++ examples/scripts/modem_data_to_phase_tensor.py
@@ -12,13 +12,16 @@ Usage Examples:
 Developer:      fei.zhang@ga.gov.au
 LastUpdate:     08/09/2017
 LastUpdate:     05/12/2017 FZ moved the function into the module mtpy.modeling.modem.Data
+LastUpdate:     21/02/2018 Added command for running the script
 """
 
-import sys
+import sys, os
 from mtpy.modeling.modem import Data
 from mtpy.mtpy_globals import NEW_TEMP_DIR
+import click
 
-if __name__ == "__main__":
+
+if __name__ == "__main__old":
 
     file_dat = sys.argv[1]
     if len(sys.argv)>2:
@@ -29,3 +32,22 @@ if __name__ == "__main__":
     obj = Data()
     
     obj.compute_phase_tensor(file_dat, outdir)
+
+# =============================================================================================
+# Command line wrapper for processing phase tensors and output to csv file
+# =============================================================================================
+
+@click.command()
+@click.option('-d','--directory_file',type=str,
+              default='examples/data/ModEM_files/Modular_MPI_NLCG_028.dat', \
+              help='input test file')
+@click.option('-o','--output_dir',type=str,default=".",help='Output directory')
+def process_phase_tensors(directory_file,output_dir):
+    if os.path.isfile(directory_file):
+        obj = Data()
+        obj.compute_phase_tensor(directory_file, output_dir)
+    else:
+        print("Please provide directory and file !")
+
+if __name__ == '__main__':
+    process_phase_tensors()
