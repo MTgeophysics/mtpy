939a7794aa5aa64bc45810e02970dc5a1b96b610
Sudhir Jain <sudhir.jain@ga.gov.au>
2018-03-13 08:57:38 +1100

    Version Resoluion of plot_penetration_depth.py

--- examples/scripts/plot_penetration_depth.py
+++ examples/scripts/plot_penetration_depth.py
@@ -5,10 +5,14 @@ Created on Wed Nov 08 12:04:38 2017
 @author: u64125
 """
 
-from mtpy.imaging import penetration_depth1d
+from mtpy.imaging import penetration_depth1d as pd
 
-edipath = r'C:\Git\mtpy\examples\data\edi_files'
-elst = [op.join(edipath,f) for f in os.listdir(edipath) if f.endswith('.edi')]
+#edipath = r'C:\Git\mtpy\examples\data\edi_files'
+edipath = r'examples\data\edi_files'
+#elst = [op.join(edipath,f) for f in os.listdir(edipath) if f.endswith('.edi')]
+elst = [pd.os.path.join(edipath,f) for f in pd.os.listdir(edipath) if f.endswith('.edi')]
+print (elst)
 
-pplot=penetration_depth1d.Depth1D()
-p
+#pplot=penetration_depth1d.Depth1D()
+pplot=pd.Depth1D()
+#p
