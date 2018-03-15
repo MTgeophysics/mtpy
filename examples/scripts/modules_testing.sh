c305d912b96a577a23c4fd7c895c4befbf78a574
sudhirjain <ubuntu@ip-172-31-1-198.ap-southeast-2.compute.internal>
2018-03-15 02:48:37 +0000

    New vesrion of shell script to run independent of package directory

--- examples/scripts/modules_testing.sh
+++ examples/scripts/modules_testing.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-cd $MTPYPATH
-echo `pwd`
-export MTPYPYTHON="${HOME}/mtpy"
-cd ${MTPYPYTHON}
+#----------------------------------------------
+# setting MTPYPATH ( mtpy packahe directory ) 
+#----------------------------------------------
 
-if [[ -d "${MTPYPYTHON}/temp" ]]; then
+echo "==================================================================="
+echo ""
+echo "Change MTPYPATH if package installation in a different directory ! "
+echo ""
+echo "==================================================================="
+
+export MTPYPATH="${HOME}/mtpy"
+
+if ! [  -d "${MTPYPATH}" ]; then
+    echo "Set mtpy package directory ! "
+fi
+
+if [ -d "${MTPYPYTHON}/temp" ]; then
     rm -f "${MTPYPYTHON}/temp/*"
     echo "Cleaned the tmp directory .... " 
 fi
@@ -14,72 +25,57 @@ fi
 # Testing plot_edsi
 #----------------------------------------------
 echo "edi files plotting   ........ "
-python examples/scripts/plot_edis.py
+python ${MTPYPATH}/examples/scripts/plot_edis.py -d ${MTPYPATH}/examples/data/edi_files
 
 #----------------------------------------------
 # Testing edi_collection
 #----------------------------------------------
 echo "edi_collection       ........ "
-python mtpy/core/edi_collection.py
+python ${MTPYPATH}/mtpy/core/edi_collection.py -i ${MTPYPATH}/examples/data/edi_files
 
 #----------------------------------------------
 # Testing modem_data_to_phase_tensor
 #----------------------------------------------
 echo "modem_data_to_phase_tensor Testig ........ "
-python examples/scripts/modem_data_to_phase_tensor.py
+python ${MTPYPATH}/examples/scripts/modem_data_to_phase_tensor.py -i ${MTPYPATH}/examples/data/ModEM_files/Modular_MPI_NLCG_028.dat
 
 
 #----------------------------------------------
 # Testing penetration depth 1d
 #----------------------------------------------
 echo "penetration depth testing 1d ........ "
-python mtpy/imaging/penetration_depth1d.py
+python ${MTPYPATH}/mtpy/imaging/penetration_depth1d.py -i ${MTPYPATH}/examples/data/edi_files 
 
 
 #----------------------------------------------
 # Testing penetration depth 2d
 #----------------------------------------------
 echo "penetration depth testing 1d ........ "
-python mtpy/imaging/penetration_depth2d.py
+python ${MTPYPATH}/mtpy/imaging/penetration_depth2d.py -i ${MTPYPATH}/examples/data/edi_files 
 
 
 #----------------------------------------------
 # Testing plot response
 #----------------------------------------------
 echo "plot response ........ "
-python mtpy/imaging/plot_response.py
-
-
-#----------------------------------------------
-# Testing PlotMcmcResults 
-#----------------------------------------------
-echo "PlotMcmcResults ............ "
-python mtpy/imaging2/PlotMcmcResults.py
-
-
-#----------------------------------------------
-# Testing concatenate input
-#----------------------------------------------
-echo "concatenate input  ........ "
-python mtpy/utils/concatenate_input.py
-
+python ${MTPYPATH}/mtpy/imaging/plot_response.py -d ${MTPYPATH}/examples/model_files/ModEM_2
 
 #----------------------------------------------
 # Testing shape files generation
 #----------------------------------------------
 echo "Shape files genetation  ........ "
-python mtpy/utils/shapefiles.py
+python ${MTPYPATH}/mtpy/utils/shapefiles.py -i ${MTPYPATH}/examples/data/edi_files
 
 
 #----------------------------------------------
 # Testing shape files creator
 #----------------------------------------------
 echo "shaope files creator  ........ "
-python mtpy/utils/shapefiles_creator.py
+python ${MTPYPATH}/mtpy/utils/shapefiles_creator.py -i ${MTPYPATH}/examples/data/edi_files
 
 #----------------------------------------------
 # Testing Creating Modem Input
 #----------------------------------------------
 echo "Modem Input           ........ "
-python examples/scripts/create_modem_input.py
+python ${MTPYPATH}/examples/scripts/create_modem_input.py -i ${MTPYPATH}/examples/data/edi_files
 
