1d3fa2bc479850e1b15c20fbb4f8e238ad1835c6
ak <alison.kirkby@ga.gov.au>
2018-03-22 14:58:10 +1100

    cleaning up examples directory

--- examples/scripts/modem_plotptresidual.py
+++ examples/scripts/modem_plotptresidual.py
@@ -12,13 +12,13 @@ from mtpy.imaging.modem_phase_tensor_maps import PlotPTMaps
 
 workdir = r'/Softlab/Githubz/mtpy2/examples/data/ModEM_files'
 workdir = r'/g/data/ha3/fxz547/Githubz/mtpy2/examples/data/ModEM_files'
-workdir = r'E:\Githubz\mtpy2\examples\data\ModEM_files'
+workdir = r'C:\mtpywin\mtpy\examples\data\ModEM_files'
 
 
 modeldir = op.join(workdir, 'VicSynthetic05')
 
 ptmap = PlotPTMaps(data_fn=op.join(modeldir, 'ModEM_Data_noise10inv.dat'),
-                   ellipse_size=40,
+                   ellipse_size=40,lw=0.5,color='k',
                    resp_fn=op.join(modeldir, 'Modular_MPI_NLCG_NLCG_015.dat'))
 
 ptmap.plot()
--- examples/scripts/plot_penetration_depth.py
+++ examples/scripts/plot_penetration_depth.py
@@ -8,11 +8,12 @@ Created on Wed Nov 08 12:04:38 2017
 from mtpy.imaging import penetration_depth1d as pd
 
 #edipath = r'C:\Git\mtpy\examples\data\edi_files'
-edipath = r'examples\data\edi_files'
+edipath = r'C:\mtpywin\mtpy\examples\data\edi_files'
 #elst = [op.join(edipath,f) for f in os.listdir(edipath) if f.endswith('.edi')]
 elst = [pd.os.path.join(edipath,f) for f in pd.os.listdir(edipath) if f.endswith('.edi')]
 print (elst)
 
 #pplot=penetration_depth1d.Depth1D()
 pplot=pd.Depth1D()
+pplot.show()
 #p
