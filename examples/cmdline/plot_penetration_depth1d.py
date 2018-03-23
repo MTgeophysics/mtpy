07d9b4ce0b00acc503ae8a87783d5ca28e1ab941
Fei Zhang <fei.zhang@ga.gov.au>
2018-03-23 14:54:24 +1100

    examples to plot penetration depth 1D [ci skip]

---
+++ examples/cmdline/plot_penetration_depth1d.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+"""
+Description:
+    Example template python script structure.
+    .......
+    .......
+    
+References: 
+    https://gajira.atlassian.net/browse/ALAMP-49
+
+CreationDate:   23/03/2018
+Developer:      fei.zhang@ga.gov.au
+
+Revision History:
+    LastUpdate:     23/03/2018   FZ
+    LastUpdate:     dd/mm/yyyy  Who     Optional description
+"""
+
+# import section
+import os
+import click
+import mtpy.imaging.penetration_depth1d as pd1d
+
+
+# Section to define functions or class
+def fun1():
+    """
+    define my function1
+    :return:
+    """
+    print("begin fun1")
+
+    return
+
+
+def main():
+    """
+    define my main function
+    :return:
+    """
+    print("Template main()")
+
+    return
+
+###############################################################################
+# Following is code for click making inputs to the plot depth
+###############################################################################
+
+@click.command()
+@click.option('-i','--input',type=str,default='examples/data/edi_files',help='directory or edsi data files')
+@click.option('-o','--output_file',type=str,default='temp',help='save jpg image file')
+def plot_penetration_depth(input, output_file):
+    if os.path.isfile(input):
+        pd1d.plot_edi_file(input,savefile=output_file)
+    elif os.path.isdir(input):
+        pd1d.plot_edi_dir(input, rholist=['det'])
+    else:
+        pass
+
+# =============================================
+# Section for quick test of this script
+# ---------------------------------------------
+
+if __name__ == '__main__':
+    plot_penetration_depth()
--- examples/scripts/plot_penetration_depth.py
+++ examples/scripts/plot_penetration_depth.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 """
+Plot Penetration Depth from EDI files
+Example how to use the module mtpy.imaging import penetration_depth1d as pd1d
+
 Created on Wed Nov 08 12:04:38 2017
 
 @author: u64125
 """
+import os, sys
+from mtpy.imaging import penetration_depth1d as pd1d
 
-from mtpy.imaging import penetration_depth1d as pd
+#edipath = r'C:\Git\mtpy\examples\data\edi_files'  # avoid using \
+edipath = r'examples/data/edi_files'  # / is Unix and Win-Dos compatible
 
-#edipath = r'C:\Git\mtpy\examples\data\edi_files'
-edipath = r'C:\mtpywin\mtpy\examples\data\edi_files'
-#elst = [op.join(edipath,f) for f in os.listdir(edipath) if f.endswith('.edi')]
-elst = [pd.os.path.join(edipath,f) for f in pd.os.listdir(edipath) if f.endswith('.edi')]
-print (elst)
+edi_path = edipath  # sys.argv[1]
 
-#pplot=penetration_depth1d.Depth1D()
-pplot=pd.Depth1D()
-pplot.show()
-#p
+if os.path.isfile(edi_path):
+    pd1d.plot_edi_file(edi_path, savefile='C:/temp/pen_depth.jpg')
+            # rholist can be any of ['zxy','zyx','det'], default all of them
+elif os.path.isdir(edi_path):  # choose a suitable function below at run
+    # plot_edi_dir(edi_path )
+    pd1d.plot_edi_dir(edi_path, rholist=['det'])
