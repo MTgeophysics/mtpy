a7da48b19dadff63394eb4ec3c44ffeae6a30f4d
zhang01GA <fei.zhang@ga.gov.au>
2018-04-07 13:35:06 +1000

    change theme update the docs/source/conf.py for readthedocs.io [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,8 +81,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-html_theme = 'classic'
+# html_theme = 'alabaster'
+# html_theme = 'classic'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
