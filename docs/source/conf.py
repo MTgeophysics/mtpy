9c20966babd426c9eaa5ac446fc2d5ef7d4566f3
zhang01GA <fei.zhang@ga.gov.au>
2018-04-16 08:34:39 +1000

    update the docs/source/conf.py to static and template dirs  [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,7 @@ extensions += [
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -108,7 +108,7 @@ html_theme = 'classic'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
