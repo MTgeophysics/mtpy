dc5f3f063e5d21cc373ae2b0bffc488a713b2446
ak <alison.kirkby@ga.gov.au>
2018-06-29 11:09:36 +1000

    update test to change temporary test output dir

--- tests/modeling/ModEM/test_ModEM_model.py
+++ tests/modeling/ModEM/test_ModEM_model.py
@@ -11,18 +11,20 @@ class TestModEM_Model(TestCase):
     def setUp(self):
         self.model_epsg = 28355
         self._temp_dir = make_temp_dir(self.__name__)
+        # directory to save created input files
+        self._output_dir = make_temp_dir(self._testMethodName, base_dir=self._temp_dir)
 
+        
+        
     def test_read_gocad_sgrid_file(self):
         
-        # directory to save created input files
-        self._output_dir = r'C:/tmp'#make_temp_dir(self._testMethodName, base_dir=self._temp_dir)
         # set the dir to the output from the previously correct run
-        self._expected_output_dir = os.path.join(SAMPLE_DIR, 'ModEM')
+        expected_output_dir = os.path.join(SAMPLE_DIR, 'ModEM')        
         
         self._sgrid_file = os.path.join(SAMPLE_DIR,'gocad','ModEM_Model_File.sg')
         
-        if not os.path.isdir(self._expected_output_dir):
-            self._expected_output_dir = None
+        if not os.path.isdir(expected_output_dir):
+            expected_output_dir = None
             
         output_fn = 'ModEM_Model_File.rho'
 
@@ -39,7 +41,7 @@ class TestModEM_Model(TestCase):
 
         self.assertTrue(os.path.isfile(output_data_file), "output data file not found")
 
-        expected_data_file = os.path.normpath(os.path.join(self._expected_output_dir, 'ModEM_Model_File.rho'))
+        expected_data_file = os.path.normpath(os.path.join(expected_output_dir, 'ModEM_Model_File.rho'))
 
         self.assertTrue(os.path.isfile(expected_data_file),
                         "Ref output data file does not exist, nothing to compare with"
@@ -50,3 +52,7 @@ class TestModEM_Model(TestCase):
         is_identical, msg = diff_files(output_data_file, expected_data_file)
         print msg
         self.assertTrue(is_identical, "The output file is not the same with the baseline file.")
+        
+    
+    def test_write_gocad_sgrid_file(self):
+        return
