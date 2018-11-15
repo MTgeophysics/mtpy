323c7c1463b325784f3d24aadbc7b61c4dfa2de1
Umma Zannat <umma.zannat@gmail.com>
2018-11-15 19:33:29 +1100

    try griddata

--- mtpy/modeling/modem/interp.py
+++ mtpy/modeling/modem/interp.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial import cKDTree as KDTree
+from scipy.interpolate import griddata
 
 def IDW(source_points, source_values, query_points, k=6, p=5):
     tree = KDTree(source_points.points, k)
@@ -13,3 +14,10 @@ def nearest(source_points, source_values, query_points):
     tree = KDTree(source_points.points, 1)
     _, indices = tree.query(query_points.points, k=1, p=2)
     return source_values[indices]
+
+
+def linear(source_points, source_values, query_grid):
+    assert query_grid.shape[-1] == 3
+    grids = (query_grid[..., 0], query_grid[..., 1], query_grid[..., 2])
+    print(grids[0].shape)
+    return griddata(source_points.points, source_values, grids)
--- mtpy/modeling/modem/winglink.py
+++ mtpy/modeling/modem/winglink.py
@@ -27,7 +27,9 @@ mask = source_in_geo.clipping_mask(geo_grid)
 source_points.points = source_points.points[mask]
 source_val = source_val[mask]
 
-resistivity = interp.nearest(source_points, source_val, utm_points)
-resistivity = resistivity.reshape(geo_grid.shape[:3])
+# resistivity = interp.nearest(source_points, source_val, utm_points)
+resistivity = interp.linear(source_points, source_val, utm_grid)
+# resistivity = resistivity.reshape(geo_grid.shape[:3])
+print(resistivity.shape)
 
 nc.write_resistivity_grid('nearest.nc', lib.WGS84_crs, geo_grid.y, geo_grid.x, geo_grid.z, resistivity.transpose([2, 0, 1]))
