002caea359f3c76a3b81a875acae70012c06ebdb
Medlin Andrew <Andrew.Medlin@ga.gov.au>
2018-12-04 14:35:34 +1100

    Updated test script to avoid hardwired paths

--- examples/scripts/plot_edis.py
+++ examples/scripts/plot_edis.py
@@ -14,17 +14,27 @@ LastUpdated:    2018-03-22  AK updated to use as script rather than command line
 """
 import os
 import os.path as op
+import tempfile
 
-os.chdir(r'C:\mtpywin\mtpy') # change to your path to your mtpy installation
+try:
+    PACK_ROOT = os.environ['PACK_ROOT']
+    MTPY_PATH = os.path.join(PACK_ROOT, 'mtpy')
+except:
+    print("Define path variable PACK_ROOT to point to root of folder containing mtpy installation.")
+    raise
+
+os.chdir(MTPY_PATH) # change to your path to your mtpy installation
 
 from mtpy.core import mt
 
 
-edi_path = r'C:\mtpywin\mtpy\examples\data\edi_files'
+edi_path = os.path.join(MTPY_PATH, 'examples', 'data', 'edi_files')
 
 edi_list = [op.join(edi_path,ff) for ff in os.listdir(edi_path) if ff.endswith('.edi')]
 
-savepath = r'C:\tmp'
+temp_dir = tempfile.gettempdir()
+print('Using temporary directory ' + temp_dir)
+savepath = temp_dir
 
 for edi_file in edi_list:
     mt_obj = mt.MT(edi_file)
@@ -33,5 +43,3 @@ for edi_file in edi_list:
     pt_obj.save_plot(op.join(savepath,
                              op.basename(edi_file)[:-4]+'.png'),
                      fig_dpi=400) # change to your preferred file resolution
-
-
