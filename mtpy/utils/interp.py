0666f9849106c2266698db9204f6393062a6d9f6
Umma Zannat <umma.zannat@gmail.com>
2018-12-07 00:16:04 +1100

    two utility function how to create netCDF file from modem outout and winGlink output has been added

---
+++ mtpy/utils/interp.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.spatial import cKDTree as KDTree
+from scipy.interpolate import griddata
+
+def IDW(source_points, source_values, query_points, k=6, p=5):
+    tree = KDTree(source_points.points, k)
+    distances, indices = tree.query(query_points.points, k=k, p=p)
+    inv_dist = 1. / distances
+    weights = inv_dist / inv_dist.sum(axis=1)[:, np.newaxis]
+    return (weights * source_values[indices]).sum(axis=1)
+
+
+def nearest(source_points, source_values, query_points):
+    tree = KDTree(source_points.points, 1)
+    _, indices = tree.query(query_points.points, k=1, p=2)
+    return source_values[indices]
+
+
+def linear(source_points, source_values, query_grid):
+    assert query_grid.shape[-1] == 3
+    grids = (query_grid[..., 0], query_grid[..., 1], query_grid[..., 2])
+    print(grids[0].shape)
+    return griddata(source_points.points, source_values, grids)
---
+++ mtpy/utils/winglink_to_netCDF.py
@@ -0,0 +1,39 @@
+import lib
+import interp
+import nc
+import numpy as np
+
+def main():
+    source_crs = lib.epsg_to_crs(32753)
+
+    geo_grid = lib.Grid3D.from_extent_and_resolution(left=(131.5, -31., -4500.),
+                                                 right=(132.5, -30., -125.),
+                                                 resolution=(0.005, 0.005, 100.),
+                                                 crs=lib.wgs84_crs)
+
+    utm_points = geo_grid.flatten().to_crs(source_crs)
+
+    utm_grid = utm_points.to_gridded_array(geo_grid.shape)
+
+    arr = np.loadtxt('winGlink_3dmod_input.xyzv')
+
+    source_points = lib.Points3D(arr[:, 0:3], source_crs).fix_false_origin()
+
+    source_val = arr[:, 3]
+
+    source_in_geo = source_points.to_crs(lib.wgs84_crs)
+
+    mask = source_in_geo.clipping_mask(geo_grid)
+
+    source_points.points = source_points.points[mask]
+    source_val = source_val[mask]
+
+    # resistivity = interp.nearest(source_points, source_val, utm_points)
+    resistivity = interp.IDW(source_points, source_val, utm_points)
+    resistivity = resistivity.reshape(geo_grid.shape[:3])
+    print(resistivity.shape)
+
+    nc.write_resistivity_grid('IDW.nc', lib.wgs84_crs, geo_grid.y, geo_grid.x, geo_grid.z, resistivity.transpose([2, 0, 1]))
+
+if __name__ == "__main__":
+    main()
