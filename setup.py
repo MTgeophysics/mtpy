314329a5fb80615258401a18cd62abbdf5a8a610
Fei Zhang <fei.zhang@ga.gov.au>
2019-07-23 12:01:23 +1000

    Update setup.py
    to see if the travis CI deploys this new minor patch automatically

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup_kwargs['install_requires'] = ['numpy>=1.8.1',
                                      'configparser']
 
 setup(name="mtpy",
-	version='1.1.0',
+	version='1.1.1',
 	description=("Python toolkit for standard MT data processing."),
 	license="GNU GENERAL PUBLIC LICENSE v3",
 	**setup_kwargs)
