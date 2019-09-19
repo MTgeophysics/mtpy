547ce26450decef5c95caeb39c8f18fda9c27361
Fei Zhang <fei.zhang@ga.gov.au>
2019-09-19 11:06:04 +1000

    Fix geotif and ASCII grid image output for modem model.

--- mtpy/utils/convert_modem_data_to_geogrid.py
+++ mtpy/utils/convert_modem_data_to_geogrid.py
@@ -9,6 +9,7 @@ Developer:      fei.zhang@ga.gov.au
 
 Revision History:
     LastUpdate:     1/05/2019   FZ
+    LastUpdate:     17/09/2019  FZ fix the geoimage coordinates, upside-down issues 
     LastUpdate:     dd/mm/yyyy
 """
 
@@ -23,7 +24,7 @@ from mtpy.contrib.netcdf import nc
 import mtpy.contrib.netcdf.modem_to_netCDF as modem2nc
 
 
-def array2geotiff_writer(newRasterfn, rasterOrigin, pixelWidth, pixelHeight, array):
+def array2geotiff_writer(newRasterfn, rasterOrigin, pixelWidth, pixelHeight, array, epsg_code=4283):
 
     cols = array.shape[1]
     rows = array.shape[0]
@@ -37,18 +38,27 @@ def array2geotiff_writer(newRasterfn, rasterOrigin, pixelWidth, pixelHeight, arr
     outband = outRaster.GetRasterBand(1)
     outband.WriteArray(array)
     outRasterSRS = osr.SpatialReference()
-    outRasterSRS.ImportFromEPSG(4326)
+    outRasterSRS.ImportFromEPSG(epsg_code)
     outRaster.SetProjection(outRasterSRS.ExportToWkt())
     outband.FlushCache()
 
+# output to ascii format
+    format2 = 'AAIGrid'
+    newRasterfn2 = "%s.asc"%newRasterfn
+    driver2 = gdal.GetDriverByName(format2)
+    dst_ds_new = driver2.CreateCopy(newRasterfn2, outRaster)
 
 
-def test_array2geotiff_writer(newRasterfn):
+    return newRasterfn
+
+
+def test_array2geotiff(newRasterfn, epsg):
     #rasterOrigin = (-123.25745,45.43013)
-    rasterOrigin = (149.298, -34.974)
+    rasterOrigin = (149.298, -34.974)  # Longitude and Lattitude in Aussi continent
     pixelWidth = 0.01
-    pixelHeight = -0.01  # this is negative value, as a Geotiff image starts from the upper left corner.
-    # Define an array: 0 = black 1=bright
+    pixelHeight = -0.01  # this must be negative value, as a Geotiff image's origin is defined as the upper-left corner.
+
+    # Define an image 2D-array: The black=0 pixels trace out GDAL; the bright=1 pixels are white background
     array = np.array([[ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                       [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                       [ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1],
@@ -66,8 +76,9 @@ def test_array2geotiff_writer(newRasterfn):
     array2= 100*array + 10*random
     print (array2)
 
-    array2geotiff_writer(newRasterfn, rasterOrigin, pixelWidth, pixelHeight, array2)  # convert array to raster
+    outfn=array2geotiff_writer(newRasterfn, rasterOrigin, pixelWidth, pixelHeight, array2, epsg_code=epsg)  # write to a raster file
 
+    return outfn
 
 def modem2geotiff(data_file, model_file, output_file, source_proj=None):
     """
@@ -105,8 +116,9 @@ def modem2geotiff(data_file, model_file, output_file, source_proj=None):
         'resistivity': np.transpose(model.res_model, axes=(2, 0, 1))
     }
 
-    #grid_proj = Proj(init='epsg:4326') # output grid Coordinate systems: 4326 WGS84
-    grid_proj = Proj(init='epsg:4283') # output grid Coordinate system , 4283 https://spatialreference.org/ref/epsg/gda94/
+    #epsgcode= 4326 # 4326 output grid Coordinate systems: 4326 WGS84
+    epsgcode= 4283  # 4283 https://spatialreference.org/ref/epsg/gda94/
+    grid_proj = Proj(init='epsg:%s'%epsgcode) # output grid Coordinate system 
     # grid_proj = Proj(init='epsg:3112') # output grid Coordinate system 4326, 4283, 3112
     result = modem2nc.interpolate(resistivity_data, source_proj, grid_proj, center,
                          modem2nc.median_spacing(model.grid_east), modem2nc.median_spacing(model.grid_north))
@@ -116,16 +128,17 @@ def modem2geotiff(data_file, model_file, output_file, source_proj=None):
     print("result['longitude'] ==", result['longitude'])
     print("result['depth'] ==", result['depth'])
 
-    origin=(result['longitude'][0],result['latitude'][0])
+    #origin=(result['longitude'][0],result['latitude'][0]) # which corner of the image?
+    origin=(result['longitude'][0],result['latitude'][-1])
     pixel_width = result['longitude'][1] - result['longitude'][0]
-    pixel_height = result['latitude'][1] - result['latitude'][0]
+    pixel_height = result['latitude'][0] - result['latitude'][1] # This should be negative for geotiff with origin at the upper-left corner
 
     # write the depth_index
     depth_index=1
-    resis_data = result['resistivity'][depth_index,:,:]
-    #resis_data_vflip = resis_data[::-1]  # flipped upside down 
+    resis_data = result['resistivity'][depth_index,:,:] # this original image may start from the lower left corner, if so must be flipped.
+    resis_data_flip = resis_data[::-1]  # flipped to ensure the image starts from the upper left corner 
 
-    array2geotiff_writer(output_file,origin,pixel_width,pixel_height,resis_data)
+    array2geotiff_writer(output_file,origin,pixel_width,pixel_height,resis_data_flip, epsg_code=epsgcode)
 
     return output_file
 
@@ -133,17 +146,25 @@ def modem2geotiff(data_file, model_file, output_file, source_proj=None):
 #####################################################################################################################
 # Section for quick test run of this script
 # cd /e/Githubz/mtpy
+# Default output grid Coordinate systems:'epsg:4283' https://spatialreference.org/ref/epsg/gda94/
+# (not the Old 4326 WGS84)
+# export PYTHONPATH=/g/data/ha3/fxz547/Githubz/mtpy
 # python mtpy/utils/convert_modem_data_to_geogrid.py examples/model_files/ModEM_2/Modular_MPI_NLCG_004.dat examples/model_files/ModEM_2/Modular_MPI_NLCG_004.rho
+# python mtpy/utils/convert_modem_data_to_geogrid.py --output-file EF_NLCG_001_4283.tif tmp/JinMing_GridData_sample/EFTF_MT_model/EF_NLCG_001.dat  tmp/JinMing_GridData_sample/EFTF_MT_model/EF_NLCG_001.rho
 #####################################################################################################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('modem_data', help="ModEM data file")
     parser.add_argument('modem_model', help="ModEM model file")
-    parser.add_argument('--epsg', help="EPSG code for the modem source data CRS", type=int)
+    parser.add_argument('--epsg', default=4283, help="EPSG code for the output CRS", type=int)
     parser.add_argument('--output-file', default="output.tif", help="Name of output file")
+
     args = parser.parse_args()
 
-    modem2geotiff(args.modem_data, args.modem_model, args.output_file, args.epsg)
+    print (args)
+
+    ##test_array2geotiff("test_geotiff_GDAL_img.tif", args.epsg)
+
+    modem2geotiff(args.modem_data, args.modem_model, args.output_file )
 
 
-    test_array2geotiff_writer("test_geotiff_GDAL_img.tif")
