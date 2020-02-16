e9f936cf7f23d3fd06144d62ea78a8606f7f7eab
Bren Moushall <bmoush@gmail.com>
2020-02-17 10:34:55 +1100

    Update example script to work with new code
    This script can be used to run conversion, along with directly
    calling 'create_geogrid' or using the CLI in the conversion module.

--- examples/scripts/create_geogrid_from_resistivity_model.py
+++ examples/scripts/create_geogrid_from_resistivity_model.py
@@ -1,50 +1,63 @@
 #! /usr/bin/env python
 """
 Description:
-    Example python script to create grid formats (geotiff and ascii) from ModEM model file
+    Example python script to create grid formats (geotiff and ascii)
+    from ModEM model file.
+
+    See: 'mtpy.utils.convert_modem_data_to_geogrid' for implementation
+    and a command line interface.
 
 References:
-
 
 CreationDate:   6/11/2019
 Developer:      fei.zhang@ga.gov.au
 
 Revision History:
     LastUpdate:     6/11/2019   FZ
-    LastUpdate:     dd/mm/yyyy  Who     Optional description
+    LastUpdate:     17/02/2020  BM    Hook up revised code to script
+
 """
-import sys
 from mtpy.utils.convert_modem_data_to_geogrid import create_geogrid
 
-# =============================================
-# quick test of this script
-#  python examples/scripts/create_geogrid_from_resistivity_model.py  /c/Data/JinMing_GridData_sample/JM_model_002/EFTF_NLCG_002.dat /c/Data/JinMing_GridData_sample/JM_model_002/EFTF_NLCG_002.rho /c/temp
-#  python examples/scripts/create_geogrid_from_resistivity_model.py  /c/Data/JinMing_GridData_sample/EFTF_MT_model/EF_NLCG_001.dat  /c/Data/JinMing_GridData_sample/EFTF_MT_model/EF_NLCG_001.rho /c/temp
-#  python examples/scripts/create_geogrid_from_resistivity_model.py  /c/Data/Alison_201910/Alison_ModEM_Grid/MT075_ModEM_files/ModEM_Data.dat  /c/Data/Alison_201910/Alison_ModEM_Grid/MT075_ModEM_files/Modular_MPI_NLCG_004.rho /c/temp/
-# ---------------------------------------------
 if __name__ == "__main__":
+    # Path to model .dat file
+    # dat_file = "/path/to/dat_file"
+    dat_file = '/home/bren/data_mtpy/3d_grid_800m/ETMT_1_1_cont5_NLCG_015.dat'
+    # Path to model .rho file
+    # rho_file = "/path/to/rho_file"
+    rho_file = '/home/bren/data_mtpy/3d_grid_800m/ETMT_1_1_cont5_NLCG_015.rho'
+    # Path to output dir, will be created if it does not exist
+    # out_dir = "/path/to/out_dir"
+    out_dir = '/home/bren/test'
 
-    if len(sys.argv) < 4:
-        print("USAGE: python %s dat_file rho_file out_dir" % sys.argv[0])
-        sys.exit(1)
-
-    dat_file = sys.argv[1]
-    rho_file = sys.argv[2]
-    out_dir = sys.argv[3]
-    # Before calling the function create_geogrid(), a user should
-    # provide the right optional parameters.
-    # Otherwise, default parameters will be used, which may not make sense
     kwargs = {
+        # TODO: @FZ can we get an explanation of these padding params?
         "xpad": 6,
         "ypad": 6,
         "zpad": 10,
-        "grid_size": 7500,
-        "center_lat": None,  # to override what is found in the file.dat, which may be incorrect.
-        "center_lon": None,  # to override what is found in the file.dat, which may be incorrect.
-        "source_proj": None,
-        "depth_index": None,  # slices [0,1,2,10] to be output
+        # Resolution in meters, e.g. '800' will produce a TIFF with
+        #  pixel width and height of 800m x 800m.
+        "grid_size": 800,
+        # Center latitude of model. Leave as 'None' to use .dat file
+        #  center latitiude.
+        "center_lat": None,
+        # Center longitude of model. Leave as 'None' to use .dat file
+        #  center longitude.
+        "center_lon": None,
+        # EPSG code of survey area, e.g. 'EPSG:4326'. Leave as 'None'
+        #  to derive from .dat file.
+        "epsg_code": None,
+        # A list of depths (in meters) to retrieve slices for, e.g.
+        #  [100, 200, 500] will return the slices closest to 100m,
+        #  200m and 500m. Leave as 'None' to retrieve all slices.
+        "depths": [0, 100, 500],
+        # Angle in degrees to rotate TIFF by. 'None' will perform
+        #  no rotation.
+        "angle": 45.0,
+        # Whether or not rotate around the origin. If 'False', the
+        #  pivot for rotation is the center point. If 'True, the pivot
+        #  is the origin (upper left point).
+        "rotate_origin": False
     }
 
-    print("User Options:", kwargs)
-
-    create_geogrid(dat_file, rho_file, out_dir, **kwargs)  # ,depth_index=[0,1,2,10])
+    create_geogrid(dat_file, rho_file, out_dir, **kwargs)
