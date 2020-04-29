a82bdd0848a96d93a7e72ae64bbccf567905e94c
Kirkby Alison <alison.kirkby@ga.gov.au>
2020-04-29 10:29:07 +1000

    fix to mesh_tools.interpolate_elevation_to_grid and gis_tools.project_point_ll2utm, project_points_ll2utm

--- mtpy/utils/gis_tools.py
+++ mtpy/utils/gis_tools.py
@@ -355,10 +355,7 @@ def project_point_ll2utm(lat, lon, datum='WGS84', utm_zone=None, epsg=None):
     # end if
 
     for ii in range(lat.size):
-        if(HAS_GDAL):
-            point = ll2utm(lon[ii], lat[ii])
-        else:
-            point = ll2utm(lat[ii], lon[ii])
+        point = ll2utm(lon[ii], lat[ii])
         projected_point['easting'][ii] = point[0]
         projected_point['northing'][ii] = point[1]
         if(HAS_GDAL): projected_point['elev'][ii] = point[2]
@@ -499,7 +496,6 @@ def project_points_ll2utm(lat, lon, datum='WGS84', utm_zone=None, epsg=None):
                         projected point in UTM in Datum
                     
     """
-
     lat = np.array(lat)
     lon = np.array(lon)
 
@@ -579,7 +575,7 @@ def project_points_ll2utm(lat, lon, datum='WGS84', utm_zone=None, epsg=None):
     
     if HAS_GDAL:
         ll2utm = osr.CoordinateTransformation(ll_cs, utm_cs).TransformPoints
-        easting, northing, elev = np.array(ll2utm(np.array([lat, lon]).T)).T
+        easting, northing, elev = np.array(ll2utm(np.array([lon, lat]).T)).T
 
     else:
         ll2utm = pp
