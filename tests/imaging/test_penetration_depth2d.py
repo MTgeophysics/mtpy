817c9f4a6384460d57974fb7a6f80e04a8a4ce97
Bren Moushall <bmoush@gmail.com>
2020-05-20 17:00:35 +1000

    Fix of penetration 3D tests, refactor pen depth
    I'd made a big mistake when implementing period selection for
    pen depth 2D. The 'get_penetration_depth' function was being used
    by pen depth 3D for getting periods by index. A
    'get_penetration_depth_generic' function exists for getting periods
    by value (yet seemed to be used exlcusively by pen depth 3D).
    
    I've reimplemented the old code for 'get_penetration_depth' so it now
    gets depth by index and renamed it to 'get_penetration_depth_by_index'.
    
    I replaced 'get_penetration_depth_generic' with the code I used for
    getting pen depth by value as it's cleaner and renamed it to
    'get_penetration_depth_by_period'. Also fixed a bug where I wasn't
    reciprocating the frequency (was returning freq instead of period).
    
    I've changed penetration depth 2D so it can plot by period or period
    indices, this is controlled by providing the 'period_by_index' argument
    to plot2Dprofile (bringing it into line with the 3D variant).
    
    This fixes the tests for both 2D and 3D, brings them in line with each
    other in regards to period selection and gives them sensible names.

--- tests/imaging/test_penetration_depth2d.py
+++ tests/imaging/test_penetration_depth2d.py
@@ -12,6 +12,7 @@ class TestPenetration_depth2d(ImageTestCase):
 
         cls._edifiles = "data/edifiles"
         cls._period_list = [10400.01, 8799.998, 1800.0, 320.0001, 57.0, 9.4, 1.72, 0.35]
+        cls._period_indices = [0, 1, 10, 20, 30, 40, 50, 59]
 
     def test_plot2Dprofile_no_period_list(self):
         """
@@ -35,6 +36,21 @@ class TestPenetration_depth2d(ImageTestCase):
     @ImageCompare(fig_size=(8, 6))
     def test_plot2Dprofile_zyx(self):
         plot2Dprofile(self._edifiles, self._period_list, zcomponent='zyx')
+
+    @ImageCompare(fig_size=(8, 6))
+    def test_plot2Dprofile_det_indices(self):
+        plot2Dprofile(self._edifiles, self._period_indices, zcomponent='det',
+                      period_by_index=True)
+
+    @ImageCompare(fig_size=(8, 6))
+    def test_plot2Dprofile_zxy_indices(self):
+        plot2Dprofile(self._edifiles, self._period_indices, zcomponent='zxy',
+                      period_by_index=True)
+
+    @ImageCompare(fig_size=(8, 6))
+    def test_plot2Dprofile_zyx_indices(self):
+        plot2Dprofile(self._edifiles, self._period_indices, zcomponent='zyx',
+                      period_by_index=True)
 
     def test_plot2Dprofile_wrong_rho(self):
         try:
