47be57d2ac8e43273a435cd1725ffb0fab8e34e6
Bren Moushall <bmoush@gmail.com>
2020-07-08 11:54:52 +1000

    Use 'optimize_line' and let O2D handle gstrike

--- examples/scripts/edi2mare2dem.py
+++ examples/scripts/edi2mare2dem.py
@@ -8,7 +8,7 @@ TODO: Original author credit
 Modified 03-07-2020 17:28:10 AEST 
 brenainn.moushall@ga.gov.au
 """
-# import os
+import os
 # os.chdir(r'C:\mtpywin\mtpy')
 
 import mtpy.modeling.occam2d as o2d
@@ -33,10 +33,8 @@ solve_statics = False
 surface_file = '/path/to/mtpy/examples/data/AussieContinent_etopo1.asc'
 
 # Generate an Occam2D data object from EDI data
-gstrike = -72
-station_list = m2d.station_list(edi_dir)
-o2d_data = o2d.Data(edi_path=edi_dir, model_mode='1', station_list=station_list,
-                    interpolate_freq=False, geoelectric_strike=gstrike, res_te_err=20.,
+o2d_data = o2d.Data(edi_path=edi_dir, model_mode='1', optimize_line=True,
+                    interpolate_freq=False, res_te_err=20.,
                     phase_te_err=10., res_tm_err=10., phase_tm_err=5.)
 
 # Save the data file
@@ -56,6 +54,8 @@ fig.savefig('/tmp/m2d_plot.png', dpi=400)
 m2d.write_elevation_file(m2d_profile, profile_elevation,
                          '/tmp/elevation.txt')
 
+gstrike = o2d_data.geoelectric_strike
+
 m2d.write_mare2dem_data(o2d_path, site_locations, site_elevations,
                         (mare_origin_x, mare_origin_y, utm_zone),
-                        gstrike, solve_statics=False, savepath=m2d_path)
+                        gstrike=gstrike, solve_statics=False, savepath=m2d_path)
