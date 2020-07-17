70ae3d33196acfb77219cdad4b1d3aef094327c3
Bren Moushall <bmoush@gmail.com>
2020-07-17 14:11:52 +1000

    Update m2d test and data

--- tests/modeling/test_mare2dem.py
+++ tests/modeling/test_mare2dem.py
@@ -6,6 +6,7 @@ import filecmp
 import tempfile
 import shutil
 import difflib
+from copy import deepcopy
 
 import pytest
 import numpy as np
@@ -18,7 +19,7 @@ from tests import M2D_DIR, EDI_DATA_DIR2, AUS_TOPO_FILE
 
 @pytest.fixture()
 def ref_output():
-    return os.path.join(M2D_DIR, 'Mare2D_data.txt')
+    return os.path.join(M2D_DIR, 'Mare2Ddata.dat')
 
 
 @pytest.fixture()
@@ -27,28 +28,35 @@ def test_output():
 
     # Full path to save Occam2D data file
     o2d_path = os.path.join(tmpdir, 'o2d_data.dat')
+    rot_o2d_path = os.path.join(tmpdir, 'rot_o2d_data.dat')
 
     # Full path to save Mare2D data file
     m2d_path = os.path.join(tmpdir, 'mare2dem_test.txt')
 
     # Generate an Occam2D data object from EDI data
-    gstrike = -72
     o2d_data = o2d.Data(edi_path=EDI_DATA_DIR2, model_mode='1', optimize_line=True,
                         interpolate_freq=False, res_te_err=20.,
                         phase_te_err=10., res_tm_err=10., phase_tm_err=5.)
 
     # Save the data file
+    # We need a data file with the non-rotated profile and the rotated
+    # profile. This is because the elevation will be interpolated over
+    # the non-projected profile and stations.
+    rot_o2d_data = deepcopy(o2d_data)  # Make a copy because 'write_data_file' will populate data
+    o2d_data._rotate_to_strike = False
     o2d_data.save_path = o2d_path
+    rot_o2d_data.save_path = rot_o2d_path
     o2d_data.write_data_file(data_fn=o2d_path)
+    rot_o2d_data.write_data_file(data_fn=rot_o2d_path)
 
     gstrike = o2d_data.geoelectric_strike
 
     # Convert the Occam2D profile to Mare2D
-    mare_origin_x, mare_origin_y, utm_zone, site_locations, site_elevations, m2d_profile, profile_elevation = \
-        m2d.occam2d_to_mare2dem(o2d_data, AUS_TOPO_FILE, elevation_sample_n=300)
+    mare_origin, utm_zone, site_locations, site_elevations, site_names, m2d_profile, profile_elevation = \
+        m2d.occam2d_to_mare2dem(o2d_data, rot_o2d_data, AUS_TOPO_FILE, elevation_sample_n=300)
 
     m2d.write_mare2dem_data(o2d_path, site_locations, site_elevations,
-                            (mare_origin_x, mare_origin_y, utm_zone),
+                            site_names, mare_origin, utm_zone,
                             gstrike, solve_statics=False, savepath=m2d_path)
 
     yield m2d_path
@@ -66,3 +74,28 @@ def test_mare2dem_data(ref_output, test_output):
             for line in diff:
                 print(line)
     assert files_are_same
+
+
+@pytest.fixture()
+def coords():
+    x = [1, 2, 3, 4]
+    y = [2, 5, 7, 8]
+    return x, y
+
+
+def test_line_length(coords):
+    x, y = coords
+    test = m2d.line_length(x[0], y[0], x[-1], y[-1])
+    expected = 6.708
+    np.testing.assert_almost_equal(test, expected, decimal=3)
+
+
+def test_points_o2d_to_m2d(coords):
+    x, y = coords
+    test = m2d.points_o2d_to_m2d(x, y)
+    expected = [-3.354, -0.192, 2.031, 3.354]
+    np.testing.assert_almost_equal(test, expected, decimal=3)
+
+
+
+    
