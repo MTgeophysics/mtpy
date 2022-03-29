707d2bbaf3802581c6a9ee4cc762df561a1c9427
JP <peacock.jared@gmail.com>
2022-03-29 15:50:54 -0700

    removed superfolous files, going to put uofa, usgs, msartMT, tstools into their own repositories

--- mtpy/gui/SmartMT/ui_asset/compile_resources_pyqt5.bat
+++
@@ -1,2 +0,0 @@
-@echo Compiling .qrc files
-@for %%f in (*.qrc) do @echo %%f -^> %%~nf_qt5_rc.py & @call pyrcc5 "%%f" -o "%%~nf_qt5_rc.py"
--- mtpy/gui/SmartMT/visualization/__init__.py
+++
@@ -1,6 +0,0 @@
-# import all VisualizationBase subclasses here
-from .mt_response import *
-from .penetration import *
-from .phase_tensor import *
-from .strike import *
-from .visualization_base import VisualizationBase
