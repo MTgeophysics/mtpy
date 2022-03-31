4b5751ce565727200aaaebf793980eb6de1f0549
JP <peacock.jared@gmail.com>
2022-03-31 15:35:40 -0700

    updating test_mt_collection

--- tests/core/test_mt_collection.py
+++ tests/core/test_mt_collection.py
@@ -16,6 +16,7 @@ Created on Mon Jan 11 15:36:38 2021
 import unittest
 
 import numpy as np
+import pandas as pd
 
 from mtpy import MT, MTCollection
 
@@ -36,6 +37,193 @@ class TestMTCollection(unittest.TestCase):
 
         self.mc.add_tf(fn_list)
 
+        self.true_dataframe = pd.DataFrame(
+            {
+                "station": {
+                    0: "14-IEB0537A",
+                    1: "NMX20",
+                    2: "24",
+                    3: "300",
+                    4: "BP05",
+                    5: "YSW212abcdefghijkl",
+                    6: "GEO858",
+                    7: "TEST01",
+                    8: "Geoscience Australia",
+                    9: "s08",
+                    10: "SAGE_2005",
+                },
+                "survey": {
+                    0: "BOULIA",
+                    1: "CONUS South",
+                    2: "unknown_survey",
+                    3: "unknown_survey",
+                    4: "unknown_survey",
+                    5: "unknown_survey",
+                    6: "unknown_survey_001",
+                    7: "unknown_survey_002",
+                    8: "unknown_survey_003",
+                    9: "unknown_survey_004",
+                    10: "unknown_survey_005",
+                },
+                "latitude": {
+                    0: -22.823722222222223,
+                    1: 34.470528,
+                    2: 32.83331167,
+                    3: 34.727,
+                    4: 0.0,
+                    5: 44.631,
+                    6: 22.691378333333333,
+                    7: -30.930285,
+                    8: -23.051133333333333,
+                    9: -34.646,
+                    10: 35.55,
+                },
+                "longitude": {
+                    0: 139.29469444444445,
+                    1: -108.712288,
+                    2: -107.08305667,
+                    3: -115.735,
+                    4: 0.0,
+                    5: -110.44,
+                    6: 139.70504,
+                    7: 127.22923,
+                    8: 139.46753333333334,
+                    9: 137.006,
+                    10: -106.28333333333333,
+                },
+                "elevation": {
+                    0: 158.0,
+                    1: 1940.05,
+                    2: 0.0,
+                    3: 0.0,
+                    4: 0.0,
+                    5: 0.0,
+                    6: 181.0,
+                    7: 175.27,
+                    8: 122.0,
+                    9: 0.0,
+                    10: 0.0,
+                },
+                "tf_id": {
+                    0: "14-IEB0537A",
+                    1: "NMX20",
+                    2: "24",
+                    3: "300",
+                    4: "BP05",
+                    5: "ysw212abcdefghijkl",
+                    6: "GEO858",
+                    7: "TEST01",
+                    8: "Geoscience Australia",
+                    9: "s08",
+                    10: "SAGE_2005",
+                },
+                "units": {
+                    0: "none",
+                    1: "none",
+                    2: "none",
+                    3: "none",
+                    4: "none",
+                    5: "none",
+                    6: "none",
+                    7: "none",
+                    8: "none",
+                    9: "none",
+                    10: "none",
+                },
+                "has_impedance": {
+                    0: True,
+                    1: True,
+                    2: True,
+                    3: True,
+                    4: True,
+                    5: False,
+                    6: True,
+                    7: True,
+                    8: True,
+                    9: True,
+                    10: True,
+                },
+                "has_tipper": {
+                    0: True,
+                    1: True,
+                    2: False,
+                    3: True,
+                    4: False,
+                    5: True,
+                    6: True,
+                    7: True,
+                    8: True,
+                    9: False,
+                    10: True,
+                },
+                "has_covariance": {
+                    0: True,
+                    1: True,
+                    2: False,
+                    3: True,
+                    4: False,
+                    5: True,
+                    6: False,
+                    7: False,
+                    8: True,
+                    9: False,
+                    10: True,
+                },
+                "period_min": {
+                    0: 0.003125,
+                    1: 4.65455,
+                    2: 0.0009765625,
+                    3: 1.16364,
+                    4: 1.333333,
+                    5: 0.01818,
+                    6: 0.005154639175257732,
+                    7: 0.0012115271966653925,
+                    8: 0.00010061273153504844,
+                    9: 0.007939999015440123,
+                    10: 0.00419639110365086,
+                },
+                "period_max": {
+                    0: 2941.176470588235,
+                    1: 29127.11,
+                    2: 42.6657564638621,
+                    3: 10922.66699,
+                    4: 64.55,
+                    5: 4096.0,
+                    6: 1449.2753623188407,
+                    7: 1211.5274902250933,
+                    8: 1.0240026214467108,
+                    9: 2730.8332372990308,
+                    10: 209.73154362416108,
+                },
+                "hdf5_reference": {
+                    0: None,
+                    1: None,
+                    2: None,
+                    3: None,
+                    4: None,
+                    5: None,
+                    6: None,
+                    7: None,
+                    8: None,
+                    9: None,
+                    10: None,
+                },
+                "station_hdf5_reference": {
+                    0: None,
+                    1: None,
+                    2: None,
+                    3: None,
+                    4: None,
+                    5: None,
+                    6: None,
+                    7: None,
+                    8: None,
+                    9: None,
+                    10: None,
+                },
+            }
+        )
+
     def test_filename(self):
         self.assertEqual(
             self.mc.working_directory.joinpath("test_collection.h5"),
@@ -43,212 +231,23 @@ class TestMTCollection(unittest.TestCase):
         )
 
     def test_dataframe(self):
-        with self.subTest("shape"):
-            self.assertEqual(self.mc.dataframe.shape, (11, 14))
-        with self.subTest("has columns"):
-            self.assertListEqual(
-                self.mc.dataframe.columns.to_list(),
-                [
-                    "station",
-                    "survey",
-                    "latitude",
-                    "longitude",
-                    "elevation",
-                    "tf_id",
-                    "units",
-                    "has_impedance",
-                    "has_tipper",
-                    "has_covariance",
-                    "period_min",
-                    "period_max",
-                    "hdf5_reference",
-                    "station_hdf5_reference",
-                ],
-            )
-        with self.subTest("stations"):
-            self.assertListEqual(
-                self.mc.dataframe.station.to_list(),
-                [
-                    "14-IEB0537A",
-                    "NMX20",
-                    "24",
-                    "300",
-                    "BP05",
-                    "GEO858",
-                    "Geoscience Australia",
-                    "SAGE_2005",
-                    "TEST01",
-                    "YSW212abcdefghijkl",
-                    "s08",
-                ],
-            )
-        with self.subTest("latitude"):
-            self.assertTrue(
-                np.isclose(
-                    self.mc.dataframe.latitude.to_numpy(),
-                    np.array(
-                        [
-                            -22.82372222,
-                            34.470528,
-                            32.83331167,
-                            34.727,
-                            0.0,
-                            22.69137833,
-                            -23.05113333,
-                            35.55,
-                            -30.930285,
-                            44.631,
-                            -34.646,
-                        ]
-                    ),
-                ).all(),
-            )
-        with self.subTest("longitude"):
-            self.assertTrue(
-                np.isclose(
-                    self.mc.dataframe.longitude.to_numpy(),
-                    np.array(
-                        [
-                            139.29469444,
-                            -108.712288,
-                            -107.08305667,
-                            -115.735,
-                            0.0,
-                            139.70504,
-                            139.46753333,
-                            -106.28333333,
-                            127.22923,
-                            -110.44,
-                            137.006,
-                        ]
-                    ),
-                ).all(),
-            )
-        with self.subTest("tf_id"):
-            self.assertListEqual(
-                self.mc.dataframe.tf_id.to_list(),
-                [
-                    "14-IEB0537A",
-                    "NMX20",
-                    "24",
-                    "300",
-                    "BP05",
-                    "GEO858",
-                    "Geoscience Australia",
-                    "SAGE_2005",
-                    "TEST01",
-                    "ysw212abcdefghijkl",
-                    "s08",
-                ],
-            )
-        with self.subTest("has_impedance"):
-            self.assertTrue(
-                np.isclose(
-                    self.mc.dataframe.has_impedance.to_numpy(),
-                    np.array(
-                        [
-                            True,
-                            True,
-                            True,
-                            True,
-                            True,
-                            True,
-                            True,
-                            True,
-                            True,
-                            False,
-                            True,
-                        ],
-                        dtype=bool,
-                    ),
-                ).all()
-            )
-        with self.subTest("has_tipper"):
-            self.assertTrue(
-                np.isclose(
-                    self.mc.dataframe.has_tipper.to_numpy(),
-                    np.array(
-                        [
-                            True,
-                            True,
-                            False,
-                            True,
-                            False,
-                            True,
-                            True,
-                            True,
-                            True,
-                            True,
-                            False,
-                        ],
-                        dtype=bool,
-                    ),
-                ).all()
-            )
-        with self.subTest("has_covariance"):
-            self.assertTrue(
-                np.isclose(
-                    self.mc.dataframe.has_covariance.to_numpy(),
-                    np.array(
-                        [
-                            True,
-                            True,
-                            False,
-                            True,
-                            False,
-                            False,
-                            True,
-                            True,
-                            False,
-                            True,
-                            False,
-                        ],
-                        dtype=bool,
-                    ),
-                ).all()
-            )
-        with self.subTest("period_min"):
-            self.assertTrue(
-                np.isclose(
-                    self.mc.dataframe.period_min.to_numpy(),
-                    np.array(
-                        [
-                            3.12500000e-03,
-                            4.65455000e00,
-                            9.76562500e-04,
-                            1.16364000e00,
-                            1.33333300e00,
-                            5.15463918e-03,
-                            1.00612732e-04,
-                            4.19639110e-03,
-                            1.21152720e-03,
-                            1.81800000e-02,
-                            7.93999902e-03,
-                        ]
-                    ),
-                ).all()
-            )
-        with self.subTest("period_max"):
-            self.assertTrue(
-                np.isclose(
-                    self.mc.dataframe.period_max.to_numpy(),
-                    np.array(
-                        [
-                            2.94117647e03,
-                            2.91271100e04,
-                            4.26657565e01,
-                            1.09226670e04,
-                            6.45500000e01,
-                            1.44927536e03,
-                            1.02400262e00,
-                            2.09731544e02,
-                            1.21152749e03,
-                            4.09600000e03,
-                            2.73083324e03,
-                        ]
-                    ),
-                ).all()
-            )
+        h5_df = self.mc.dataframe[
+            self.mc.dataframe.columns[
+                ~self.mc.dataframe.columns.isin(
+                    ["hdf5_reference", "station_hdf5_reference"]
+                )
+            ]
+        ]
+
+        true_df = self.true_dataframe[
+            self.true_dataframe.columns[
+                ~self.true_dataframe.columns.isin(
+                    ["hdf5_reference", "station_hdf5_reference"]
+                )
+            ]
+        ]
+
+        self.assertTrue((h5_df == true_df).all().all())
 
     def test_get_tf(self):
         entry = self.mc.dataframe.iloc[0]
