2968dc082f47e07e2ca4202ef4b44bc69106958d
JP <peacock.jared@gmail.com>
2022-10-11 16:54:10 -0700

    Update errors.py

--- mtpy/modeling/errors.py
+++ mtpy/modeling/errors.py
@@ -12,6 +12,53 @@ import numpy as np
 # =============================================================================
 
 
+def validate_percent(value):
+    """
+    Make sure the percent is a decimal
+
+    :param value: DESCRIPTION
+    :type value: TYPE
+    :return: DESCRIPTION
+    :rtype: TYPE
+
+    """
+
+    if value > 1:
+        value /= 100.0
+
+    return value
+
+
+def validate_z_array_shape(array):
+    """
+
+    :param array: DESCRIPTION
+    :type array: TYPE
+    :return: DESCRIPTION
+    :rtype: TYPE
+
+    """
+
+    if array.shape == (2, 2):
+        array = array.reshape((1, 2, 2))
+
+    return array
+
+
+def mask_zeros(array):
+    """
+    mask zeros
+
+    :param array: DESCRIPTION
+    :type array: TYPE
+    :return: DESCRIPTION
+    :rtype: TYPE
+
+    """
+
+    return np.ma.masked_equal(array, 0)
+
+
 def compute_percent_error(array, percent):
     """
     Percent error
@@ -24,6 +71,7 @@ def compute_percent_error(array, percent):
     :rtype: TYPE
 
     """
+    percent = validate_percent(percent)
 
     return percent * np.abs(array)
 
@@ -40,6 +88,8 @@ def set_floor(error_array, floor):
     :rtype: TYPE
 
     """
+
+    floor = validate_percent(floor)
 
     error_array[np.where(error_array) < floor] = floor
     return error_array
@@ -58,10 +108,73 @@ def compute_off_diagonal_mean_error(z_array, error_value):
     :rtype: TYPE
 
     """
+    error_value = validate_percent(error_value)
+    z_array = validate_z_array_shape(z_array)
 
-    od = np.array([z_array[:, 0, 1], z_array[:, 1, 0]])
-    nz = np.nonzeros(od)
+    od = mask_zeros(np.array([z_array[:, 0, 1], z_array[:, 1, 0]]))
+    err = error_value * np.abs(np.ma.mean(od, axis=0))
 
-    return error_value * np.mean(od[nz])
+    return err
 
-def compute_median_error(array, error_value)
+
+def compute_median_error(array, error_value):
+    """
+    median(array) * error_value
+
+    :param array: DESCRIPTION
+    :type array: TYPE
+    :param error_value: DESCRIPTION
+    :type error_value: TYPE
+    :return: DESCRIPTION
+    :rtype: TYPE
+
+    """
+    error_value = validate_percent(error_value)
+    array = mask_zeros(array)
+    err = np.ma.abs(np.ma.median(array)) * error_value
+
+    return err
+
+
+def compute_eigen_value_error(array, error_value):
+    """
+    error_value * eigen(array).mean()
+
+    :param array: DESCRIPTION
+    :type array: TYPE
+    :param error_value: DESCRIPTION
+    :type error_value: TYPE
+    :return: DESCRIPTION
+    :rtype: TYPE
+
+    """
+
+    err = err_value * np.abs(np.linalg.eigvals(array)).mean()
+    if np.atleast_1d(err).sum() == 0:
+        err = error_value * array[np.nonzero(array)].mean()
+    return err
+
+
+def compute_geometric_mean_error(array, error_value):
+    """
+    error_value * sqrt(Zxy * Zyx)
+
+    :param array: DESCRIPTION
+    :type array: TYPE
+    :param error_value: DESCRIPTION
+    :type error_value: TYPE
+    :return: DESCRIPTION
+    :rtype: TYPE
+
+    """
+
+    array = validate_z_array_shape(array)
+    # if both components masked, then take error floor from
+    # max of z_xx or z_yy
+    zero_xy = np.where(array[:, 0, 1] == 0)
+    array[zero_xy] = array[zero_xy[0], 1, 0]
+
+    zero_yx = np.where(array[:, 1, 0] == 0)
+    array[zero_yx] = array[zero_yx[0], 0, 1]
+
+    return err_value * np.sqrt(np.abs(array[:, 0, 1] * array[:, 1, 0]))
