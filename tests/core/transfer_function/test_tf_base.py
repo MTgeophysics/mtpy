e4e2920a1f32181ca63c5df7e6b8506cca3fa0b7
JP <peacock.jared@gmail.com>
2022-10-21 14:24:03 -0700

    added test for tf_model_error

--- tests/core/transfer_function/test_tf_base.py
+++ tests/core/transfer_function/test_tf_base.py
@@ -80,6 +80,38 @@ class TestTFBaseTFErrorInput(unittest.TestCase):
         self.assertEqual((self.tf.period == np.arange(1, 3, 1)).all(), True)
 
 
+class TestTFBaseTFModelErrorInput(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.tf = TFBase(tf_model_error=np.array([[[0]], [[1]]]))
+        self.expected_shape = (2, 1, 1)
+        self.expected = {
+            "transfer_function": {"dtype": complex, "empty": True},
+            "transfer_function_error": {"dtype": float, "empty": True},
+            "transfer_function_model_error": {"dtype": float, "empty": False},
+        }
+
+        def test_shape_zeros_dtype(self):
+            for key, v_dict in self.expected.items():
+                tf = getattr(self.tf._dataset, key)
+                with self.subTest(f"{key} shape"):
+                    self.assertEqual(tf.shape, self.expected_shape)
+
+                with self.subTest(f"{key} dtype"):
+                    self.assertEqual(tf.dtype, v_dict["dtype"])
+
+                with self.subTest(f"{key} empty"):
+                    self.assertEqual((tf.values == 0).all(), v_dict["empty"])
+
+    def test_frequency(self):
+        self.assertEqual(
+            (self.tf.frequency == 1.0 / np.arange(1, 3, 1)).all(), True
+        )
+
+    def test_period(self):
+        self.assertEqual((self.tf.period == np.arange(1, 3, 1)).all(), True)
+
+
 # =============================================================================
 # Run
 # =============================================================================
