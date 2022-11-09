fbc24fb52aaa069f99d3e79575c7bf63aa2d77b5
JP <peacock.jared@gmail.com>
2022-11-09 10:57:37 -0800

    Update test_tipper.py

--- tests/core/transfer_function/test_tipper.py
+++ tests/core/transfer_function/test_tipper.py
@@ -206,6 +206,20 @@ class TestSetTipper(unittest.TestCase):
             ).all()
         )
 
+    def test_rotate(self):
+        b = self.tipper.rotate(45)
+
+        with self.subTest("angle_real"):
+            self.assertTrue(np.isclose(b.angle_real, np.array([90])).all())
+        with self.subTest("angle_imag"):
+            self.assertTrue(np.isclose(b.angle_imag, np.array([90])).all())
+        with self.subTest("mag_error"):
+            self.assertTrue(np.isclose(b.mag_error, np.array([0.02])).all())
+        with self.subTest("mag_model_error"):
+            self.assertTrue(
+                np.isclose(b.mag_model_error, np.array([0.06])).all()
+            )
+
 
 # =============================================================================
 # Run
