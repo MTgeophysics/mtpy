f0a31c0efecec82395501469369aac42f1113494
JP <peacock.jared@gmail.com>
2023-04-19 12:30:30 -0700

    Update plot_stations.py

--- mtpy/gui/plot_stations.py
+++ mtpy/gui/plot_stations.py
@@ -21,58 +21,69 @@ except ImportError:
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+)
+from matplotlib.backends.backend_qt5agg import (
+    NavigationToolbar2QT as NavigationToolbar,
+)
 from matplotlib.figure import Figure
 
 # =============================================================================
 # Plot stations
 # =============================================================================
 
+
 class PlotStations(QtWidgets.QWidget):
     """
     plot station locations
     """
+
     stationChanged = QtCore.pyqtSignal()
+
     def __init__(self, station_locations):
         self.station_locations = station_locations
         self.plot_crs = None
         self.current_station = None
         self.current_index = 0
         self.previous_index = 0
-        self.text_offset = 0.001
-        self.marker_dict = {'ls': "None",
-                            "ms": 7,
-                            "color": 'k',
-                            "mfc": "k",
-                            "marker":"v"}
-        self.current_marker_dict = {'ls': "None",
-                            "ms": 7,
-                            "color": 'r',
-                            "mfc": "r",
-                            "marker":"v"}
+        self.text_offset = 0.0015
+        self.marker_dict = {
+            "ls": "None",
+            "ms": 3,
+            "color": "k",
+            "mfc": "k",
+            "marker": "o",
+        }
+        self.current_marker_dict = {
+            "ls": "None",
+            "ms": 7,
+            "color": "r",
+            "mfc": "r",
+            "marker": "v",
+        }
         self.text_dict = {
             "size": 8,
             "weight": None,
             "rotation": 0,
-            "color": 'k',
+            "color": "k",
             "ha": "center",
-            "va": "baseline"
+            "va": "baseline",
         }
         self.current_text_dict = {
             "size": 8,
             "weight": None,
             "rotation": 0,
-            "color": 'r',
+            "color": "r",
             "ha": "center",
-            "va": "baseline"
+            "va": "baseline",
         }
 
         super().__init__()
         self.setup_ui()
 
     def setup_ui(self):
-        
+
         # this is the Canvas Widget that displays the `figure`
         # it takes the `figure` instance as a parameter to __init__
         self.figure = Figure(dpi=150)
@@ -82,7 +93,7 @@ class PlotStations(QtWidgets.QWidget):
 
         # be able to edit the data
         self.mpl_widget.mpl_connect("pick_event", self.on_pick)
-        #self.mpl_widget.mpl_connect("button_press_event", self.on_pick)
+        # self.mpl_widget.mpl_connect("button_press_event", self.on_pick)
 
         # make sure the figure takes up the entire plottable space
         self.mpl_widget.setSizePolicy(
@@ -100,15 +111,13 @@ class PlotStations(QtWidgets.QWidget):
 
         self.setLayout(mpl_vbox)
         self.mpl_widget.updateGeometry()
-        
+
     def plot(self):
         """
         Plot stations with names
         """
-        
-        plt.rcParams["font.size"] = 10
 
-        
+        plt.rcParams["font.size"] = 10
 
         label_font_dict = {"size": 12, "weight": "bold"}
         # self.xlimits = (
@@ -128,73 +137,103 @@ class PlotStations(QtWidgets.QWidget):
         self.figure.clf()
         self.ax = self.figure.add_subplot(1, 1, 1, aspect="equal")
         self.ax.plot(
-                self.station_locations.lon,
-                self.station_locations.lat,
-                picker=True,
-                pickradius=10,
-                **self.marker_dict
-            )
+            self.station_locations.lon,
+            self.station_locations.lat,
+            picker=True,
+            pickradius=10,
+            **self.marker_dict,
+        )
 
-        for station, x, y in zip(self.station_locations.station, 
-                                 self.station_locations.lon,
-                                 self.station_locations.lat):
-            
+        for station, x, y in zip(
+            self.station_locations.station,
+            self.station_locations.lon,
+            self.station_locations.lat,
+        ):
 
             self.ax.text(
                 x,
                 y + self.text_offset * np.sign(y),
                 station,
                 fontdict=self.text_dict,
+                clip_on=True,
             )
 
         # set axis properties
         self.ax.set_xlabel(xlabel, fontdict=label_font_dict)
         self.ax.set_ylabel(ylabel, fontdict=label_font_dict)
-        self.ax.grid(alpha=0.35, color=(0.25, 0.25, 0.25), lw=.25)
+        self.ax.grid(alpha=0.35, color=(0.25, 0.25, 0.25), lw=0.25)
         self.ax.set_axisbelow(True)
         self.figure.tight_layout()
 
         self.mpl_widget.draw()
-        
+
     def plot_new_station(self):
         self.ax.plot(
-            self.station_locations.station_locations["lon"][self.previous_index],
-            self.station_locations.station_locations["lat"][self.previous_index],
+            self.station_locations.station_locations["lon"][
+                self.previous_index
+            ],
+            self.station_locations.station_locations["lat"][
+                self.previous_index
+            ],
             **self.marker_dict,
-            )
+        )
 
         self.ax.text(
-            self.station_locations.station_locations["lon"][self.previous_index],
-            self.station_locations.station_locations["lat"][self.previous_index] +
-            self.text_offset * np.sign(self.station_locations.station_locations["lat"][self.previous_index]),
-            self.station_locations.station_locations["station"][self.previous_index],
+            self.station_locations.station_locations["lon"][
+                self.previous_index
+            ],
+            self.station_locations.station_locations["lat"][
+                self.previous_index
+            ]
+            + self.text_offset
+            * np.sign(
+                self.station_locations.station_locations["lat"][
+                    self.previous_index
+                ]
+            ),
+            self.station_locations.station_locations["station"][
+                self.previous_index
+            ],
             fontdict=self.text_dict,
-            )
-        
+        )
+
         self.ax.plot(
-            self.station_locations.station_locations["lon"][self.current_index],
-            self.station_locations.station_locations["lat"][self.current_index],
-            **self.current_marker_dict
-            )
+            self.station_locations.station_locations["lon"][
+                self.current_index
+            ],
+            self.station_locations.station_locations["lat"][
+                self.current_index
+            ],
+            **self.current_marker_dict,
+        )
 
         self.ax.text(
-            self.station_locations.station_locations["lon"][self.current_index],
-            self.station_locations.station_locations["lat"][self.current_index] +
-            self.text_offset * np.sign(self.station_locations.station_locations["lat"][self.current_index]),
-            self.station_locations.station_locations["station"][self.current_index],
-            **self.current_text_dict
-            )
-        self.ax.figure.canvas.draw()  
+            self.station_locations.station_locations["lon"][
+                self.current_index
+            ],
+            self.station_locations.station_locations["lat"][self.current_index]
+            + self.text_offset
+            * np.sign(
+                self.station_locations.station_locations["lat"][
+                    self.current_index
+                ]
+            ),
+            self.station_locations.station_locations["station"][
+                self.current_index
+            ],
+            **self.current_text_dict,
+        )
+        self.ax.figure.canvas.draw()
 
     def redraw_plot(self, new_station_locations):
         """
-        
+
         :param new_station_locations: DESCRIPTION
         :type new_station_locations: TYPE
         :return: DESCRIPTION
         :rtype: TYPE
 
-        """   
+        """
         self.station_locations = new_station_locations
         self.plot()
 
@@ -212,12 +251,16 @@ class PlotStations(QtWidgets.QWidget):
             self.previous_index = int(self.current_index)
 
             # get the indicies where the data point has been edited
-            self.current_index = np.where((self.station_locations.station_locations["lat"] == data_lat) &
-                             (self.station_locations.station_locations["lon"] == data_lon))[0][0]
-            self.current_station = self.station_locations.station[self.current_index]
-            
+            self.current_index = np.where(
+                (self.station_locations.station_locations["lat"] == data_lat)
+                & (self.station_locations.station_locations["lon"] == data_lon)
+            )[0][0]
+            self.current_station = self.station_locations.station[
+                self.current_index
+            ]
+
             self.plot_new_station()
-            
+
             self.stationChanged.emit()
             print(f"station changed to: {self.current_station}")
 
